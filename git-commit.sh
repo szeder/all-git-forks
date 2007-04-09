94a34dc97ef8af6a48cc7d5be8c4ac53d98954e0
Junio C Hamano <junkio@cox.net>
2007-04-08 21:25:42 -0700

    Move check_base() shell function to git-sh-setup
    The scripts that attempt to make a commit on top of the current
    HEAD need to use the same logic in check_base() to make sure the
    current index is based on the HEAD commit we are going to build
    on top of.  Move this function from git-commit to git-sh-setup,
    so that it is available to everybody else.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- git-commit.sh
+++ git-commit.sh
@@ -25,22 +25,6 @@ refuse_partial () {
 	exit 1
 }
 
-check_base () {
-	quiet="$1"
-	if HEAD=`git rev-parse --verify HEAD 2>/dev/null` &&
-	   BASE=`git update-index --get-base` &&
-	   test -n "$BASE" &&
-	   test "$BASE" != "$HEAD"
-	then
-		test -z "$quiet" || exit 1
-		ours=`git show -s --pretty=oneline --abbrev-commit $BASE`
-		theirs=`git show -s --pretty=oneline --abbrev-commit $HEAD`
-		echo >&2 "* The index is based on '$ours', however, the HEAD"
-		echo >&2 "  points at different commit '$theirs'"
-		exit 1
-	fi
-}
-
 THIS_INDEX="$GIT_DIR/index"
 NEXT_INDEX="$GIT_DIR/next-index$$"
 rm -f "$NEXT_INDEX"
