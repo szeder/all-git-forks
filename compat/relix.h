6b77188c604e3a69e05f09682f208a889fa4a17c
Joshua Juran <jjuran@metamage.com>
2009-04-02 20:19:40 -0700

    Add compat/relix.h to support MacRelix
    Include <ctype.h> *before* defining macros of its functions, in case it's
    included later (which it is, in the case of fnmatch and xdiff).
    
    Define GIT_VERSION.
    
    Define a smaller LARGE_PACKET_MAX for MacRelix due to limited local data
    space on 68K.
    
    Define USE_CPLUSPLUS_FOR_INIT so #pragma cplusplus will be enabled during
    initialization of aggregates involving values computed at runtime.
    
    Add dummy definitions of GIT_{MAN,INFO,HTML}_PATH.
    
    Declare reexec(), which when called after vfork() spawns a lightweight
    process, which shares memory space with the parent but has its own file
    descriptor set (among other things).  This substitutes for fork() in
    start_async().

---
+++ compat/relix.h
@@ -0,0 +1,48 @@
+/*
+	relix.h
+	-------
+	
+	MacRelix support, by Josh Juran
+*/
+
+// Include <ctype.h> early so git's macros don't interfere with it later
+#include <ctype.h>
+
+#define GIT_VERSION "1.7.5"
+
+// MacRelix has small thread stacks, and 68K has a hard 32K local data limit.
+#define LARGE_PACKET_MAX (16384 - 16)
+
+// Enable #pragma cplusplus over dynamic aggregate initializers.
+#define USE_CPLUSPLUS_FOR_INIT 1
+
+#define NO_CURL  1
+#define NO_EXPAT 1
+#define NO_ICONV 1
+#define NO_IPV6  1
+#define NO_NSEC  1
+#define NO_OPENSSL 1
+#define NO_PTHREADS 1
+#define NO_LIBGEN_H 1
+
+#define HAVE_STRING_H 1
+
+#define SHA1_HEADER "block-sha1/sha1.h"
+
+#define ETC_GITCONFIG "/etc/gitconfig"
+
+#define ETC_GITATTRIBUTES "/etc/gitattributes"
+
+#define GIT_EXEC_PATH "/usr/lib/git-core"
+
+#define GIT_MAN_PATH  "man"
+#define GIT_INFO_PATH "info"
+#define GIT_HTML_PATH "html"
+
+#define PREFIX "/usr"
+
+#define gitmemmem     memmem
+#define gitstrcasestr strcasestr
+
+extern int reexec( void* f, ... );
+
