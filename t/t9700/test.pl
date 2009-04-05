dd39cdd6a27317fdcccfaee8ca651c3b3201c8ec
Sam Vilain <sam.vilain@catalyst.net.nz>
2009-04-06 11:39:57 +1200

    perl: make Git.pm use new Git::Config module
    Plug these two modules together.  The only minor API difference is that
    in void context (ie, return value is not being used), the new Git::Config
    function does not try to unpack the value.  So, the exist test - which was
    testing an error condition - must be changed to actually try to retrieve
    the values so that the exceptions can happen.
    
    Signed-off-by: Sam Vilain <sam.vilain@catalyst.net.nz>

--- t/t9700/test.pl
+++ t/t9700/test.pl
@@ -35,9 +35,9 @@ is($r->get_color("color.test.slot1", "red"), $ansi_green, "get_color");
 # Save and restore STDERR; we will probably extract this into a
 # "dies_ok" method and possibly move the STDERR handling to Git.pm.
 open our $tmpstderr, ">&STDERR" or die "cannot save STDERR"; close STDERR;
-eval { $r->config("test.dupstring") };
+eval { my $x = $r->config("test.dupstring") };
 ok($@, "config: duplicate entry in scalar context fails");
-eval { $r->config_bool("test.boolother") };
+eval { my $x = $r->config_bool("test.boolother") };
 ok($@, "config_bool: non-boolean values fail");
 open STDERR, ">&", $tmpstderr or die "cannot restore STDERR";
 
