51b6d9885a4089cdb1d42a252a4842b4926434f9
Tay Ray Chuan <rctay89@gmail.com>
2009-04-12 01:43:10 +0800

    http: add_fill_function checks if invocation is a duplicate
    add_fill_action checks if the callback function and associated callback
    data to be added has already been added. Allocation of memory for the
    list ''fill_chain*'' is postponed until the check passes, unlike
    previously.
    
    Calling a fill function repeatedly won't break behaviour, because fill
    functions (those that are currently defined in git) are designed to be
    called repeatedly, but it's pointless to call the same fill function
    with the same data repeatedly, without any good reason.

--- http.c
+++ http.c
@@ -467,13 +467,17 @@ static struct fill_chain *fill_cfg;
 
 void add_fill_function(void *data, int (*fill)(void *))
 {
-	struct fill_chain *new = xmalloc(sizeof(*new));
+	struct fill_chain *new;
 	struct fill_chain **linkp = &fill_cfg;
+	for (;*linkp; linkp = &(*linkp)->next)
+		if ((*linkp)->data == data && (*linkp)->fill == fill)
+			return;
+
+	new = xmalloc(sizeof(*new));
 	new->data = data;
 	new->fill = fill;
 	new->next = NULL;
-	while (*linkp)
-		linkp = &(*linkp)->next;
+
 	*linkp = new;
 }
 
