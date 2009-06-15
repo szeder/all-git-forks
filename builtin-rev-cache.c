5efdb46948213636fc549ee3fd647660fa829619
Sam Vilain <sam@vilain.net>
2009-06-15 20:32:07 +1200

    restore builtin-rev-cache and rev-cache from remotes/sirnot/t/revcache/revcache

--- builtin-rev-cache.c
+++ builtin-rev-cache.c
@@ -4,6 +4,63 @@
 #include "diff.h"
 #include "revision.h"
 
+/* porcelain for rev-cache.c */
+static int handle_add(int argc, const char *argv[]) /* args beyond this command */
+{
+	struct rev_info revs;
+	char dostdin = 0, dont_pack_it = 0, do_legs = 0;
+	unsigned int flags = 0;
+	int i;
+	
+	init_revisions(&revs, 0);
+	
+	for (i = 0; i < argc; i++) {
+		if (!strcmp(argv[i], "--stdin"))
+			dostdin = 1;
+		else if (!strcmp(argv[i], "--fresh"))
+			uninteresting_from_slices(&revs, 0, 0);
+		else if (!strcmp(argv[i], "--not"))
+			flags ^= UNINTERESTING;
+		else if (!strcmp(argv[i], "--nopack"))
+			dont_pack_it = 1;
+		else if (!strcmp(argv[i], "--legs"))
+			do_legs = 1;
+		else
+			handle_revision_arg(argv[i], &revs, flags, 1);
+	}
+	
+	if (dostdin) {
+		char line[1000];
+		
+		flags = 0;
+		while (fgets(line, sizeof(line), stdin)) {
+			int len = strlen(line);
+			while (len && (line[len - 1] == '\n' || line[len - 1] == '\r'))
+				line[--len] = 0;
+			
+			if (!len)
+				break;
+			
+			if (!strcmp(line, "--not"))
+				flags ^= UNINTERESTING;
+			else
+				handle_revision_arg(line, &revs, flags, 1);
+		}
+	}
+	
+	return make_cache_slice(&revs, 0, 0, do_legs);
+}
+
+static int handle_show(int argc, const char *argv[])
+{
+	die("haven't implemented cache enumeration yet (try 'git-rev-cache help' to show usage)");
+}
+
+static int handle_rm(int argc, const char *argv[])
+{
+	die("haven't implemented rm thingy yet");
+}
+
 static int handle_walk(int argc, const char *argv[])
 {
 	struct commit *commit;
@@ -84,6 +141,14 @@ git-rev-cache COMMAND [options] [<commit-id>...]\n\
 commands:\n\
  (none) - display caches.  passing a slice hash will display detailed\n\
           information about that cache slice\n\
+ add    - add revisions to the cache.  reads commit ids from stdin, \n\
+          formatted as: END END ... --not START START ...\n\
+          options:\n\
+           --fresh    exclude everything already in a cache slice\n\
+           --nopack   don't generate a cache slice pack\n\
+           --stdin    also read commit ids from stdin (same form as cmd)\n\
+           --legs     ensure branch is entirely self-contained\n\
+ rm     - delete a cache slice\n\
  walk   - walk a cache slice based on set of commits; formatted as add";
 	
 	puts(usage);
@@ -96,6 +161,15 @@ commands:\n\
 usage:
 git-rev-cache COMMAND [options] [<commit-id>...]
 commands:
+ (nothing)	- display caches.  passing a slice hash will display detailed
+ 			  information about that cache slice.
+ add		- add revisions to the cache.  reads commit hashes from stdin, 
+ 			  formatted as: END END ... --not START START ...
+ 			  option --cmd allows reading from the command line (anything 
+ 			  beyond -- will be interpreted as a commit-id under this), 
+ 			  --fresh excludes anything already in a cache.
+ rm			- delete a cache slice.  --all will remove everything, otherwise
+ 			  will read hashes from stdin.  --cmd will work as with add.
  walk		- walk a cache slice based on a given commit
    
 
