eb64c9760e1aa4f9318648bd04550cbaccd5bda0
Nick Edelen <sirnot@gmail.com>
2009-08-12 23:26:56 +0200

    cleanup

--- rev-cache.c
+++ rev-cache.c
@@ -179,7 +179,6 @@ static int setup_traversal(struct rc_slice_header *head, unsigned char *map, str
 	struct commit_list *prev, *wp, **wpp;
 	int retval;
 	
-	/* printf("adding %s\n", sha1_to_hex(commit->object.sha1)); */
 	iep = search_index(commit->object.sha1);
 	oep = OE_CAST(map + ntohl(iep->pos));
 	oep->include = 1;
