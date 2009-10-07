b9f83a100dcf03b672350acc212bc74a651a6785
Greg Price <price@ksplice.com>
2009-10-07 17:49:02 -0400

    rebase -i -p: always merge --no-ff
    If the user left a merge command in the TODO file, they wanted a
    merge commit.  If we generated a merge command, there was a merge
    commit, so the user has asked us to preserve it.  Either way,
    we should generate a merge commit.
    
    Signed-off-by: Greg Price <price@ksplice.com>

--- git-rebase--interactive.sh
+++ git-rebase--interactive.sh
@@ -287,7 +287,7 @@ merge_one () {
 	# the command was "merge parents ...", so "parents" was recorded
 	echo "$sha1" > "$REWRITTEN"/original
 	msg="$(commit_message $sha1)"
-	git merge $STRATEGY -m "$msg" $parents &&
+	git merge $STRATEGY --no-ff -m "$msg" $parents &&
 	add_rewritten ||
 	die_with_patch $sha1 "Could not redo merge $sha1 with parents $parents"
 	echo "$sha1 $(git rev-parse HEAD^0)" >> "$REWRITTEN_LIST"
