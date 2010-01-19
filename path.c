85a0baff19934c14ff7c82832de24f4a6758596f
Johannes Sixt <j6t@kdbg.org>
2010-01-19 13:50:35 +0100

    Treat Windows style paths in daemon_avoid_alias.
    Without this increased portability, http-backend always dies with code
    500 because it mistakes a path beginning with a drive prefix as a relative
    path.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>

--- path.c
+++ path.c
@@ -1190,9 +1190,11 @@ int daemon_avoid_alias(const char *p)
 	 * be true as long as dots continue after that without intervening
 	 * non-dot character.
 	 */
-	if (!p || (*p != '/' && *p != '~'))
+	if (!p || (!is_absolute_path(p) && *p != '~'))
 		return -1;
 	sl = 1; ndot = 0;
+	if (has_dos_drive_prefix(p))
+		p += 2;
 	p++;
 
 	while (1) {
@@ -1200,7 +1202,7 @@ int daemon_avoid_alias(const char *p)
 		if (sl) {
 			if (ch == '.')
 				ndot++;
-			else if (ch == '/') {
+			else if (is_dir_sep(ch)) {
 				if (ndot < 3)
 					/* reject //, /./ and /../ */
 					return -1;
@@ -1217,7 +1219,7 @@ int daemon_avoid_alias(const char *p)
 		}
 		else if (ch == 0)
 			return 0;
-		else if (ch == '/') {
+		else if (is_dir_sep(ch)) {
 			sl = 1;
 			ndot = 0;
 		}
