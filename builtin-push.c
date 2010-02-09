8f6369e58ee44d41c9ee535b55aee2ffb52f69ae
Larry D'Anna <larry@elder-gods.org>
2010-02-09 00:54:00 -0500

    git-push: squelch advice message if in --porcelain mode
    By default, git-push may give the user a verbose advice message if a ref is
    rejected for not being a fast-forward.  This patch squelches that message for
    --porcelain mode.
    
    Signed-off-by: Larry D'Anna <larry@elder-gods.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-push.c
+++ builtin-push.c
@@ -226,6 +226,11 @@ int cmd_push(int argc, const char **argv, const char *prefix)
 	git_config(git_default_config, NULL);
 	argc = parse_options(argc, argv, prefix, options, push_usage, 0);
 
+	if (flags & TRANSPORT_PUSH_PORCELAIN) {
+		/* Do not give advice messages to Porcelain scripts */
+		advice_push_nonfastforward = 0;
+	}
+
 	if (deleterefs && (tags || (flags & (TRANSPORT_PUSH_ALL | TRANSPORT_PUSH_MIRROR))))
 		die("--delete is incompatible with --all, --mirror and --tags");
 	if (deleterefs && argc < 2)
