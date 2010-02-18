73f7e7960b50b6ffb4e08a83e83b2e5c45164e6c
Tay Ray Chuan <rctay89@gmail.com>
2010-02-18 18:34:45 +0800

    transport-helper: provide abort_helper()
    Mostly changes function calls to take an additional argument
    (transport) to set the stage for later patches,  and does not change
    behaviour significantly.
    
    Presently, it exit()'s, as before, and alerts the user with an error
    message.
    
    Signed-off-by: Tay Ray Chuan <rctay89@gmail.com>

--- transport-helper.c
+++ transport-helper.c
@@ -49,15 +49,23 @@ static void sendline_str(struct helper_data *helper, const char *str)
 	sendline_str_len(helper, str, strlen(str));
 }
 
-static int recvline_fh(FILE *helper, struct strbuf *buffer)
+static void abort_helper(struct transport *transport);
+
+static int recvline_fh(struct transport *transport, FILE *helper,
+	struct strbuf *buffer)
 {
+	struct helper_data *data = (struct helper_data *)transport->data;
+
 	strbuf_reset(buffer);
 	if (debug)
 		fprintf(stderr, "Debug: Remote helper: Waiting...\n");
 	if (strbuf_getline(buffer, helper, '\n') == EOF) {
 		if (debug)
 			fprintf(stderr, "Debug: Remote helper quit.\n");
-		exit(128);
+		fprintf(stderr, "Remote helper '%s' has quit unexpectedly.\n",
+			data->name);
+		abort_helper(transport);
+		return 1;
 	}
 
 	if (debug)
@@ -65,15 +73,17 @@ static int recvline_fh(FILE *helper, struct strbuf *buffer)
 	return 0;
 }
 
-static int recvline(struct helper_data *helper, struct strbuf *buffer)
+static int recvline(struct transport *transport, struct helper_data *helper,
+	struct strbuf *buffer)
 {
-	return recvline_fh(helper->out, buffer);
+	return recvline_fh(transport, helper->out, buffer);
 }
 
-static void xchgline(struct helper_data *helper, struct strbuf *buffer)
+static void xchgline(struct transport *transport, struct helper_data *helper,
+	struct strbuf *buffer)
 {
 	sendline(helper, buffer);
-	recvline(helper, buffer);
+	recvline(transport, helper, buffer);
 }
 
 const char *remove_ext_force(const char *url)
@@ -144,7 +154,7 @@ static struct child_process *get_helper(struct transport *transport)
 	while (1) {
 		const char *capname;
 		int mandatory = 0;
-		recvline(data, &buf);
+		recvline(transport, data, &buf);
 
 		if (!*buf.buf)
 			break;
@@ -227,6 +237,11 @@ static int disconnect_helper(struct transport *transport)
 	return 0;
 }
 
+static void abort_helper(struct transport *transport)
+{
+	exit(128);
+}
+
 static const char *unsupported_options[] = {
 	TRANS_OPT_UPLOADPACK,
 	TRANS_OPT_RECEIVEPACK,
@@ -270,7 +285,7 @@ static int set_helper_option(struct transport *transport,
 		quote_c_style(value, &buf, NULL, 0);
 	strbuf_addch(&buf, '\n');
 
-	xchgline(data, &buf);
+	xchgline(transport, data, &buf);
 
 	if (!strcmp(buf.buf, "ok"))
 		ret = 0;
@@ -333,7 +348,7 @@ static int fetch_with_fetch(struct transport *transport,
 	sendline(data, &buf);
 
 	while (1) {
-		recvline(data, &buf);
+		recvline(transport, data, &buf);
 
 		if (!prefixcmp(buf.buf, "lock ")) {
 			const char *name = buf.buf + 5;
@@ -449,7 +464,8 @@ static int process_connect_service(struct transport *transport,
 		goto exit;
 
 	sendline(data, &cmdbuf);
-	recvline_fh(input, &cmdbuf);
+	recvline_fh(transport, input, &cmdbuf);
+
 	if (!strcmp(cmdbuf.buf, "")) {
 		data->no_disconnect_req = 1;
 		if (debug)
@@ -607,7 +623,7 @@ static int push_refs(struct transport *transport,
 		char *refname, *msg;
 		int status;
 
-		recvline(data, &buf);
+		recvline(transport, data, &buf);
 		if (!buf.len)
 			break;
 
@@ -713,7 +729,7 @@ static struct ref *get_refs_list(struct transport *transport, int for_push)
 
 	while (1) {
 		char *eov, *eon;
-		recvline(data, &buf);
+		recvline(transport, data, &buf);
 
 		if (!*buf.buf)
 			break;
