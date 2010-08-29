42b398e12bc4ffdc8d5502fb7fa054408df69bb5
Jon Seymour <jon.seymour@gmail.com>
2010-08-29 19:01:56 +1000

    Introduce git-test.sh and git-test-lib.sh
    This command is intended provide a uniform command line interface
    to a suite of assertions that can be made about the state of
    the working tree, index and repository.
    
    This commit introduces the core assert infrastructure. Subsequent
    commits will introduce check functions that extend the infrastructure
    in a modular way with additional tests.
    
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>
    
    Replace environment with git configuration.
    
    test_must_fail: core

---
+++ git-conditions-lib.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# (c) Copyright Jon Seymour 2010
+#
+if test -z "${__GIT_CONDITIONS_LIB_INCLUDED}"
+then
+__GIT_CONDITIONS_LIB_INCLUDED=t
+#
+# Provides a function that enables the evaluation of assertions.
+#
+
+fi
