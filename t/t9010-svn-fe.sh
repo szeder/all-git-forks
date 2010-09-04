3842418f26c793f61a21f3664f0e1b2139492f50
Ramkumar Ramachandra <artagnon@gmail.com>
2010-09-05 00:24:54 +0530

    t9010-svn-fe: Add tests for svndiff0 parser
    Add a couple of tests for the svndiff0 parser in contrib/ along with
    corresponding test data in t9135/

--- t/t9010-svn-fe.sh
+++ t/t9010-svn-fe.sh
@@ -5,6 +5,7 @@ test_description='check svn dumpfile importer'
 . ./test-lib.sh
 
 svnfe_bin=$GIT_EXEC_PATH/contrib/svn-fe/svn-fe
+parser_bin=$GIT_EXEC_PATH/contrib/svn-fe/svndiff-parser
 svnconf=$PWD/svnconf
 export svnconf
 
@@ -16,6 +17,16 @@ svn_cmd () {
 		return
 	fi
 	svn "$orig_svncmd" --config-dir "$svnconf" "$@"
+}
+
+test_parse() {
+	diff0=$1
+	source=$2
+	expected=$3
+	test_expect_success PARSER "$diff0" '
+		$parser_bin "$TEST_DIRECTORY/$source" <"$TEST_DIRECTORY/$diff0" >actual &&
+		test_cmp "$TEST_DIRECTORY/$expected" actual
+	'
 }
 
 test_dump() {
@@ -45,6 +56,12 @@ if [ -x $svnfe_bin ]; then
     test_set_prereq SVNFE
 fi
 
+if [ -x $svndiff_parser ]; then
+    test_set_prereq PARSER
+fi
+
 test_dump simple t9135/svn.dump
+test_parse t9135/newdata.diff0 t9135/blank.done t9135/newdata.done
+test_parse t9135/src.diff0 t9135/newdata.done t9135/src.done
 
 test_done
