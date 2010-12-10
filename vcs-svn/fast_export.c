2160bbd7c01f6f92bca46497d5875a9fbee89ab9
Jonathan Nieder <jrnieder@gmail.com>
2010-12-09 18:57:13 -0600

    vcs-svn: save marks for imported commits
    This way, a person can use
    
    	svnadmin dump $path |
    	svn-fe |
    	git fast-import --relative-marks --export-marks=svn-revs
    
    to get a list of what commit corresponds to each svn revision (plus
    some irrelevant blob names) in .git/info/fast-import/svn-revs.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: David Barr <david.barr@cordelta.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- vcs-svn/fast_export.c
+++ vcs-svn/fast_export.c
@@ -47,6 +47,7 @@ void fast_export_commit(uint32_t revision, const char *author,
 		*gitsvnline = '\0';
 	}
 	printf("commit refs/heads/master\n");
+	printf("mark :%"PRIu32"\n", revision);
 	printf("committer %s <%s@%s> %ld +0000\n",
 		   *author ? author : "nobody",
 		   *author ? author : "nobody",
