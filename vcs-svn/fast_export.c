f24b0f55868a160237f9ca82c91bc71f104dc4e4
Jonathan Nieder <jrnieder@gmail.com>
2010-12-28 04:30:54 -0600

    vcs-svn: improve reporting of input errors
    Catch input errors and exit early enough to print a reasonable
    diagnosis based on errno.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- vcs-svn/fast_export.c
+++ vcs-svn/fast_export.c
@@ -62,14 +62,23 @@ void fast_export_commit(uint32_t revision, uint32_t author, char *log,
 	printf("progress Imported commit %d.\n\n", revision);
 }
 
+static void die_short_read(void)
+{
+	if (buffer_ferror())
+		die_errno("error reading dump file");
+	die("invalid dump: unexpected end of file");
+}
+
 void fast_export_blob(uint32_t mode, uint32_t mark, uint32_t len)
 {
 	if (mode == REPO_MODE_LNK) {
 		/* svn symlink blobs start with "link " */
-		buffer_skip_bytes(5);
 		len -= 5;
+		if (buffer_skip_bytes(5) != 5)
+			die_short_read();
 	}
 	printf("blob\nmark :%d\ndata %d\n", mark, len);
-	buffer_copy_bytes(len);
+	if (buffer_copy_bytes(len) != len)
+		die_short_read();
 	fputc('\n', stdout);
 }
