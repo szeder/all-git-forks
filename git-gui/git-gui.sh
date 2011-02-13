7f6232990e359486a554d67f8aff84cc03b670ef
Heiko Voigt <hvoigt@hvoigt.net>
2011-02-13 13:44:16 +0100

    git-gui: document the gui.maxfilesdisplayed variable
    This removes a TODO item in the git-gui source.
    
    Signed-off-by: Heiko Voigt <hvoigt@hvoigt.net>

--- git-gui/git-gui.sh
+++ git-gui/git-gui.sh
@@ -894,7 +894,6 @@ set default_config(gui.newbranchtemplate) {}
 set default_config(gui.spellingdictionary) {}
 set default_config(gui.fontui) [font configure font_ui]
 set default_config(gui.fontdiff) [font configure font_diff]
-# TODO: this option should be added to the git-config documentation
 set default_config(gui.maxfilesdisplayed) 5000
 set default_config(gui.usettk) 1
 set default_config(gui.warndetachedcommit) 1
