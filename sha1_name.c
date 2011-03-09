b4c685ad165ae2fccfece38d1f44798d68a93b92
Michael J Gruber <git@drmicha.warpmail.net>
2011-03-09 09:56:52 +0100

    sha1_name: introduce ref:refname for refname objects

--- sha1_name.c
+++ sha1_name.c
@@ -1342,6 +1342,20 @@ static int get_sha1_with_context_1(const char *name,
 
 			free(new_filename);
 			return ret;
+		} else if (!memcmp(name, "ref", 3)) {
+			unsigned char discard[20];
+			char *full;
+
+			switch (dwim_ref(name+4, strlen(name+4), discard, &full)) {
+			case 0:
+				die("Invalid refname '%s'.", name+4);
+			case 1: /* happy */
+				break;
+			default: /* ambiguous */
+				error("Refname '%s' is ambiguous.", name+4);
+				break;
+			}
+			return pretend_sha1_file((void *) (full), strlen(full), OBJ_BLOB, sha1);
 		} else {
 			if (only_to_die)
 				die("Invalid object name '%.*s'.", len, name);
