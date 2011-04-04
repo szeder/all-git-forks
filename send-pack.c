8d4a8c3e22e2a7e16d311bcd5afa6bc9e55533e8
Jeff King <peff@peff.net>
2011-04-03 20:53:08 -0400

    send-pack: kill pack-objects helper on signal or exit
    We spawn an external pack-objects process to actually send
    objects to the remote side. If we are killed by a signal
    during this process, the pack-objects will keep running and
    complete the push, which may surprise the user. We should
    take it down when we go down.

--- send-pack.c
+++ send-pack.c
@@ -88,6 +88,7 @@ static int pack_objects(int fd, struct ref *refs, struct sha1_array *extra, stru
 	po.in = -1;
 	po.out = args->stateless_rpc ? -1 : fd;
 	po.git_cmd = 1;
+	po.clean_on_exit = 1;
 	if (start_command(&po))
 		die_errno("git pack-objects failed");
 
