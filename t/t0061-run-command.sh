a16a188f3cc19418d364c08a8b5d8d00af1ea0b0
Jonathan Nieder <jrnieder@gmail.com>
2011-04-19 02:05:10 -0500

    run-command: write full error message in die_child
    The run_command facility writes a truncated error message when the
    command is present but cannot be executed for some other reason.  For
    example, if I add a 'hello' command to git:
    
    	$ echo 'echo hello' >git-hello
    	$ chmod +x git-hello
    	$ PATH=.:$PATH git hello
    	hello
    
    and then make it non-executable, this is what I get from 'maint':
    
    	$ chmod a-x git-hello
    	$ git hello
    	fatal: cannot exec 'git-hello': Permission denied
    
    But with 'master', we get disturbing output:
    
    	$ PATH=.:$PATH git hello
    	fatal: $
    
    That is a regression introduced by v1.7.5-rc0~29^2 (run-command:
    prettify -D_FORTIFY_SOURCE workaround, 2011-03-16), which uses the
    construct "if (write(...) || write(...) || write(...))" to perform
    some writes in sequence, with the "if" body acknowledging errors from
    them once.  write does not return 0 on success, so only the first
    write succeeds.  Oops.
    
    While fixing the above, let's actually pay attention to the return
    value and handle partial writes.  write_in_full has the desired
    semantics --- it loops until the desired number of bytes have been
    written and on error it returns -1 to let us handle the error.
    
    The "if" to appease warn_unused_result is no longer necessary after
    this patch since xwrite and write_in_full check the return value from
    write(2), but we leave it in for clarity and for robustness against
    future static analyzers.
    
    Reported-by: Junio C Hamano <gitster@pobox.com>
    Analysis-by: Jeff King <peff@peff.net>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0061-run-command.sh
+++ t/t0061-run-command.sh
@@ -7,8 +7,32 @@ test_description='Test run command'
 
 . ./test-lib.sh
 
+cat >hello-script <<-EOF
+	#!$SHELL_PATH
+	echo hello
+EOF
+>empty
+
 test_expect_success 'start_command reports ENOENT' '
 	test-run-command start-command-ENOENT ./does-not-exist
+'
+
+test_expect_success 'run_command can run a command' '
+	echo hello >expect &&
+	cat hello-script >hello.sh &&
+	chmod +x hello.sh &&
+	test-run-command run-command ./hello.sh >actual 2>err &&
+
+	test_cmp expect actual &&
+	test_cmp empty err
+'
+
+test_expect_success POSIXPERM,SANITY 'run_command reports EACCES' '
+	cat hello-script >hello.sh &&
+	chmod -x hello.sh &&
+	test_must_fail test-run-command run-command ./hello.sh 2>err &&
+
+	grep "fatal: cannot exec.*hello.sh" err
 '
 
 test_done
