91fb1639102c46ddc1308a4e9ee71dc6a566d07d
Fredrik Gustafsson <iveqy@iveqy.com>
2011-05-12 23:22:10 +0200

    wip

--- t/t7406-submodule-update.sh
+++ t/t7406-submodule-update.sh
@@ -302,8 +302,7 @@ test_expect_success 'submodule update continues after error' '
 	(cd super &&
 	 git reset --hard HEAD &&
 	 git submodule add ../submodule submodule2 &&
-	 git add submodule2 &&
-	 git commit -am "new_submodule"
+	 git commit -am "new_submodule" &&
 	 (cd submodule &&
 	  test_commit "update_submodule" file
 	 ) &&
@@ -316,8 +315,9 @@ test_expect_success 'submodule update continues after error' '
 	 (cd submodule &&
 	 	echo "" > file
 	 ) &&
-	 git checkout HEAD^
+	 git checkout HEAD^ &&
 	 test_must_fail git submodule update
+	 bash
 	) 
 '
 
