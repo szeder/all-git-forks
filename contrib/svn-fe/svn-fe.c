09f1079489097f4f1bbb9107e3673ddd506a931c
Dmitry Ivankov <divanorama@gmail.com>
2011-06-16 13:05:52 +0600

    Allow to specify git branch for a svn fast-export stream
    Add corresponding --ref= parameter to svn-fe.
    
    Signed-off-by: Dmitry Ivankov <divanorama@gmail.com>

--- contrib/svn-fe/svn-fe.c
+++ contrib/svn-fe/svn-fe.c
@@ -9,17 +9,21 @@
 #include "svndump.h"
 
 static void print_usage() {
-	fprintf(stderr, "svn-fe [[--url=]url] < dump | fast-import-backend\n\n"
+	fprintf(stderr, "svn-fe [--ref=dst_ref] [[--url=]url] < dump | fast-import-backend\n\n"
+			"dst_ref - git ref to be updated with svn commits, defaults to refs/heads/master\n"
 			"url - if set commit logs will have git-svn-id: line appended\n");
 }
 
 int main(int argc, char **argv)
 {
 	const char* url = NULL;
+	const char* ref = "refs/heads/master";
 	int i;
 	for (i = 1; i < argc; ++i) {
 		if (!strncmp(argv[i], "--url=", 6))
 			url = argv[i] + 6;
+		else if (!strncmp(argv[i], "--ref=", 6))
+			ref = argv[i] + 6;
 		else
 			break;
 	}
@@ -29,7 +33,7 @@ int main(int argc, char **argv)
 		print_usage();
 		return 1;
 	}
-	if (svndump_init(NULL))
+	if (svndump_init(NULL, ref))
 		return 1;
 	svndump_read(url);
 	svndump_deinit();
--- vcs-svn/svndump.h
+++ vcs-svn/svndump.h
@@ -1,7 +1,7 @@
 #ifndef SVNDUMP_H_
 #define SVNDUMP_H_
 
-int svndump_init(const char *filename);
+int svndump_init(const char *filename, const char* branch);
 void svndump_read(const char *url);
 void svndump_deinit(void);
 void svndump_reset(void);
