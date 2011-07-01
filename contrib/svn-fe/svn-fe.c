1b5da101ef5900976313f0dc98c451da1b65882f
Dmitry Ivankov <divanorama@gmail.com>
2011-07-01 21:13:09 +0600

    svn-fe,test-svn-fe: use parse-options
    There was custom options parsing. As more options arise it will
    be easier to add and document new options with parse-options api.
    
    Use parse-options api in svn-fe and test-svn-fe. This implies adding
    help messages. And by the way clarify the "url" parameter meaning,
    renaming it to git-svn-id-url and updating svn-fe.txt. Also allow a
    --git-svn-id-url=url way of specifying it.
    
    $ svn-fe --git-svn-id-url=url
    does the same thing as
    $ svn-fe url
    i.e., url is used to generate git-svn-id: lines, if url is set.
    
    Signed-off-by: Dmitry Ivankov <divanorama@gmail.com>
    Reviewed-by: Jonathan Nieder <jrnieder@gmail.com>

--- contrib/svn-fe/svn-fe.c
+++ contrib/svn-fe/svn-fe.c
@@ -3,14 +3,40 @@
  * You may freely use, modify, distribute, and relicense it.
  */
 
-#include <stdlib.h>
+#include "git-compat-util.h"
+#include "parse-options.h"
 #include "svndump.h"
 
-int main(int argc, char **argv)
+static const char * const svn_fe_usage[] = {
+	"svn-fe [options] [git-svn-id-url] < dump | fast-import-backend",
+	NULL
+};
+
+static const char *url;
+
+static struct option svn_fe_options[] = {
+	OPT_STRING(0, "git-svn-id-url", &url, "url",
+		"add git-svn-id line to log messages, imitating git-svn"),
+	OPT_END()
+};
+
+int main(int argc, const char **argv)
 {
+	argc = parse_options(argc, argv, NULL, svn_fe_options,
+						svn_fe_usage, 0);
+	if (argc > 1)
+		usage_with_options(svn_fe_usage, svn_fe_options);
+
+	if (argc == 1) {
+		if (url)
+			usage_msg_opt("git-svn-id-url is set twice: as a "
+					"--parameter and as a [parameter]",
+					svn_fe_usage, svn_fe_options);
+		url = argv[0];
+	}
 	if (svndump_init(NULL))
 		return 1;
-	svndump_read((argc > 1) ? argv[1] : NULL);
+	svndump_read(url);
 	svndump_deinit();
 	svndump_reset();
 	return 0;
