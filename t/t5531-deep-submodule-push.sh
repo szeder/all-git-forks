ad7773ea46264e48dd5f40ea07897d20cb00ad06
Heiko Voigt <hvoigt@hvoigt.net>
2011-07-08 09:00:16 +0200

    test whether push checks for unpushed remotes in submodules
    These tests are used to extend push to check whether all recorded
    submodule commits have also been pushed.
    
    Signed-off-by: Heiko Voigt <hvoigt@hvoigt.net>

--- t/t5531-deep-submodule-push.sh
+++ t/t5531-deep-submodule-push.sh
@@ -32,4 +32,48 @@ test_expect_success push '
 	)
 '
 
+test_expect_failure 'push if submodule has no remote' '
+	(
+		cd work/gar/bage &&
+		>junk2 &&
+		git add junk2 &&
+		git commit -m "Second junk"
+	) &&
+	(
+		cd work &&
+		git add gar/bage &&
+		git commit -m "Second commit for gar/bage" &&
+		git push --recurse-submodule=check ../pub.git master
+	)
+'
+
+test_expect_failure 'push fails if submodule commit not on remote' '
+	(
+		cd work/gar &&
+		git clone --bare bage ../../submodule.git &&
+		cd bage &&
+		git remote add origin ../../../submodule.git &&
+		git fetch &&
+		>junk3 &&
+		git add junk3 &&
+		git commit -m "Third junk"
+	) &&
+	(
+		cd work &&
+		git add gar/bage &&
+		git commit -m "Third commit for gar/bage" &&
+		test_must_fail git push ../pub.git master
+	)
+'
+
+test_expect_failure 'push succeeds after commit was pushed to remote' '
+	(
+		cd work/gar/bage &&
+		git push origin master
+	) &&
+	(
+		cd work &&
+		git push --recurse-submodule=check ../pub.git master
+	)
+'
 test_done
