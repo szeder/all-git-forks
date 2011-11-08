2f3fb0d5afcb459aab42ad70112fbc4a9556129b
Junio C Hamano <gitster@pobox.com>
2011-11-07 16:29:34 -0800

    merge: force edit mode when merging a tag object
    Now that we allow pulling a tag from the remote site to validate the
    authenticity, we should give the user the final chance to verify and
    edit the merge message.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/merge.c
+++ builtin/merge.c
@@ -1253,6 +1253,10 @@ int cmd_merge(int argc, const char **argv, const char *prefix)
 			    sha1_to_hex(commit->object.sha1));
 		setenv(buf.buf, argv[i], 1);
 		strbuf_reset(&buf);
+		if (merge_remote_util(commit) &&
+		    merge_remote_util(commit)->obj &&
+		    merge_remote_util(commit)->obj->type == OBJ_TAG)
+			option_edit = 1;
 	}
 
 	if (!use_strategies) {
