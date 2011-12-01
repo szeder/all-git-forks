a2903a27a248ba76eba7dfc8d7c2649735197663
Chris West (Faux) <git@goeswhere.com>
2011-12-01 00:05:37 +0000

    Build without HASH defined

--- sha1-sph-compat.h
+++ sha1-sph-compat.h
@@ -1,6 +1,8 @@
 #ifndef SHA1_SPH_COMPAT_H
 #define SHA1_SPH_COMPAT_H
 
+#define HASH_OCTETS 20
+
 #define git_HASH_CTX	git_SHA_CTX
 #define git_HASH_Init	git_SHA1_Init
 #define git_HASH_Update	git_SHA1_Update
