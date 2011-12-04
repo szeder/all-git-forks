8274cc835f33eafdc7842fb2574388626114a250
Sven Strickroth <email@cs-ware.de>
2011-12-04 13:45:39 +0100

    make --follow work with gitdll.dll
    Related to issue #337 and issue #678.
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- log-tree.c
+++ log-tree.c
@@ -874,13 +874,11 @@ int log_tree_commit(struct rev_info *opt, struct commit *commit)
 	shown = log_tree_diff(opt, commit, &log);
 	if (!shown && opt->loginfo && opt->always_show_header) {
 		log.parent = NULL;
-		show_log(opt);
 		shown = 1;
 	}
 	if (opt->track_linear && !opt->linear && opt->reverse_output_stage)
 		fprintf(opt->diffopt.file, "\n%s\n", opt->break_bar);
 	opt->loginfo = NULL;
-	maybe_flush_or_die(opt->diffopt.file, "stdout");
 	if (close_file)
 		fclose(opt->diffopt.file);
 	return shown;
