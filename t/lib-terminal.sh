532cec4cee0c7c789de4cc6339906b68a6756f8e
Thomas Rast <trast@student.ethz.ch>
2011-12-12 19:09:15 +0100

    t/lib-terminal: test test-terminal's sanity
    The previous two commits show that getting test-terminal.perl right is
    not trivial.  Let lib-terminal.sh run a simple test that ensures it
    actually opens TTYs for std{in,out,err} and that it does not let stdin
    pass through.
    
    Signed-off-by: Thomas Rast <trast@student.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/lib-terminal.sh
+++ t/lib-terminal.sh
@@ -17,3 +17,22 @@ test_expect_success 'set up terminal for tests' '
 		}
 	fi
 '
+
+cat >expect1 <<EOF
+stdin: 1
+stdout: 1
+stderr: 1
+EOF
+: >expect2
+
+test_expect_success TTY 'test-terminal.perl is sane' '
+	test_terminal perl -e "
+		use POSIX qw(isatty);
+		print \"stdin: \", isatty(STDIN), \"\\n\";
+		print \"stdout: \", isatty(STDOUT), \"\\n\";
+		print \"stderr: \", isatty(STDERR), \"\\n\";
+	" >actual1 &&
+	test_cmp expect1 actual1 &&
+	echo foo | test_terminal cat - >actual2 &&
+	test_cmp expect2 actual2
+'
