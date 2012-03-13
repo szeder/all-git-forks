301cb191d731fcc1621151f056461cd3935d0e08
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2012-03-13 19:09:13 +0700

    Add column layout skeleton and git-column
    A column option string consists of many token separated by either
    a space or a  comma. A token belongs to one of three groups:
    
     - enabling: always, never and auto
     - layout mode: currently plain (which does not layout at all)
     - other future tuning flags
    
    git-column can be used to pipe output to from a command that wants
    column layout, but not to mess with its own output code. Simpler output
    code can be changed to use column layout code directly.
    
    Thanks-to: Ramsay Jones <ramsay@ramsay1.demon.co.uk>
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ column.h
@@ -0,0 +1,35 @@
+#ifndef COLUMN_H
+#define COLUMN_H
+
+#define COL_LAYOUT_MASK   0x000F
+#define COL_ENABLE_MASK   0x0030   /* always, never or auto */
+#define COL_PARSEOPT      0x0040   /* --column is given from cmdline */
+
+#define COL_ENABLE(c) ((c) & COL_ENABLE_MASK)
+#define COL_DISABLED      0x0000   /* must be zero */
+#define COL_ENABLED       0x0010
+#define COL_AUTO          0x0020
+
+#define COL_LAYOUT(c) ((c) & COL_LAYOUT_MASK)
+#define COL_PLAIN             15   /* one column */
+
+#define explicitly_enable_column(c) \
+	(((c) & COL_PARSEOPT) && COL_ENABLE(c) == COL_ENABLED)
+
+struct column_options {
+	int width;
+	int padding;
+	const char *indent;
+	const char *nl;
+};
+
+struct option;
+extern int parseopt_column_callback(const struct option *, const char *, int);
+extern int git_column_config(const char *var, const char *value,
+			     const char *command, unsigned int *colopts);
+extern int finalize_colopts(unsigned int *colopts, int stdout_is_tty);
+
+extern void print_columns(const struct string_list *list, unsigned int colopts,
+			  const struct column_options *opts);
+
+#endif
