58cc99cc32cdf6ecbed9eab3dd9c88883fd5453f
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2012-03-14 15:18:23 +0100

    t0303: immediately bail out w/o GIT_TEST_CREDENTIAL_HELPER
    t0300-credential-helpers.sh requires GIT_TEST_CREDENTIAL_HELPER to be
    configured to do something sensible. If it is not set, prove will say:
      ./t0303-credential-external.sh .. skipped: (no reason given)
    which isn't very nice.
    
    Use skip_all="..." && test_done to bail out immediately and provide a
    nicer message. In case GIT_TEST_CREDENTIAL_HELPER is set, but the
    timeout tests are skipped, mention GIT_TEST_CREDENTIAL_HELPER_TIMEOUT.
    
    Signed-off-by: Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0303-credential-external.sh
+++ t/t0303-credential-external.sh
@@ -4,36 +4,28 @@ test_description='external credential helper tests'
 . ./test-lib.sh
 . "$TEST_DIRECTORY"/lib-credential.sh
 
-pre_test() {
-	test -z "$GIT_TEST_CREDENTIAL_HELPER_SETUP" ||
-	eval "$GIT_TEST_CREDENTIAL_HELPER_SETUP"
-
-	# clean before the test in case there is cruft left
-	# over from a previous run that would impact results
-	helper_test_clean "$GIT_TEST_CREDENTIAL_HELPER"
-}
-
-post_test() {
-	# clean afterwards so that we are good citizens
-	# and don't leave cruft in the helper's storage, which
-	# might be long-term system storage
-	helper_test_clean "$GIT_TEST_CREDENTIAL_HELPER"
-}
-
 if test -z "$GIT_TEST_CREDENTIAL_HELPER"; then
-	say "# skipping external helper tests (set GIT_TEST_CREDENTIAL_HELPER)"
-else
-	pre_test
-	helper_test "$GIT_TEST_CREDENTIAL_HELPER"
-	post_test
+	skip_all="used to test external credential helpers"
+	test_done
 fi
+
+$GIT_TEST_CREDENTIAL_HELPER_SETUP
+
+# clean before the test in case there is cruft left
+# over from a previous run that would impact results
+helper_test_clean "$GIT_TEST_CREDENTIAL_HELPER"
+
+helper_test "$GIT_TEST_CREDENTIAL_HELPER"
 
 if test -z "$GIT_TEST_CREDENTIAL_HELPER_TIMEOUT"; then
-	say "# skipping external helper timeout tests"
+	say "# skipping timeout tests (GIT_TEST_CREDENTIAL_HELPER_TIMEOUT not set)"
 else
-	pre_test
 	helper_test_timeout "$GIT_TEST_CREDENTIAL_HELPER_TIMEOUT"
-	post_test
 fi
+
+# clean afterwards so that we are good citizens
+# and don't leave cruft in the helper's storage, which
+# might be long-term system storage
+helper_test_clean "$GIT_TEST_CREDENTIAL_HELPER"
 
 test_done
