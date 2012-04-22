d5d4ba98e06028b15a6d50275c2929108f5d11a1
Pete Wyckoff <pw@padd.com>
2012-04-22 16:30:58 -0400

    git-remote-testgit: fix race when spawning fast-import
    Test "pushing to local repo" in t5800-remote-helpers can hang
    due to a race condition in git-remote-testgit.  Fix it by
    setting stdin to unbuffered.
    
    On the writer side, "git push" invokes push_refs_with_export(),
    which sends to stdout the command "export\n" and immediately
    starts up "git fast-export".  The latter writes its output stream
    to the same stdout.
    
    On the reader side, remote helper "git-remote-testgit" reads from
    stdin to get its next command.  It uses getc() to read characters
    from libc up until \n.  Libc has buffered a potentially much
    larger chunk of stdin.  When it sees the "export\n" command, it
    forks "git fast-import" to read the stream.
    
    If fast-export finishes before git fast-import starts, the
    fast-export output can end up in libc's buffer in
    git-remote-testgit, rather than in git fast-import.  The latter
    hangs indefinitely on a now-empty stdin.
    
    Signed-off-by: Pete Wyckoff <pw@padd.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5800-remote-helpers.sh
+++ t/t5800-remote-helpers.sh
@@ -72,6 +72,20 @@ test_expect_success 'pushing to local repo' '
 	compare_refs localclone HEAD server HEAD
 '
 
+# Generally, skip this test.  It demonstrates a now-fixed
+# race in git-remote-testgit, but is too slow to leave in
+# for general use.
+test_expect_success DEBUG_TESTGIT_RACE 'racily pushing to local repo' '
+	cp -a server server2 &&
+	git clone "testgit::${PWD}/server2" localclone2 &&
+	test_when_finished "rm -rf server2 localclone2" &&
+	(cd localclone2 &&
+	echo content >>file &&
+	git commit -a -m three &&
+	GIT_REMOTE_TESTGIT_SLEEPY=2 git push) &&
+	compare_refs localclone2 HEAD server2 HEAD
+'
+
 test_expect_success 'synch with changes from localclone' '
 	(cd clone &&
 	 git pull)
