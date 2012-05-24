45cb7a1dd1deb7b25a81e175bf71e2206f87db1c
Simon Perrat <simon.perrat@gmail.com>
2012-05-24 17:34:16 +0200

    Added header for test script

--- t/tXXXX-git-mediawiki.sh
+++ t/tXXXX-git-mediawiki.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 #
-# Copyright (c) 2012
-#
+# Copyright (C) 2012
+#     Charles Roussel <charles.roussel@ensimag.imag.fr>
+#     Simon Cathebras <simon.cathebras@ensimag.imag.fr>
+#     Julien Khayat <julien.khayat@ensimag.imag.fr>
+#     Guillaume Sasdy <guillaume.sasdy@ensimag.imag.fr>
+#     Simon Perrat <simon.perrat@ensimag.imag.fr>
+# License:
+
+# tests for git-mediawiki
 
 test_description='Test the Git Mediawiki remote helper'
 
