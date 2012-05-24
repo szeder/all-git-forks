53edb418a8e1d9d02ccc3a5423056e7f4d9f8399
Simon Cathebras <simon.cathebras@ensimag.imag.fr>
2012-05-24 16:46:27 +0200

    Ajout du squelette de test git-mediawiki

---
+++ t/tXXXX-git-mediawiki.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Copyright (c) 2012
+#
+
+test_description='Test the Git Mediawiki remote helper'
+
+. ./test-lib.sh
+. ./test-gitmw-lib.sh
+
+test_expect_success 'git clone works on mediawiki::...' '
+	
+'
+
+test_done
