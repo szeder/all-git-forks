f08a474965259c76951257266df908d22db92c86
Charles Roussel <charles.roussel@ensimag.fr>
2012-05-28 15:33:25 +0200

    Correction of test functions

--- t/test-gitmw-lib.sh
+++ t/test-gitmw-lib.sh
@@ -19,16 +19,16 @@ wiki_getpage () {
 	$mw->{config}->{api_url} = $wikiurl;
 	if (!defined($mw->login( { lgname => "$username",
 		lgpassword => "$password" } ))) {
-		die "getpage : login failed";
+		die "getpage: login failed";
 	}
 	my $page = $mw->get_page( { title => $pagename } );
 	if (!defined($page)) {
-		die "getpage : wiki does not exist";
+		die "getpage: wiki does not exist";
 	}
 	my $content = $page->{'"'"'*'"'"'};
 
 	if (!defined($content)) {
-		die "getpage : page does not exist";
+		die "getpage: page does not exist";
 	}
 	$pagename=~s/\ /_/;
 	open(file, ">$destdir/$pagename.mw");
@@ -98,11 +98,11 @@ wiki_editpage (){
 		my $ref = $mw->get_page( { title => $wiki_page } );
 		$previous_text = $ref->{'"'"'*'"'"'};
 	}
-	my $text = $wiki_content;
+	my $text = "$wiki_content";
 	if (defined($previous_text)) {
 		$text="$previous_text\n\n$text";
 	}
-	$mw->edit( { action => '"'"'edit'"'"', title => $wiki_page, text => "$text"} );' "$1" "$2" "$3" "$url"
+	$mw->edit( { action => '"'"'edit'"'"', title => "$wiki_page", text => "$text"} );' "$1" "$2" "$3" "$url"
 }
 
 git_content (){
@@ -110,25 +110,7 @@ git_content (){
 #precondition : file1 and file2 must exist
 #behavior : if file_1 and file_2 do not match, the program exit with an error.
 
-	result=$(diff $1 $2)
-
-	if echo $result | grep -q ">" ; then
-		echo "test failed : file $1 and $2 do not match"
-		exit 1;
-	fi
-}
-
-git_exist (){
-#usage : git_exist rep_name file_name
-#behavior : if file_name is not present in rep_name 
-#or in his subdirectory, the program exit with an error
-	result=$(find $1 -type f -name $2)
-
-	if ! echo $result | grep -q $2; then
-		echo "test failed : file $1/$2 does not exist"
-		exit 1;
-	fi
-
+	result=$(diff -Bw $1 $2) || (echo "test failed: file $1 and $2 do not match" && exit 1)
 }
 
 wiki_page_content (){
@@ -137,15 +119,13 @@ wiki_page_content (){
 #Exit with error code 1 if and only if the content of
 #<page_name> and <file> do not match.
 
-	test -d $test_rep || mkdir $test_rep
-	wiki_getpage $2 $test_rep
+	wiki_getpage $2 .
 
-	if find $test_rep -name $2 -type f | grep -q $2; then
-		git_content.sh $1 $test_rep/$2
-		rm -rf $test_rep
+	if [ -f $2.mw ]
+	then
+		git_content $1 $2.mw
 	else
-		rm -rf $test_rep
-		echo "ERROR : file $2 not found on wiki"
+		echo "ERROR: file $2 not found on wiki"
 		exit 1;
 	fi
 }
@@ -154,16 +134,14 @@ wiki_page_exist (){
 #usage wiki_page_exist <page_name>
 #Exit with error code 1 if and only if the page <page_name> is not on wiki
 
+	wiki_getpage $1 .
 
-	test -d $test_rep || mkdir $test_rep
-	wiki_getpage $1 $test_rep
-
-	if find $test_rep -name $1 -type f | grep -q $1; then
-		rm -rf $test_rep
-	else
-		rm -rf $test_rep
+	if [ ! -f $1.mw ] 
+	then
 		echo "ERROR : file $1 not found on wiki"
 		exit 1;
+	else 
+		rm $1.mw
 	fi
 }
 
