5f449fb87c3f53f10a07769de558038589014f5d
Simon Cathebras <simon.cathebras@ensimag.imag.fr>
2012-05-29 15:06:46 +0200

    update of skipping condition on tests of git clone for mediawiki

--- t/tXXXX-git-mediawiki.sh
+++ t/tXXXX-git-mediawiki.sh
@@ -12,9 +12,20 @@
 
 test_description='Test the Git Mediawiki remote helper'
 
-#. ./install_wiki.shUser
 . ./test-gitmw-lib.sh
 . ./test-lib.sh
+
+if ! test_have_prereq PERL
+then
+	skip_all='skipping gateway git-mw tests, perl not available'
+	test_done
+fi
+
+if [ ! -d "$WIKI_DIR_INST/$WIKI_DIR_NAME" ] ;
+then
+	skip_all='skipping gateway git-mw tests, no mediawiki found'
+	test_done
+fi
 
 
 test_expect_success 'git clone works with page added' '
