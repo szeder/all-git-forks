9da94be49145065735daa2cb9117742d5e51d2e5
Jaseem Abid <jaseemabid@gmail.com>
2012-06-03 22:33:20 +0530

    gitweb: Test files for the console
    These files are concatenated in order to generate test files for the
    console
    
    Signed-off-by: Jaseem Abid <jaseemabid@gmail.com>

---
+++ gitweb/test/spec/includes.js
@@ -0,0 +1,8 @@
+/*
+	Import all library files here for testing.
+	Expose functions to be tested to the global object.
+*/
+
+datetime = require('../gitweb/static/js/lib/datetime.js');
+timezoneOffset = datetime.timezoneOffset;
+
