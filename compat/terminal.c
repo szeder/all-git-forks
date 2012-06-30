4d7eed188c5dbe4365caee5aed4b6622e4d543fb
Erik Faye-Lund <kusmabite@gmail.com>
2012-06-30 18:22:25 +0200

    windows: reuse /dev/tty-version of git_terminal_prompt
    The getpass-implementation we use on Windows isn't at all ideal;
    it works in raw-mode (as opposed to cooked mode), and as a result
    does not deal correcly with deletion, arrow-keys etc.
    
    Instead, use cooked mode to read a line at the time, allowing the
    C run-time to process the input properly.
    
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>

--- compat/terminal.c
+++ compat/terminal.c
@@ -3,7 +3,12 @@
 #include "sigchain.h"
 #include "strbuf.h"
 
+#if defined(HAVE_DEV_TTY) || defined(WIN32)
+
 #ifdef HAVE_DEV_TTY
+
+#define INPUT_PATH "/dev/tty"
+#define OUTPUT_PATH "/dev/tty"
 
 static int term_fd = -1;
 static struct termios old_term;
@@ -46,17 +51,68 @@ error:
 	return -1;
 }
 
+#elif defined(WIN32)
+
+#define INPUT_PATH "CONIN$"
+#define OUTPUT_PATH "CONOUT$"
+#define FORCE_TEXT "t"
+
+static HANDLE hconin = INVALID_HANDLE_VALUE;
+static DWORD cmode;
+
+static BOOL WINAPI ctrl_c_handler(DWORD dwCtrlType)
+{
+	SetConsoleMode(hconin, cmode);
+	return FALSE;
+}
+
+static void restore_term(void)
+{
+	if (hconin == INVALID_HANDLE_VALUE)
+		return;
+
+	SetConsoleCtrlHandler(ctrl_c_handler, FALSE);
+	SetConsoleMode(hconin, cmode);
+	CloseHandle(hconin);
+	hconin = INVALID_HANDLE_VALUE;
+}
+
+static int disable_echo(void)
+{
+	hconin = CreateFile("CONIN$", GENERIC_READ | GENERIC_WRITE,
+	    FILE_SHARE_READ, NULL, OPEN_EXISTING,
+	    FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hconin == INVALID_HANDLE_VALUE)
+		return -1;
+
+	GetConsoleMode(hconin, &cmode);
+	if (!SetConsoleCtrlHandler(ctrl_c_handler, TRUE) ||
+	    !SetConsoleMode(hconin, cmode & (~ENABLE_ECHO_INPUT))) {
+		CloseHandle(hconin);
+		hconin = INVALID_HANDLE_VALUE;
+		return -1;
+	}
+
+	return 0;
+}
+
+#endif
+
+#ifndef FORCE_TEXT
+#define FORCE_TEXT
+#endif
+
 char *git_terminal_prompt(const char *prompt, int echo)
 {
 	static struct strbuf buf = STRBUF_INIT;
 	int r;
 	FILE *input_fh, *output_fh;
 
-	input_fh = fopen("/dev/tty", "r");
+	input_fh = fopen(INPUT_PATH, "r" FORCE_TEXT);
 	if (!input_fh)
 		return NULL;
 
-	output_fh = fopen("/dev/tty", "w");
+	output_fh = fopen(OUTPUT_PATH, "w" FORCE_TEXT);
 	if (!output_fh) {
 		fclose(input_fh);
 		return NULL;
