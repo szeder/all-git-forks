3de147a1c101d40cb3d4163cf341ea03671e5c42
Erik Faye-Lund <kusmabite@gmail.com>
2012-07-03 17:36:10 +0200

    clean: add a flag to back up cleaned files
    The combination of "git clean" and fat fingers can some times cause
    data-loss, which can be quite frustrating.
    
    So let's add a flag that imports the files to be deleted into the
    object-database, in a way similar to what git-stash does.
    
    Also maintain a reflog of the previously backed up clean-runs.
    
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>

--- builtin/clean.c
+++ builtin/clean.c
@@ -11,9 +11,14 @@
 #include "dir.h"
 #include "parse-options.h"
 #include "string-list.h"
+#include "tree-walk.h"
+#include "unpack-trees.h"
+#include "cache-tree.h"
+#include "refs.h"
 #include "quote.h"
 
 static int force = -1; /* unset */
+static int quiet, backup;
 
 static const char *const builtin_clean_usage[] = {
 	"git clean [-d] [-f] [-n] [-q] [-e <pattern>] [-x | -X] [--] <paths>...",
@@ -24,6 +29,8 @@ static int git_clean_config(const char *var, const char *value, void *cb)
 {
 	if (!strcmp(var, "clean.requireforce"))
 		force = !git_config_bool(var, value);
+	else if (!strcmp(var, "clean.backup"))
+		backup = !git_config_bool(var, value);
 	return git_default_config(var, value, cb);
 }
 
@@ -34,10 +41,125 @@ static int exclude_cb(const struct option *opt, const char *arg, int unset)
 	return 0;
 }
 
+static int backed_up_anything;
+
+static void backup_file(const char *path, struct stat *st)
+{
+	if (add_to_cache(path, st, 0))
+		die(_("updating files failed"));
+	backed_up_anything = 1;
+}
+
+static void backup_dir_recursively(struct strbuf *path, const char *prefix)
+{
+	DIR *dir;
+	struct dirent *e;
+	int original_len = path->len;
+
+	dir = opendir(path->buf);
+	if (!dir)
+		die_errno(_("opendir('%s') failed"), path->buf);
+
+	while ((e = readdir(dir)) != NULL) {
+		struct stat st;
+		if (is_dot_or_dotdot(e->d_name))
+			continue;
+
+		strbuf_addstr(path, e->d_name);
+		if (lstat(path->buf, &st))
+			; /* fall through */
+		else if (S_ISDIR(st.st_mode)) {
+			strbuf_addch(path, '/');
+			backup_dir_recursively(path, prefix);
+		} else if (S_ISREG(st.st_mode))
+			backup_file(path->buf, &st);
+		strbuf_setlen(path, original_len);
+	}
+
+	closedir(dir);
+}
+
+static struct commit_list *parents;
+
+static void prepare_backup(void)
+{
+	struct unpack_trees_options opts;
+	unsigned char sha1[20];
+	struct tree *tree;
+	struct commit *parent;
+	struct tree_desc t;
+
+	if (get_sha1("HEAD", sha1))
+		die(_("You do not have the initial commit yet"));
+
+	/* prepare parent-list */
+	parent = lookup_commit_or_die(sha1, "HEAD");
+	commit_list_insert(parent, &parents);
+
+	/* load HEAD into the index */
+
+	tree = parse_tree_indirect(sha1);
+	if (!tree)
+		die(_("Failed to unpack tree object %s"), sha1);
+
+	parse_tree(tree);
+	init_tree_desc(&t, tree->buffer, tree->size);
+
+	memset(&opts, 0, sizeof(opts));
+	opts.head_idx = -1;
+	opts.src_index = &the_index;
+	opts.dst_index = &the_index;
+	opts.index_only = 1;
+
+	if (unpack_trees(1, &t, &opts)) {
+		/* We've already reported the error, finish dying */
+		exit(128);
+	}
+}
+
+static void finish_backup(void)
+{
+	const char *ref = "refs/clean-backup";
+	unsigned char commit_sha1[20];
+	struct strbuf msg = STRBUF_INIT;
+	char logfile[PATH_MAX];
+	struct stat st;
+
+	if (!backed_up_anything)
+		return;
+
+	if (!active_cache_tree)
+		active_cache_tree = cache_tree();
+
+	if (!cache_tree_fully_valid(active_cache_tree)) {
+		if (cache_tree_update(active_cache_tree,
+		    active_cache, active_nr, 0) < 0)
+			die("failed to update cache");
+	}
+
+	strbuf_addstr(&msg, "Automatically committed by git-clean");
+
+	/* create a reflog, if there isn't one */
+	git_snpath(logfile, sizeof(logfile), "logs/%s", ref);
+	if (stat(logfile, &st)) {
+		FILE *fp = fopen(logfile, "w");
+		if (!fp)
+			warning(_("Can not do reflog for '%s'\n"), ref);
+		else
+			fclose(fp);
+	}
+
+	if (commit_tree(&msg, active_cache_tree->sha1, parents, commit_sha1,
+	    NULL, NULL))
+		die("failed to commit :(");
+
+	update_ref(msg.buf, ref, commit_sha1, NULL, 0, DIE_ON_ERR);
+}
+
 int cmd_clean(int argc, const char **argv, const char *prefix)
 {
 	int i;
-	int show_only = 0, remove_directories = 0, quiet = 0, ignored = 0;
+	int show_only = 0, remove_directories = 0, ignored = 0;
 	int ignored_only = 0, config_set = 0, errors = 0;
 	int rm_flags = REMOVE_DIR_KEEP_NESTED_GIT;
 	struct strbuf directory = STRBUF_INIT;
@@ -53,6 +175,8 @@ int cmd_clean(int argc, const char **argv, const char *prefix)
 		OPT__FORCE(&force, "force"),
 		OPT_BOOLEAN('d', NULL, &remove_directories,
 				"remove whole directories"),
+		OPT_BOOLEAN('b', "backup", &backup, "store blobs in "
+		  "object-database before deleting them"),
 		{ OPTION_CALLBACK, 'e', "exclude", &exclude_list, "pattern",
 		  "add <pattern> to ignore rules", PARSE_OPT_NONEG, exclude_cb },
 		OPT_BOOLEAN('x', NULL, &ignored, "remove ignored files, too"),
@@ -96,6 +220,9 @@ int cmd_clean(int argc, const char **argv, const char *prefix)
 
 	if (!ignored)
 		setup_standard_excludes(&dir);
+
+	if (backup && !show_only)
+		prepare_backup();
 
 	for (i = 0; i < exclude_list.nr; i++)
 		add_exclude(exclude_list.items[i].string, "", 0,
@@ -155,6 +282,9 @@ int cmd_clean(int argc, const char **argv, const char *prefix)
 				printf(_("Would remove %s\n"), qname);
 			} else if (remove_directories ||
 				   (matches == MATCHED_EXACTLY)) {
+				if (backup)
+					backup_dir_recursively(&directory,
+					    prefix);
 				if (!quiet)
 					printf(_("Removing %s\n"), qname);
 				if (remove_dir_recursively(&directory,
@@ -175,7 +305,11 @@ int cmd_clean(int argc, const char **argv, const char *prefix)
 			if (show_only) {
 				printf(_("Would remove %s\n"), qname);
 				continue;
-			} else if (!quiet) {
+			}
+			if (backup) {
+				backup_file(ent->name, &st);
+			}
+			if (!quiet) {
 				printf(_("Removing %s\n"), qname);
 			}
 			if (unlink(ent->name) != 0) {
@@ -188,5 +322,9 @@ int cmd_clean(int argc, const char **argv, const char *prefix)
 
 	strbuf_release(&directory);
 	string_list_clear(&exclude_list, 0);
+
+	if (backup && !show_only)
+		finish_backup();
+
 	return (errors != 0);
 }
