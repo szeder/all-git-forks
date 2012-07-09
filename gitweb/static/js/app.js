4eb5439479db5ee88c78df812cda3c221269a2e0
Jaseem Abid <jaseemabid@gmail.com>
2012-07-09 16:11:03 +0530

    gitweb:  UI for converting the time
    Signed-off-by: Jaseem Abid <jaseemabid@gmail.com>

--- gitweb/static/js/app.js
+++ gitweb/static/js/app.js
@@ -1,12 +1,65 @@
 /*!
-  * Code to be excecuted on page load
-  * Author : Jaseem Abid <jaseemabid@gmail.com>
-*/
+ * Code to be excecuted on page load
+ * Author : Jaseem Abid <jaseemabid@gmail.com>
+ */
 
-(function($){
+(function ($, gitweb) {
+	"use strict";
 
-	$(document).ready(function(){
+	$(document).ready(function () {
+		var popup,
+			select,
+			oldTime,
+			tz,
+			op,
+			timeZones =  gitweb.timeZones;
 
+		/* Create TZ selector from timeZones */
+		for (tz in timeZones) {
+			if (timeZones.hasOwnProperty(tz)) {
+				/* <option value="utc">UTC/GMT</option> */
+				op = ['<option value="', tz, '">', timeZones[tz], '</option>'];
+				select += op.join('');
+			}
+		}
+
+		select = ['<select name="tzoffset">', select, '</select>'].join('');
+
+		/* Create the popup */
+		popup = $("<div>")
+			.addClass("popup")
+			.append(
+				$("<div>")
+					.addClass("close-button")
+					.attr("title", "Click to close")
+					.html("X")
+			)
+			.append('<span>Select timezone:</span><br clear="all" />')
+			.append(select);
+
+		/* Insert the popup on click */
+		$('span.datetime').click(function () {
+			$(this)
+				.css({'position': 'relative',
+					  'display': 'inline-block'});
+			oldTime = $(this).text();
+			$(this).append(popup);
+		});
+
+		/* Close button on popup */
+		$("body").on("click", "div.close-button", function () {
+			$(this).parent().remove();
+		});
+
+		/* Trigger event on timezone selection */
+		$("body").on("change", "select", function () {
+			var span = $(this).parent().parent('span.datetime'),
+				newTz = $(this).val(),
+				newTime = new gitweb.DateView(oldTime).convertTo(newTz).dateView;
+			// Remove the popup after val selection
+			$(this).parent().remove();
+			// Update with new time
+			span.text(newTime);
+		});
 	});
-
-})(jQuery);
+}(jQuery, window.gitweb));
