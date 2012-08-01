1c94e33a9404f2dcf798d6f247b50d0e50a213a9
Thomas Gummerer <t.gummerer@gmail.com>
2012-08-01 20:46:31 +0200

    read-cache: read resolve-undo data
    Make git read the resolve-undo data from the index.
    
    Since the resolve-undo data is joined with the conflicts in
    the ondisk format of the index file version 5, conflicts and
    resolved data is read at the same time, and the resolve-undo
    data is then converted to the in-memory format.
    
    Helped-by: Thomas Rast <trast@student.ethz.ch>
    Signed-off-by: Thomas Gummerer <t.gummerer@gmail.com>

--- read-cache-v5.c
+++ read-cache-v5.c
@@ -1,5 +1,6 @@
 #include "cache.h"
 #include "read-cache.h"
+#include "string-list.h"
 #include "resolve-undo.h"
 #include "cache-tree.h"
 #include "dir.h"
@@ -308,6 +309,31 @@ static int read_entry(struct cache_entry **ce, char *pathname, size_t pathlen,
 	return 0;
 }
 
+static struct conflict_part *conflict_part_from_ondisk(struct ondisk_conflict_part *ondisk)
+{
+	struct conflict_part *cp = xmalloc(sizeof(struct conflict_part));
+
+	cp->flags      = ntoh_s(ondisk->flags);
+	cp->entry_mode = ntoh_s(ondisk->entry_mode);
+	hashcpy(cp->sha1, ondisk->sha1);
+	return cp;
+}
+
+static void conflict_entry_push(struct conflict_entry **head,
+				struct conflict_entry **tail,
+				struct conflict_entry *conflict_entry)
+{
+	if (!*head) {
+		*head = *tail = conflict_entry;
+		(*tail)->next = NULL;
+		return;
+	}
+
+	(*tail)->next = conflict_entry;
+	conflict_entry->next = NULL;
+	*tail = (*tail)->next;
+}
+
 struct conflict_entry *create_new_conflict(char *name, int len, int pathlen)
 {
 	struct conflict_entry *conflict_entry;
@@ -343,6 +369,106 @@ void add_part_to_conflict_entry(struct directory_entry *de,
 			conflict_search = conflict_search->next;
 		conflict_search->next = conflict_part;
 	}
+}
+
+static int read_conflicts(struct conflict_entry **head,
+			  struct directory_entry *de,
+			  void *mmap, unsigned long mmap_size)
+{
+	struct conflict_entry *tail;
+	unsigned int croffset, i;
+	char *full_name;
+
+	croffset = de->de_cr;
+	tail = NULL;
+	for (i = 0; i < de->de_ncr; i++) {
+		struct conflict_entry *conflict_new;
+		unsigned int len, *nfileconflicts;
+		char *name;
+		void *crc_start;
+		int k, offset;
+		uint32_t *filecrc;
+
+		offset = croffset;
+		crc_start = ptr_add(mmap, offset);
+		name = ptr_add(mmap, offset);
+		len = strlen(name);
+		offset += len + 1;
+		nfileconflicts = ptr_add(mmap, offset);
+		offset += 4;
+
+		full_name = xmalloc(sizeof(char) * (len + de->de_pathlen));
+		memcpy(full_name, de->pathname, de->de_pathlen);
+		memcpy(full_name + de->de_pathlen, name, len);
+		conflict_new = create_new_conflict(full_name,
+				len + de->de_pathlen, de->de_pathlen);
+		for (k = 0; k < ntoh_l(*nfileconflicts); k++) {
+			struct ondisk_conflict_part *ondisk;
+			struct conflict_part *cp;
+
+			ondisk = ptr_add(mmap, offset);
+			cp = conflict_part_from_ondisk(ondisk);
+			cp->next = NULL;
+			add_part_to_conflict_entry(de, conflict_new, cp);
+			offset += sizeof(struct ondisk_conflict_part);
+		}
+		filecrc = ptr_add(mmap, offset);
+		free(full_name);
+		if (!check_crc32(0, crc_start,
+			len + 1 + 4 + conflict_new->nfileconflicts
+			* sizeof(struct ondisk_conflict_part),
+			ntoh_l(*filecrc)))
+			return -1;
+		croffset = offset + 4;
+		conflict_entry_push(head, &tail, conflict_new);
+	}
+	return 0;
+}
+
+static int convert_resolve_undo(struct index_state *istate,
+				struct directory_entry *de,
+				void *mmap, unsigned long mmap_size)
+{
+	int i;
+	struct conflict_entry *conflicts = NULL;
+
+	if (read_conflicts(&conflicts, de, mmap, mmap_size) < 0)
+		return -1;
+
+	while (conflicts) {
+		struct string_list_item *lost;
+		struct resolve_undo_info *ui;
+		struct conflict_part *cp;
+
+		if (conflicts->entries &&
+		    (conflicts->entries->flags & CONFLICT_CONFLICTED)) {
+			conflicts = conflicts->next;
+			continue;
+		}
+		if (!istate->resolve_undo) {
+			istate->resolve_undo = xcalloc(1, sizeof(struct string_list));
+			istate->resolve_undo->strdup_strings = 1;
+		}
+
+		lost = string_list_insert(istate->resolve_undo, conflicts->name);
+		if (!lost->util)
+			lost->util = xcalloc(1, sizeof(*ui));
+		ui = lost->util;
+
+		cp = conflicts->entries;
+		for (i = 0; i < 3; i++)
+			ui->mode[i] = 0;
+		while (cp) {
+			ui->mode[conflict_stage(cp) - 1] = cp->entry_mode;
+			hashcpy(ui->sha1[conflict_stage(cp) - 1], cp->sha1);
+			cp = cp->next;
+		}
+		conflicts = conflicts->next;
+	}
+	for (i = 0; i < de->de_nsubtrees; i++)
+		if (convert_resolve_undo(istate, de->sub[i], mmap, mmap_size) < 0)
+			return -1;
+	return 0;
 }
 
 static int read_entries(struct index_state *istate, struct directory_entry *de,
@@ -443,6 +569,10 @@ static int read_index_v5(struct index_state *istate, void *mmap,
 
 	if (!need_root)
 		parse_pathspec(&adjusted_pathspec, PATHSPEC_ALL_MAGIC, PATHSPEC_PREFER_CWD, NULL, paths);
+
+	if (!opts || opts->read_resolve_undo)
+		if (convert_resolve_undo(istate, root_directory, mmap, mmap_size) < 0)
+			return -1;
 
 	de = root_directory;
 	last_de = de;
