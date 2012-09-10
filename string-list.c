920890444c95b895f9802928a27bf71e554e28c2
Michael Haggerty <mhagger@alum.mit.edu>
2012-09-10 23:18:42 +0200

    string_list: add two new functions for splitting strings
    Add two new functions, string_list_split() and
    string_list_split_in_place().  These split a string into a string_list
    on a separator character.  The first makes copies of the substrings
    (leaving the input string untouched) and the second splits the
    original string in place, overwriting the separator characters with
    NULs and referring to the original string's memory.
    
    These functions are similar to the strbuf_split_*() functions except
    that they work with the more powerful string_list interface.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- string-list.c
+++ string-list.c
@@ -204,3 +204,52 @@ void unsorted_string_list_delete_item(struct string_list *list, int i, int free_
 	list->items[i] = list->items[list->nr-1];
 	list->nr--;
 }
+
+int string_list_split(struct string_list *list, const char *string,
+		      int delim, int maxsplit)
+{
+	int count = 0;
+	const char *p = string, *end;
+
+	assert(list->strdup_strings);
+	for (;;) {
+		count++;
+		if (maxsplit >= 0 && count > maxsplit) {
+			string_list_append(list, p);
+			return count;
+		}
+		end = strchr(p, delim);
+		if (end) {
+			string_list_append_nodup(list, xmemdupz(p, end - p));
+			p = end + 1;
+		} else {
+			string_list_append(list, p);
+			return count;
+		}
+	}
+}
+
+int string_list_split_in_place(struct string_list *list, char *string,
+			       int delim, int maxsplit)
+{
+	int count = 0;
+	char *p = string, *end;
+
+	assert(!list->strdup_strings);
+	for (;;) {
+		count++;
+		if (maxsplit >= 0 && count > maxsplit) {
+			string_list_append(list, p);
+			return count;
+		}
+		end = strchr(p, delim);
+		if (end) {
+			*end = '\0';
+			string_list_append(list, p);
+			p = end + 1;
+		} else {
+			string_list_append(list, p);
+			return count;
+		}
+	}
+}
