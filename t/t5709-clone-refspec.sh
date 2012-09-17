e84acb491a10ca4589266ac2daa3ea4b59b5b897
Junio C Hamano <gitster@pobox.com>
2012-09-16 21:48:34 -0700

    fixup! check what we really care about in a more direct way
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5709-clone-refspec.sh
+++ t/t5709-clone-refspec.sh
@@ -4,6 +4,7 @@ test_description='test refspec written by clone-command'
 . ./test-lib.sh
 
 test_expect_success 'setup' '
+	# Make two branches, "master" and "side"
 	echo one >file &&
 	git add file &&
 	git commit -m one &&
@@ -12,48 +13,96 @@ test_expect_success 'setup' '
 	git tag two &&
 	echo three >file &&
 	git commit -a -m three &&
-	git checkout -b foo &&
+	git checkout -b side &&
 	echo four >file &&
 	git commit -a -m four &&
-	git checkout master
-'
-
-test_expect_success 'refspec contains all branches by default' '
-	git clone "file://$PWD" dir_all &&
-	echo "+refs/heads/*:refs/remotes/origin/*" > expected &&
-	git --git-dir=dir_all/.git config --get remote.origin.fetch > actual &&
-	test_cmp expected actual
-'
-
-test_expect_success 'refspec contains only master with option --single-branch and remotes HEAD point to master' '
-	git clone --single-branch "file://$PWD" dir_master &&
-	echo "+refs/heads/master:refs/remotes/origin/master" > expected &&
-	git --git-dir=dir_master/.git config --get remote.origin.fetch > actual &&
-	test_cmp expected actual
-'
-
-test_expect_success 'refspec contains only foo with option --single-branch and remotes HEAD point to foo' '
-	git checkout foo &&
-	git clone --single-branch "file://$PWD" dir_foo &&
-	echo "+refs/heads/foo:refs/remotes/origin/foo" > expected &&
-	git --git-dir=dir_foo/.git config --get remote.origin.fetch > actual &&
-	test_cmp expected actual
-'
-
-test_expect_success 'refspec contains one branch after using option --single-branch with --branch' '
 	git checkout master &&
-	git clone --single-branch --branch foo "file://$PWD" dir_foo2 &&
-	echo "+refs/heads/foo:refs/remotes/origin/foo" > expected &&
-	git --git-dir=dir_foo2/.git config --get remote.origin.fetch > actual &&
-	test_cmp expected actual
+
+	# default clone
+	git clone . dir_all &&
+
+	# default --single that follows HEAD=master
+	git clone --single-branch . dir_master &&
+
+	# default --single that follows HEAD=side
+	git checkout side &&
+	git clone --single-branch . dir_side &&
+
+	# explicit --single that follows side
+	git checkout master &&
+	git clone --single-branch --branch side . dir_side2 &&
+
+	# --single that does not know what branch to follow
+	git checkout two^ &&
+	git clone --single-branch . dir_detached &&
+
+	# advance both "master" and "side" branches
+	git checkout side &&
+	echo five >file &&
+	git commit -a -m five &&
+	git checkout master &&
+	echo six >file &&
+	git commit -a -m six
 '
 
-test_expect_success 'no refspec is written if remotes HEAD is detached' '
-	git checkout two^ &&
-	git clone --single-branch "file://$PWD" dir_detached &&
-	rm expected && touch expected &&
-	git --git-dir=dir_detached/.git config --get remote.origin.fetch > actual
-	test_cmp expected actual
+test_expect_success 'by default all branches will be kept updated' '
+	(
+		cd dir_all && git fetch &&
+		git for-each-ref refs/remotes/origin |
+		sed -e "/HEAD$/d" \
+		    -e "s|/remotes/origin/|/heads/|" >../actual
+	) &&
+	# follow both master and side
+	git for-each-ref refs/heads >expect &&
+	test_cmp expect actual
+'
+
+test_expect_success '--single-branch while HEAD pointing at master' '
+	(
+		cd dir_master && git fetch &&
+		git for-each-ref refs/remotes/origin |
+		sed -e "/HEAD$/d" \
+		    -e "s|/remotes/origin/|/heads/|" >../actual
+	) &&
+	# only follow master
+	git for-each-ref refs/heads/master >expect &&
+	test_cmp expect actual
+'
+
+test_expect_success '--single-branch while HEAD pointing at side' '
+	(
+		cd dir_side && git fetch &&
+		git for-each-ref refs/remotes/origin |
+		sed -e "/HEAD$/d" \
+		    -e "s|/remotes/origin/|/heads/|" >../actual
+	) &&
+	# only follow side
+	git for-each-ref refs/heads/side >expect &&
+	test_cmp expect actual
+'
+
+test_expect_success '--single-branch with explicit --branch side' '
+	(
+		cd dir_side2 && git fetch &&
+		git for-each-ref refs/remotes/origin |
+		sed -e "/HEAD$/d" \
+		    -e "s|/remotes/origin/|/heads/|" >../actual
+	) &&
+	# only follow side
+	git for-each-ref refs/heads/side >expect &&
+	test_cmp expect actual
+'
+
+test_expect_success '--single-branch with detached' '
+	(
+		cd dir_detached && git fetch &&
+		git for-each-ref refs/remotes/origin |
+		sed -e "/HEAD$/d" \
+		    -e "s|/remotes/origin/|/heads/|" >../actual
+	)
+	# nothing
+	>expect &&
+	test_cmp expect actual
 '
 
 test_done
