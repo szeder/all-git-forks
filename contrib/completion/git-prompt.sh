b365b419274d5478559ce61caa271be91edf29b1
Junio C Hamano <gitster@pobox.com>
2012-10-10 15:48:20 -0700

    fixup: correct misconversion in 67cf84a2
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/completion/git-prompt.sh
+++ contrib/completion/git-prompt.sh
@@ -220,7 +220,7 @@ __git_ps1 ()
 	#defaults/examples:
 	local ps1pc_start='\u@\h:\w '
 	local ps1pc_end='\$ '
-	local printf_format='(%s)'
+	local printf_format=' (%s)'
 
 	case "$#" in
 		2)	pcmode=yes
