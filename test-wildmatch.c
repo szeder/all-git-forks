a72906de8a82ae7ba68154f6c47c122825b922bd
Johannes Sixt <j6t@kdbg.org>
2012-10-12 09:27:50 +0200

    test-wildmatch: avoid exit code -1
    Our bash on Windows does not recognize -1 as failure.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- test-wildmatch.c
+++ test-wildmatch.c
@@ -4,11 +4,11 @@
 int main(int argc, char **argv)
 {
 	if (!strcmp(argv[1], "wildmatch"))
-		return wildmatch(argv[3], argv[2], 0);
+		return !!wildmatch(argv[3], argv[2], 0);
 	else if (!strcmp(argv[1], "iwildmatch"))
-		return wildmatch(argv[3], argv[2], FNM_CASEFOLD);
+		return !!wildmatch(argv[3], argv[2], FNM_CASEFOLD);
 	else if (!strcmp(argv[1], "fnmatch"))
-		return fnmatch(argv[3], argv[2], FNM_PATHNAME);
+		return !!fnmatch(argv[3], argv[2], FNM_PATHNAME);
 	else
 		return 1;
 }
