642828d42f3397dc866b43096993d9cbdf306924
Junio C Hamano <gitster@pobox.com>
2012-10-13 22:43:39 -0700

    t0003: run working tree test before chdir
    Anything below "setup bare" runs inside bare.git subdirectory,
    and adding "cd .." with more tests in higher level is not a
    good taste; it does not allow other topics to have a similar
    approach---merging them together will end up climbing one
    level too much and escaping the test playpen.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0003-attributes.sh
+++ t/t0003-attributes.sh
@@ -206,6 +206,16 @@ test_expect_success 'root subdir attribute test' '
 	attr_check subdir/a/i unspecified
 '
 
+test_expect_success 'negative patterns' '
+	echo "!f test=bar" >.gitattributes &&
+	test_must_fail git check-attr test -- f
+'
+
+test_expect_success 'patterns starting with exclamation' '
+	echo "\!f test=foo" >.gitattributes &&
+	attr_check "!f" foo
+'
+
 test_expect_success 'setup bare' '
 	git clone --bare . bare.git &&
 	cd bare.git
@@ -240,20 +250,6 @@ test_expect_success 'bare repository: test info/attributes' '
 	attr_check a/c/f f &&
 	attr_check a/i a/i &&
 	attr_check subdir/a/i unspecified
-'
-
-test_expect_success 'leave bare' '
-	cd ..
-'
-
-test_expect_success 'negative patterns' '
-	echo "!f test=bar" >.gitattributes &&
-	test_must_fail git check-attr test -- f
-'
-
-test_expect_success 'patterns starting with exclamation' '
-	echo "\!f test=foo" >.gitattributes &&
-	attr_check "!f" foo
 '
 
 test_done
