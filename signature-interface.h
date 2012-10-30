ebe6d128f981e8945c21bf72b033d0f05507a861
mtausig <mtausig@fsmat.at>
2012-10-30 12:07:38 +0100

    Made all members of signature_scheme const, created typedefs for function pointers

--- signature-interface.h
+++ signature-interface.h
@@ -1,12 +1,17 @@
 #ifndef SIGNATURE_INTERFACE_H
 #define SIGNATURE_INTERFACE_H
 
+typedef int (*sign_buffer_function)(struct strbuf*, struct strbuf*, const char*);
+typedef int (*verify_signed_buffer_function)(const char *, size_t, const char*, size_t, struct strbuf*);
+typedef  const char* (*get_signing_key_function)(void);
+
+
 struct signature_scheme {
   const char* sig_header;
-  int sig_header_len;
-  int (*sign_buffer)(struct strbuf*, struct strbuf*, const char*);
-  int (*verify_signed_buffer)(const char *, size_t, const char*, size_t, struct strbuf*);
-  const char* (*get_signing_key)(void);
+  const int sig_header_len;
+  const sign_buffer_function sign_buffer;
+  const verify_signed_buffer_function verify_signed_buffer;
+  const get_signing_key_function get_signing_key;
 };
 
 const char* parse_for_signature(char* line);
