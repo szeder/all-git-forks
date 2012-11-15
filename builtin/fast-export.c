8fb5e3f82e5735c1c2997048c6f8c60f3e079fe0
Antoine Pelisse <apelisse@gmail.com>
2012-11-15 11:02:51 +0100

    fast-export: Allow pruned-references in mark file
    fast-export can fail because of some pruned-reference when importing a
    mark file.
    
    The problem happens in the following scenario:
    
        $ git fast-export --export-marks=MARKS master
        (rewrite master)
        $ git prune
        $ git fast-export --import-marks=MARKS master
    
    This might fail if some references have been removed by prune
    because some marks will refer to non-existing commits.
    
    Let's warn when we have a mark for a commit we don't know.
    Also, increment the last_idnum before, so we don't override
    the mark.
    
    Signed-off-by: Antoine Pelisse <apelisse@gmail.com>

--- builtin/fast-export.c
+++ builtin/fast-export.c
@@ -607,16 +607,19 @@ static void import_marks(char *input_file)
 			|| *mark_end != ' ' || get_sha1(mark_end + 1, sha1))
 			die("corrupt mark line: %s", line);
 
+		if (last_idnum < mark)
+			last_idnum = mark;
+
 		object = parse_object(sha1);
-		if (!object)
-			die ("Could not read blob %s", sha1_to_hex(sha1));
+		if (!object) {
+			warning("Could not read blob %s", sha1_to_hex(sha1));
+			continue;
+		}
 
 		if (object->flags & SHOWN)
 			error("Object %s already has a mark", sha1_to_hex(sha1));
 
 		mark_object(object, mark);
-		if (last_idnum < mark)
-			last_idnum = mark;
 
 		object->flags |= SHOWN;
 	}
