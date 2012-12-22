c3e60a0e8d68ebf902bafa47093419e7d4b9b7fb
Heiko Voigt <hvoigt@hvoigt.net>
2012-12-22 01:15:54 +0100

    simplify reading file with revision into strbuf

--- test-read-sha1.c
+++ test-read-sha1.c
@@ -1,8 +1,8 @@
 #include "cache.h"
 #include "tree.h"
 
-int read_sha1_file_strbuf(struct strbuf *s, const unsigned char *sha1,
-		enum object_type *type)
+int read_sha1_strbuf(struct strbuf *s, const unsigned char *sha1,
+		     enum object_type *type)
 {
 	unsigned long size = 0;
 
@@ -10,68 +10,36 @@ int read_sha1_file_strbuf(struct strbuf *s, const unsigned char *sha1,
 	s->alloc = size;
 	s->len = size;
 
+	if (!s->buf)
+		return 0;
+
 	return size;
-}
-
-struct read_sha1_file_strbuf_data {
-	struct strbuf *s;
-	int ret;
-};
-
-static int read_file(const unsigned char *sha1, const char *base, int baselen,
-		const char *pathname, unsigned mode, int stage, void *context)
-{
-	struct read_sha1_file_strbuf_data *data = context;
-	enum object_type type;
-
-	read_sha1_file_strbuf(data->s, sha1, &type);
-
-	if (type != OBJ_BLOB)
-		return READ_TREE_RECURSIVE;
-
-	data->ret = 1;
-
-	return 2;
-}
-
-int read_tree_file_strbuf(struct strbuf *s, const unsigned char *sha1,
-			  const char *filename)
-{
-	struct read_sha1_file_strbuf_data data;
-	const char *file[] = {NULL, NULL};
-	int i;
-	struct tree *tree;
-	struct pathspec pathspec;
-
-	data.s = s;
-	data.ret = 0;
-
-	file[0] = filename;
-	init_pathspec(&pathspec, get_pathspec(NULL, file));
-	for (i = 0; i < pathspec.nr; i++)
-		pathspec.items[i].use_wildcard = 0;
-
-	tree = parse_tree_indirect(sha1);
-	if (!tree)
-		die("not a tree object");
-
-	read_tree_recursive(tree, "", 0, 0, &pathspec, read_file, &data);
-	return data.ret;
 }
 
 int main(int argc, char **argv)
 {
 	struct strbuf buf = STRBUF_INIT;
 	unsigned char sha1[20];
+	enum object_type type;
 	int ret;
 
-	if (argc < 3)
-		return 0;
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <revision:file>\n", argv[0]);
+		return 1;
+	}
 
-	if (get_sha1(argv[1], sha1))
-		return 0;
+	if (get_sha1(argv[1], sha1) < 0) {
+		fprintf(stderr, "Failed to find file: %s\n", argv[1]);
+		return 1;
+	}
 
-	ret = read_tree_file_strbuf(&buf, sha1, argv[2]);
+
+	ret = read_sha1_strbuf(&buf, sha1, &type);
+	if (!ret) {
+		fprintf(stderr, "Failed to read sha1: %s\n", sha1_to_hex(sha1));
+		return 1;
+	}
+
 	printf("Content: '%s'\n", buf.buf);
 	strbuf_release(&buf);
 
