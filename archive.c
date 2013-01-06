cc37e5bf18ca11d9a884bddfebcdff61df3e6279
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-01-06 13:21:08 +0700

    Convert more init_pathspec() to parse_pathspec()
    init_pathspec() was introduced to work with the result from
    get_pathspec(). init_pathspec() will be removed eventually after
    parse_pathspec() takes over, so that there is only place that
    initializes struct pathspec.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- archive.c
+++ archive.c
@@ -218,7 +218,7 @@ static int path_exists(struct tree *tree, const char *path)
 	struct pathspec pathspec;
 	int ret;
 
-	init_pathspec(&pathspec, paths);
+	parse_pathspec(&pathspec, 0, 0, "", paths);
 	ret = read_tree_recursive(tree, "", 0, 0, &pathspec, reject_entry, NULL);
 	free_pathspec(&pathspec);
 	return ret != 0;
