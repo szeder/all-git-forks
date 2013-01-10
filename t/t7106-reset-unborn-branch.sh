8ee0d59d1e0ebebea5d758d222a2e7ae41f06fd7
Junio C Hamano <gitster@pobox.com>
2013-01-10 15:32:15 -0800

    [SQUASH???] script portability fixes

--- t/t7106-reset-unborn-branch.sh
+++ t/t7106-reset-unborn-branch.sh
@@ -11,7 +11,7 @@ test_expect_success 'setup' '
 test_expect_success 'reset' '
 	git add a b &&
 	git reset &&
-	test "$(git ls-files)" == ""
+	test "$(git ls-files)" = ""
 '
 
 test_expect_success 'reset HEAD' '
@@ -24,28 +24,28 @@ test_expect_success 'reset $file' '
 	rm .git/index &&
 	git add a b &&
 	git reset a &&
-	test "$(git ls-files)" == "b"
+	test "$(git ls-files)" = "b"
 '
 
 test_expect_success 'reset -p' '
 	rm .git/index &&
 	git add a &&
 	echo y | git reset -p &&
-	test "$(git ls-files)" == ""
+	test "$(git ls-files)" = ""
 '
 
 test_expect_success 'reset --soft is a no-op' '
 	rm .git/index &&
 	git add a &&
 	git reset --soft
-	test "$(git ls-files)" == "a"
+	test "$(git ls-files)" = "a"
 '
 
 test_expect_success 'reset --hard' '
 	rm .git/index &&
 	git add a &&
 	git reset --hard &&
-	test "$(git ls-files)" == "" &&
+	test "$(git ls-files)" = "" &&
 	test_path_is_missing a
 '
 
