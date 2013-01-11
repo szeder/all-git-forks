809fbca5f89cd7104bff42cb92cecf2d1434c0e8
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-01-11 18:16:14 +0700

    Rename field "raw" to "_raw" in struct pathspec
    This patch is essentially no-op. It helps catching new use of this
    field though. This field is introduced as an intermediate step for the
    pathspec conversion and will be removed eventually. At this stage no
    more access sites should be introduced.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- pathspec.c
+++ pathspec.c
@@ -371,7 +371,7 @@ void parse_pathspec(struct pathspec *pathspec,
 		raw[0] = prefix;
 		raw[1] = NULL;
 		pathspec->nr = 1;
-		pathspec->raw = raw;
+		pathspec->_raw = raw;
 		return;
 	}
 
@@ -381,7 +381,7 @@ void parse_pathspec(struct pathspec *pathspec,
 
 	pathspec->nr = n;
 	pathspec->items = item = xmalloc(sizeof(*item) * n);
-	pathspec->raw = argv;
+	pathspec->_raw = argv;
 	prefixlen = prefix ? strlen(prefix) : 0;
 
 	for (i = 0; i < n; i++) {
@@ -442,7 +442,7 @@ const char **get_pathspec(const char *prefix, const char **pathspec)
 		       ~(PATHSPEC_FROMTOP | PATHSPEC_LITERAL),
 		       PATHSPEC_PREFER_CWD,
 		       prefix, pathspec);
-	return ps.raw;
+	return ps._raw;
 }
 
 void copy_pathspec(struct pathspec *dst, const struct pathspec *src)
