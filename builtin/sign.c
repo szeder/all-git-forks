4cc08962f211dd6f1e127423f18526fe0c8f4dd1
vjc <vjclasgens@gmail.com>
2013-01-30 19:33:26 -0800

    Added a dummy sign command

---
+++ builtin/sign.c
@@ -0,0 +1,15 @@
+//
+//  sign.c
+//  
+//
+//  Created by Vincent Clasgens on 1/30/13.
+//
+//
+
+#include <stdio.h>
+
+int cmd_sign(int argc, const char **argv, const char *prefix)
+{    
+    printf("This is the sign command\n");
+
+}
