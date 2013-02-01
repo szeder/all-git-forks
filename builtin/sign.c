c2c63fd4322f1f7477e7e8ed933a20e8a32033be
David Garcia <daveed.garcia18@gmail.com>
2013-01-31 22:18:59 -0800

    added comments and a little error check for the script execution

--- builtin/sign.c
+++ builtin/sign.c
@@ -3,13 +3,17 @@
 //
 //
 //  Created by Vincent Clasgens on 1/30/13.
-//
+//  University of Portland (Team Rogue)
+// 
 //
 
 #include <stdio.h>
+#define BASH_ERROR -1
 
 int cmd_sign(int argc, const char **argv, const char *prefix)
 {
+    
+    //prepare script to grab commit, sign, and add to notes
     char * bash_script =
         "HASH=$(git log -n1 | cut -d ' ' -f 2 | head -n1) \
         openssl smime -sign -signer mycert.pem -in \".git/objects/${HASH:0:2}/${HASH:2}\" > cryptoSig.txt \
@@ -17,5 +21,12 @@ int cmd_sign(int argc, const char **argv, const char *prefix)
         rm cryptoSig.txt \
         echo \"Pushing signed note to the origin\" \
         git push origin refs/notes/crypto/*";
-    system(bash_script);
+    
+    //execute commit using prepared commit
+    int bashResult = system(bash_script);
+    
+    if(bashResult == BASH_ERROR)
+        prinf("Error fetching signature, signing, adding to notes/n");
+    
+    
 }
