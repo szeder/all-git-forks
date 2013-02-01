f08b9d8c1dc4186ddaf31c8307d4f7ca88d65bc4
Dustin Dalen <dustin.dalen@gmail.com>
2013-01-31 21:49:34 -0800

    Integrated the script into the sign command. Currently this relies on the script being in the repo's root. We would need to hard char * the script into sign.c. But as this is a temporary solution this is acceptable

--- builtin/sign.c
+++ builtin/sign.c
@@ -1,6 +1,6 @@
 //
 //  sign.c
-//  
+//
 //
 //  Created by Vincent Clasgens on 1/30/13.
 //
@@ -9,7 +9,13 @@
 #include <stdio.h>
 
 int cmd_sign(int argc, const char **argv, const char *prefix)
-{    
-    printf("This is the sign command\n");
-
+{
+    char * bash_script =
+        "HASH=$(git log -n1 | cut -d ' ' -f 2 | head -n1) \
+        openssl smime -sign -signer mycert.pem -in \".git/objects/${HASH:0:2}/${HASH:2}\" > cryptoSig.txt \
+        git notes --ref=crypto add -F cryptoSig.txt HEAD \
+        rm cryptoSig.txt \
+        echo \"Pushing signed note to the origin\" \
+        git push origin refs/notes/crypto/*";
+    system(bash_script);
 }
--- getAndSignCommit.sh
+++ getAndSignCommit.sh
@@ -5,3 +5,5 @@ HASH=$(git log -n1 | cut -d ' ' -f 2 | head -n1)
 openssl smime -sign -signer mycert.pem -in ".git/objects/${HASH:0:2}/${HASH:2}" > cryptoSig.txt
 git notes --ref=crypto add -F cryptoSig.txt HEAD
 rm cryptoSig.txt
+echo "Pushing signed note to the origin"
+git push origin refs/notes/crypto/*
