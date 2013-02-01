2099e8104b32f477ed71bf926c714171fab75f03
Dustin Dalen <dustin.dalen@gmail.com>
2013-02-01 13:48:12 -0800

    Adding a reference to a script instead of a hard coded char*

--- builtin/sign.c
+++ builtin/sign.c
@@ -6,16 +6,9 @@
 //
 //
 
-#include <stdio.h>
+#include <stdlib.h>
 
 int cmd_sign(int argc, const char **argv, const char *prefix)
 {
-    char * bash_script =
-        "HASH=$(git log -n1 | cut -d ' ' -f 2 | head -n1) \
-        openssl smime -sign -signer mycert.pem -in \".git/objects/${HASH:0:2}/${HASH:2}\" > cryptoSig.txt \
-        git notes --ref=crypto add -F cryptoSig.txt HEAD \
-        rm cryptoSig.txt \
-        echo \"Pushing signed note to the origin\" \
-        git push origin refs/notes/crypto/*";
-    system(bash_script);
+    system("./getAndSignCommit.sh");
 }
