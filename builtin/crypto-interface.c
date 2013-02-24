1b8d35b2733ad727a58fddb9350a1d1ab3722f27
Dustin Dalen <dustin.dalen@gmail.com>
2013-02-24 15:30:27 -0800

    Expanding the builting options handling for the crypto-interface

--- builtin/crypto-interface.c
+++ builtin/crypto-interface.c
@@ -11,27 +11,51 @@
 #include "crypto-interface.h"
 #include "builtin.h"
 #include "run-command.h"
-#include "strbuf.h"
 #include "parse-options.h"
+
+// To be filled in later see other builtin/*.c files for examples
+static const char * const git_crypto_usage[] = {};
+
+
+static int sign(const char *ref)
+{
+    // Waiting to convert this to C to pass in our ref arg
+    //crypto_sign_buffer();
+    printf("Dummy C sign handler in builtin/crypto-interface\n");
+    return 0;
+}
+
+static int verify(int argc, const char **argv, const char *prefix)
+{
+    printf("Dummy C verify handler in builtin/crypto-interface");
+    // Waiting to conver the verify to C before passing our args
+    //crypto_verify_signed_buffer();
+    return 0;
+}
 
 int cmd_crypto(int argc, const char **argv, const char *prefix)
 {
-    // The list of our options which reference methods in
-    // /crypto-interface.c
-    static struct option builtin_crypto_options[] = {
+    int result;
+    const char *sign_arg = NULL;
+    const char *verify_arg = NULL;
+
+    // The list of our options which and instance variable
+    // crypto-interface.c
+    struct option options[] = {
         OPT_GROUP(N_("Crypto actions")),
-        OPT_STRING('s', "sign", &crypto_sign_buffer,
+        OPT_STRING('s', "sign", &sign_arg,
             N_("Sign"), N_("Sign the HEAD commit.")),
-        OPT_STRING('v', "verify", &crypto_verify_signed_buffer,
+        OPT_STRING('v', "verify", &verify_arg,
             N_("Verify"), N_("Verify the HEAD commit.")),
         OPT_END()
     };
 
-    // TEMPORARY
-    if(strcmp(argv[1], "-s") == 0){
-        crypto_sign_buffer();
-    }
-    else if(strcmp(argv[1], "-v") == 0){
-        crypto_verify_signed_buffer();
-    }
+    argc = parse_options(argc, argv, prefix, options,
+            git_crypto_usage, PARSE_OPT_STOP_AT_NON_OPTION);
+
+    if(!strcmp(argv[1], "sign") || sign_arg)
+        result = sign(sign_arg);
+    else if(verify_arg)
+        result = verify(argc, argv, prefix);
+    return result;
 }
