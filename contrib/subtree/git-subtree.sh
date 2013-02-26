f0fb12dcccf398d21f9b2290e0a0fd603e677347
Paul Campbell <pcampbell@carnegiecollege.ac.uk>
2013-02-26 14:45:05 +0000

    contrib/subtree: add link sub-command
    Adds subtree details to .gitsubtree where the directory already exists.
    
    Signed-off-by: Paul Campbell <pcampbell@carnegiecollege.ac.uk>

--- contrib/subtree/git-subtree.sh
+++ contrib/subtree/git-subtree.sh
@@ -11,6 +11,7 @@ OPTS_SPEC="\
 git subtree add    --prefix=<prefix> <commit>
 git subtree add    --prefix=<prefix> <repository> <commit>
 git subtree fetch  --prefix=<prefix> [<repository> <refspec...>]
+git subtree link   --prefix=<prefix> <repository> <refspec...>
 git subtree merge  --prefix=<prefix> <commit>
 git subtree pull   --prefix=<prefix> [<repository> <refspec...>]
 git subtree push   --prefix=<prefix> [<repository> <refspec...>]
@@ -103,7 +104,7 @@ done
 command="$1"
 shift
 case "$command" in
-	add|merge|pull|fetch) default= ;;
+	add|merge|pull|fetch|link) default= ;;
 	split|push|status) default="--default HEAD" ;;
 	*) die "Unknown command '$command'" ;;
 esac
@@ -122,7 +123,7 @@ esac
 dir="$(dirname "$prefix/.")"
 
 if [ "$command" != "pull" -a "$command" != "add" -a "$command" != "push" \
-	-a "$command" != "fetch" -a "$command" != "status" ]; then
+	-a "$command" != "fetch" -a "$command" != "status" -a "$command" != "link" ]; then
 	revs=$(git rev-parse $default --revs-only "$@") || exit $?
 	dirs="$(git rev-parse --no-revs --no-flags "$@")" || exit $?
 	if [ -n "$dirs" ]; then
@@ -542,6 +543,35 @@ cmd_add()
 	    die "'$2' does not refer to a commit"
 
 	    "cmd_add_repository" "$@"
+	else
+	    say "error: parameters were '$@'"
+	    die "Provide either a commit or a repository and commit."
+	fi
+}
+
+cmd_link()
+{
+	debug "Linking $dir..."
+	if test ! -e "$dir"
+	then
+		die "'$dir' doesn't exists.  Try adding instead."
+	fi
+
+	ensure_clean
+
+	add_subtree "$@"
+
+	if [ $# -eq 1 ]; then
+	    git rev-parse -q --verify "$1^{commit}" >/dev/null ||
+	    die "'$1' does not refer to a commit"
+	elif [ $# -eq 2 ]; then
+	    # Technically we could accept a refspec here but we're
+	    # just going to turn around and add FETCH_HEAD under the
+	    # specified directory.  Allowing a refspec might be
+	    # misleading because we won't do anything with any other
+	    # branches fetched via the refspec.
+	    git rev-parse -q --verify "$2^{commit}" >/dev/null ||
+	    die "'$2' does not refer to a commit"
 	else
 	    say "error: parameters were '$@'"
 	    die "Provide either a commit or a repository and commit."
