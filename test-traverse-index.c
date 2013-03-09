f9ab4f8984ee8184a4619d75c887387696633073
Clemens Buchacher <drizzd@aon.at>
2013-03-09 09:39:15 +0100

    create index with sample entry

--- test-traverse-index.c
+++ test-traverse-index.c
@@ -1,11 +1,63 @@
 #include "git-compat-util.h"
+#include "cache.h"
 
 static const char *usage_msg = "test-traverse-index";
 
+struct ce_sample {
+	unsigned int obj_nr;
+	const char *path;
+};
+
+#define DIV_CEIL(num, den) (((num)+(den)-1)/(den))
+
+static const unsigned char *sha1_from_obj_nr(unsigned int obj_nr)
+{
+	int i;
+	static unsigned char sha1[20];
+	unsigned int bitwidth = bitsizeof(sha1[0]);
+
+	for (i = 0; i < DIV_CEIL(sizeof(obj_nr), sizeof(sha1[0])); i++)
+		sha1[i] = (obj_nr >> (i * bitwidth)) & ~(1 << bitwidth);
+	for (; i < ARRAY_SIZE(sha1); i++)
+		sha1[i] = 0;
+
+	return sha1;
+}
+
+static struct cache_entry *create_cache_entry(const struct ce_sample *sample)
+{
+	int mode = 0100644;
+	struct cache_entry *ce;
+	int namelen = strlen(sample->path);
+	unsigned size = cache_entry_size(namelen);
+
+	ce = xcalloc(1, size);
+	hashcpy(ce->sha1, sha1_from_obj_nr(sample->obj_nr));
+	memcpy(ce->name, sample->path, namelen);
+	ce->ce_mode = create_ce_mode(mode);
+	ce->ce_flags = create_ce_flags(0);
+	ce->ce_namelen = namelen;
+
+	return ce;
+}
+
 int main(int argc, char **argv)
 {
+	struct cache_entry *ce;
+	struct index_state *index;
+	struct ce_sample sample = { 1, "a"};
+
 	if (argc > 1)
 		usage(usage_msg);
+
+	index = xcalloc(1, sizeof(*index));
+
+	ce = create_cache_entry(&sample);
+	if (add_index_entry(index, ce, ADD_CACHE_OK_TO_ADD) < 0)
+		die(_("unable to add cache entry: %s"), sample.path);
+
+	discard_index(index);
+	free(index);
 
 	return 0;
 }
