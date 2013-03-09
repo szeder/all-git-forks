80b0ccc373c7c22cccb97195e234eae49fcb79ae
bibendi <bibendi@bk.ru>
2013-03-09 18:31:37 +0000

    contrib/subtree: Add commands pull_all and push_all
    For each subtree listed in .gittrees perform a push or a pull.
    
    Signed-off-by: Paul Campbell <pcampbell@kemitix.net>
    
    Conflicts:
    	contrib/subtree/git-subtree.sh

--- contrib/subtree/git-subtree.sh
+++ contrib/subtree/git-subtree.sh
@@ -12,7 +12,9 @@ git subtree add   --prefix=<prefix> <commit>
 git subtree add   --prefix=<prefix> <repository> <commit>
 git subtree merge --prefix=<prefix> <commit>
 git subtree pull  --prefix=<prefix> [<repository> [<refspec>...]]
+git subtree pull_all
 git subtree push  --prefix=<prefix> [<repository> [<refspec>...]]
+git subtree pull_all
 git subtree split --prefix=<prefix> <commit...>
 git subtree from-submodule --prefix=<prefix>
 --
@@ -105,16 +107,18 @@ prefix="${prefix%/}";
 command="$1"
 shift
 case "$command" in
-	add|merge|pull|from-submodule) default= ;;
+	add|merge|pull|from-submodule|pull_all|push_all) default= ;;
 	split|push) default="--default HEAD" ;;
 	*) die "Unknown command '$command'" ;;
 esac
 
-if [ -z "$prefix" ]; then
+if [ -z "$prefix" -a "$command" != "pull_all" -a "$command" != "push_all" ]; then
 	die "You must provide the --prefix option."
 fi
 
 case "$command" in
+	pull_all);;
+	push_all);;
 	add) [ -e "$prefix" ] && 
 		die "prefix '$prefix' already exists." ;;
 	*)   [ -e "$prefix" ] || 
@@ -123,7 +127,7 @@ esac
 
 dir="$(dirname "$prefix/.")"
 
-if [ "$command" != "pull" -a "$command" != "add" -a "$command" != "push" ]; then
+if [ "$command" != "pull" -a "$command" != "add" -a "$command" != "push" -a "$command" != "pull_all" ]; then
 	revs=$(git rev-parse $default --revs-only "$@") || exit $?
 	dirs="$(git rev-parse --no-revs --no-flags "$@")" || exit $?
 	if [ -n "$dirs" ]; then
@@ -772,6 +776,22 @@ cmd_from-submodule()
 
 	# Remove submodule repo.
 	rm -rf $tmp_repo
+}
+
+cmd_pull_all()
+{
+	git config -f .gittrees -l | grep subtree | grep path | grep -o '=.*' | grep -o '[^=].*' |
+	while read path; do
+		git subtree pull -P $path master || exit $?
+	done
+}
+
+cmd_push_all()
+{
+	git config -f .gittrees -l | grep subtree | grep path | grep -o '=.*' | grep -o '[^=].*' |
+	while read path; do
+		git subtree push -P $path master || exit $?
+	done
 }
 
 "cmd_$command" "$@"
