891f2405c6cdf1d53c48d82bb2adfe73e77e06f8
Matt Hoffman <matt.hoffman@quantumretail.com>
2013-03-10 22:09:55 +0000

    contrib/subtree: Rename commands push_all/pull_all to push-all/pull-all
    Changing underscores to dashes (push_all -> push-all)
    
    Signed-off-by: Paul Campbell <pcampbell@kemitix.net>
    
    Conflicts:
    	.gitignore
    	contrib/subtree/git-subtree.sh

--- contrib/subtree/git-subtree.sh
+++ contrib/subtree/git-subtree.sh
@@ -12,9 +12,9 @@ git subtree add   --prefix=<prefix> <commit>
 git subtree add   --prefix=<prefix> <repository> <commit>
 git subtree merge --prefix=<prefix> <commit>
 git subtree pull  --prefix=<prefix> [<repository> [<refspec>...]]
-git subtree pull_all
+git subtree pull-all
 git subtree push  --prefix=<prefix> [<repository> [<refspec>...]]
-git subtree pull_all
+git subtree pull-all
 git subtree split --prefix=<prefix> <commit...>
 git subtree from-submodule --prefix=<prefix>
 --
@@ -107,18 +107,18 @@ prefix="${prefix%/}";
 command="$1"
 shift
 case "$command" in
-	add|merge|pull|from-submodule|pull_all|push_all) default= ;;
+	add|merge|pull|from-submodule|pull-all|push-all) default= ;;
 	split|push) default="--default HEAD" ;;
 	*) die "Unknown command '$command'" ;;
 esac
 
-if [ -z "$prefix" -a "$command" != "pull_all" -a "$command" != "push_all" ]; then
+if [ -z "$prefix" -a "$command" != "pull-all" -a "$command" != "push-all" ]; then
 	die "You must provide the --prefix option."
 fi
 
 case "$command" in
-	pull_all);;
-	push_all);;
+	pull-all);;
+	push-all);;
 	add) [ -e "$prefix" ] && 
 		die "prefix '$prefix' already exists." ;;
 	*)   [ -e "$prefix" ] || 
@@ -127,7 +127,7 @@ esac
 
 dir="$(dirname "$prefix/.")"
 
-if [ "$command" != "pull" -a "$command" != "add" -a "$command" != "push" -a "$command" != "pull_all" ]; then
+if [ "$command" != "pull" -a "$command" != "add" -a "$command" != "push" -a "$command" != "pull-all" ]; then
 	revs=$(git rev-parse $default --revs-only "$@") || exit $?
 	dirs="$(git rev-parse --no-revs --no-flags "$@")" || exit $?
 	if [ -n "$dirs" ]; then
@@ -778,7 +778,7 @@ cmd_from-submodule()
 	rm -rf $tmp_repo
 }
 
-cmd_pull_all()
+cmd_pull-all()
 {
 	git config -f .gittrees -l | grep subtree | grep path | grep -o '=.*' | grep -o '[^=].*' |
 	while read path; do
@@ -786,7 +786,7 @@ cmd_pull_all()
 	done
 }
 
-cmd_push_all()
+cmd_push-all()
 {
 	git config -f .gittrees -l | grep subtree | grep path | grep -o '=.*' | grep -o '[^=].*' |
 	while read path; do
