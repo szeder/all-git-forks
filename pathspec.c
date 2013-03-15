3b1ba043992713eee904c3a93611f2a472864a8e
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-03-15 13:06:23 +0700

    parse_pathspec: add PATHSPEC_PREFER_{CWD,FULL}
    We have two ways of dealing with empty pathspec:
    
    1. limit it to current prefix
    2. match the entire working directory
    
    Some commands go with #1, some #2. get_pathspec() and parse_pathspec()
    only support #1. Make parse_pathspec() reject empty pathspec by
    default. #1 and #2 can be specified via new flags. This makes it more
    expressive about default behavior at command level.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>

--- pathspec.c
+++ pathspec.c
@@ -270,9 +270,19 @@ void parse_pathspec(struct pathspec *pathspec,
 	if (!entry && !prefix)
 		return;
 
+	if ((flags & PATHSPEC_PREFER_CWD) &&
+	    (flags & PATHSPEC_PREFER_FULL))
+		die("BUG: PATHSPEC_PREFER_CWD and PATHSPEC_PREFER_FULL are incompatible");
+
 	/* No arguments with prefix -> prefix pathspec */
 	if (!entry) {
 		static const char *raw[2];
+
+		if (flags & PATHSPEC_PREFER_FULL)
+			return;
+
+		if (!(flags & PATHSPEC_PREFER_CWD))
+			die("BUG: parse_pathspec cannot take no arguments in this case");
 
 		pathspec->items = item = xmalloc(sizeof(*item));
 		item->match = prefix;
@@ -338,7 +348,8 @@ const char **get_pathspec(const char *prefix, const char **pathspec)
 	struct pathspec ps;
 	parse_pathspec(&ps,
 		       PATHSPEC_ALL_MAGIC & ~PATHSPEC_FROMTOP,
-		       0, prefix, pathspec);
+		       PATHSPEC_PREFER_CWD,
+		       prefix, pathspec);
 	return ps.raw;
 }
 
