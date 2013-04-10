a6e276b99c0592bec63b0bbe0b9401b144a76f8c
Felipe Contreras <felipe.contreras@gmail.com>
2013-04-10 17:15:52 -0400

    transport-helper: report errors properly
    If a push fails because the remote-helper died (with fast-export),
    the user may not see any error message. We do correctly die with a
    failed exit code, as we notice that the helper has died while
    reading back the ref status from the helper. However, we don't print
    any message. This is OK if the helper itself printed a useful error
    message, but we cannot count on that; let's let the user know that
    the helper failed.
    
    In the long run, it may make more sense to propagate the error back
    up to push, so that it can present the usual status table and give a
    nicer message. But this is a much simpler fix that can help
    immediately.
    
    While we're adding tests, let's also confirm that the remote-helper
    dying is also detected when importing refs. We currently do so
    robustly when the helper uses the "done" feature (and that is what
    we test).  We cannot do so reliably when the helper does not use the
    "done" feature, but it is not even worth testing; the right solution
    is for the helper to start using "done".
    
    Suggested-by: Jeff King <peff@peff.net>
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    Signed-off-by: Jeff King <peff@peff.net>
    Acked-by: Sverre Rabbelier <srabbelier@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5801-remote-helpers.sh
+++ t/t5801-remote-helpers.sh
@@ -197,6 +197,25 @@ test_expect_success 'push messages' '
 	git commit -a -m new &&
 	git push origin new_branch 2> msg &&
 	! grep "\[new branch\]" msg
+'
+
+test_expect_success 'proper failure checks for fetching' '
+	(GIT_REMOTE_TESTGIT_FAILURE=1 &&
+	export GIT_REMOTE_TESTGIT_FAILURE &&
+	cd local &&
+	test_must_fail git fetch 2> error &&
+	cat error &&
+	grep -q "Error while running fast-import" error
+	)
+'
+
+test_expect_success 'proper failure checks for pushing' '
+	(GIT_REMOTE_TESTGIT_FAILURE=1 &&
+	export GIT_REMOTE_TESTGIT_FAILURE &&
+	cd local &&
+	test_must_fail git push --all 2> error &&
+	cat error &&
+	grep -q "Reading from remote helper failed" error
 	)
 '
 
