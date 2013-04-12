22374f1ce963534397750c32ced271074fb465f4
Junio C Hamano <gitster@pobox.com>
2013-04-12 10:57:04 -0700

    SQUASH??? avoid referencing undefined positional vars???

--- contrib/completion/git-completion.bash
+++ contrib/completion/git-completion.bash
@@ -1828,7 +1828,7 @@ _git_config ()
 		local remote="${prev#remote.}"
 		remote="${remote%.fetch}"
 		if [ -z "$cur" ]; then
-			__gitcompadd "refs/heads/"
+			__gitcompadd "refs/heads/" "" "" ""
 			return
 		fi
 		__gitcomp_nl "$(__git_refs_remotes "$remote")"
