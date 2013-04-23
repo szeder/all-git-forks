f54f78bb609d49bfb97838e1aafebb816b9292c5
Michael J Gruber <git@drmicha.warpmail.net>
2013-04-23 14:11:54 +0200

    show: honor --textconv for blobs
    Currently, "diff" and "cat-file" for blobs honor "--textconv" options
    (with the former defaulting to "--textconv" and the latter to
    "--no-textconv") whereas "show" does not honor this option, even though
    it takes diff options.
    
    Make "show" on blobs behave like "diff", i.e. honor "--textconv" by
    default and "--no-textconv" when given.
    
    Signed-off-by: Michael J Gruber <git@drmicha.warpmail.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t4030-diff-textconv.sh
+++ t/t4030-diff-textconv.sh
@@ -90,9 +90,15 @@ test_expect_success 'status -v produces text' '
 	git reset --soft HEAD@{1}
 '
 
-test_expect_failure 'show blob produces text' '
+test_expect_success 'show blob produces text' '
 	git show HEAD:file >actual &&
 	printf "0\\n1\\n" >expect &&
+	test_cmp expect actual
+'
+
+test_expect_success 'show --no-textconv blob produces binary' '
+	git show --no-textconv HEAD:file >actual &&
+	printf "\\0\\n\\1\\n" >expect &&
 	test_cmp expect actual
 '
 
