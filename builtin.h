b22d6a9f3a5464ebb24c96f0c11728bf74044ead
Felipe Contreras <felipe.contreras@gmail.com>
2013-04-27 18:27:30 -0500

    Add proper 'stage' command
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- builtin.h
+++ builtin.h
@@ -114,6 +114,7 @@ extern int cmd_send_pack(int argc, const char **argv, const char *prefix);
 extern int cmd_shortlog(int argc, const char **argv, const char *prefix);
 extern int cmd_show(int argc, const char **argv, const char *prefix);
 extern int cmd_show_branch(int argc, const char **argv, const char *prefix);
+extern int cmd_stage(int argc, const char **argv, const char *prefix);
 extern int cmd_status(int argc, const char **argv, const char *prefix);
 extern int cmd_stripspace(int argc, const char **argv, const char *prefix);
 extern int cmd_symbolic_ref(int argc, const char **argv, const char *prefix);
