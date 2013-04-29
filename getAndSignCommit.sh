dc2a4ad5f85042dabfe1ceacdf95b3d4f060c0dd
Dustin Dalen <dustin.dalen@gmail.com>
2013-04-29 16:17:54 -0700

    Fixing the bash script and removing an used method

--- getAndSignCommit.sh
+++ getAndSignCommit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#NOTE: you must have a cert name mycert.pem in ./.
-HASH=$(git log -n1 | cut -d ' ' -f 2 | head -n1)
-FILE=$(date +%s)
-git show $(HASH) > "$FILE".txt
-openssl cms -sign -in "$FILE".txt -text -out "$FILE".msg -signer myCert.pem
-git notes --ref=crypto add -F "$FILE".msg HEAD
-rm "$FILE" "$FILE".msg
-echo "Pushing signed note to the origin"
-git push origin refs/notes/crypto/*
+#NOTE: you must have a cert name mycert.pem in ~/.
+HASH=$(git log -n1 | cut -d ' ' -f 2 | head -n1);
+FILE=$(date +\%s);
+git cat-file -p "$HASH" > "$FILE".txt;
+openssl dgst -sha256 "$FILE".txt | cut -d ' ' -f 2 > "$FILE".sha
+openssl cms -sign -in "$FILE".sha -text -out "$FILE".msg -signer ~/myCert.pem;
+git notes --ref=crypto add -F "$FILE".msg HEAD;
+rm "$FILE".txt "$FILE".msg "$FILE".sha;
+echo "Successfully signed: $HASH";
