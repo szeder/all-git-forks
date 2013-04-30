8f254cfb807b9c9091e1e9317c79db94b9c0d369
Felipe Contreras <felipe.contreras@gmail.com>
2013-04-29 23:57:50 -0500

    grep: add --staged option
    Synonym for --cached.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- builtin/grep.c
+++ builtin/grep.c
@@ -640,6 +640,8 @@ int cmd_grep(int argc, const char **argv, const char *prefix)
 	struct option options[] = {
 		OPT_BOOL(0, "cached", &cached,
 			N_("search in index instead of in the work tree")),
+		OPT_BOOLEAN(0, "staged", &cached,
+			N_("search in index instead of in the work tree")),
 		OPT_NEGBIT(0, "no-index", &use_index,
 			 N_("find in contents not managed by git"), 1),
 		OPT_BOOL(0, "untracked", &untracked,
