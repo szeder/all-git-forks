c22fd08c4f2bff0fca57098ae0924bcb3443570b
Felipe Contreras <felipe.contreras@gmail.com>
2013-05-01 04:51:28 -0500

    Add new @ shortcut for HEAD
    Even though we often can do without having to type "HEAD", e.g. "git
    log origin.." substitutes missing side of the range with "HEAD",
    typing 'HEAD' is still sometimes necessary and tedious.
    
    Make "@" usable as its synonym.  The reason for choosing '@' is that
    it follows naturally from the ref@op syntax (e.g. HEAD@{u}), except
    we have no ref, and no operation, and when we don't have those, it
    makes sens to assume 'HEAD'.
    
    With the change, we can use 'git show @~1', for example.
    
    Until now '@' was a valid name, but it conflicts with this idea, so
    make it invalid.  Probably very few people, if any, used this name.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1508-at-combinations.sh
+++ t/t1508-at-combinations.sh
@@ -45,6 +45,9 @@ check "@{u}" upstream-two
 check "@{u}@{1}" upstream-one
 check "@{-1}@{u}" master-two
 check "@{-1}@{u}@{1}" master-one
+check "@" new-two
+check "HEAD@{u}" upstream-two
+check "@@{u}" upstream-two
 nonsense "@{u}@{-1}"
 nonsense "@{1}@{u}"
 
