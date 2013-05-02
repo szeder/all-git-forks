d4126f925509acc33710fe9b448bd3438883f7cd
Felipe Contreras <felipe.contreras@gmail.com>
2013-05-02 14:28:07 -0500

    completion: update 'git reset' new stage options
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- contrib/completion/git-completion.bash
+++ contrib/completion/git-completion.bash
@@ -2231,7 +2231,8 @@ _git_reset ()
 
 	case "$cur" in
 	--*)
-		__gitcomp "--merge --mixed --hard --soft --patch"
+		__gitcomp "--merge --mixed --hard --soft --patch --keep --merge
+			--stage --no-stage --work --no-work"
 		return
 		;;
 	esac
