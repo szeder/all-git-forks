697c8e754cfabaa2fe7c0ac7814a9f36f2d441fb
Ilya Basin <basinilya@gmail.com>
2013-05-18 16:04:27 +0400

    foo

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 set -e
+cd "`dirname "$0"`"
+
+#rm -f git-svn
+
 export CC="ccache gcc"
 export PYTHON_PATH="/usr/bin/python2"
 make CC="$CC" all
 
-rm -f git-svn
-ln -s git-svn.perl git-svn
+#ln -s git-svn.perl git-svn
 for pm in Git/SVN.pm Git/SVN/Fetcher.pm Git/SVN/Ra.pm; do
   rm -f "perl/blib/lib/$pm" && ln -s --relative "`pwd`/perl/$pm" "perl/blib/lib/$pm"
 done
