65864a92b501658f3e832f5fdec433de74736490
Ramkumar Ramachandra <artagnon@gmail.com>
2013-05-23 19:32:31 +0530

    remote: expose parse_push_refspec()
    parse_fetch_refspec() is already available to other callers via
    remote.h.  There's no reason why parse_push_refspec() shouldn't be.
    
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>

--- remote.h
+++ remote.h
@@ -94,6 +94,7 @@ void ref_remove_duplicates(struct ref *ref_map);
 
 int valid_fetch_refspec(const char *refspec);
 struct refspec *parse_fetch_refspec(int nr_refspec, const char **refspec);
+struct refspec *parse_push_refspec(int nr_refspec, const char **refspec);
 
 void free_refspec(int nr_refspec, struct refspec *refspec);
 
