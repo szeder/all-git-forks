07d6cb176764d4777c79973b946c628d6dcd7b15
Jorge Garcia <Jorge-Juan.Garcia-Garcia@ensimag.imag.fr>
2013-06-07 14:19:48 +0200

    Documentation et test update

--- t/t7508-status.sh
+++ t/t7508-status.sh
@@ -1335,4 +1335,111 @@ test_expect_failure '.git/config ignore=all suppresses submodule summary' '
 	git config -f .gitmodules  --remove-section submodule.subname
 '
 
+#
+# Copyright (c) 2013 Mathieu Lienard--Mayor et Jorge Garcia
+#
+
+test_description='Test status.short'
+
+test_expect_success '"status.short=true" same as "-s"' '
+    >short_status &&
+    >status &&
+    git -c status.short=true status >short_status &&
+    git status -s >status &&
+    test_cmp status short_status
+'
+
+test_expect_success '"status.short=true" different from "--no-short"' '
+    >short_status &&
+    >status &&
+    git -c status.short=true status >short_status &&
+    git status --no-short >status &&
+    test_must_fail test_cmp status short_status
+'
+
+test_expect_success '"status.short=true" weaker than "--no-short"' '
+    >no-short_status &&
+    >status &&
+    git -c status.short=true status --no-short >no-short_status &&
+    git status --no-short >status &&
+    test_cmp status no-short_status
+'
+
+test_expect_success '"status.short=false" same as "--no-short"' '
+    >shortfalse_status &&
+    >status &&
+    git -c status.short=false status >shortfalse_status &&
+    git status --no-short >status &&
+    test_cmp status shortfalse_status
+'
+
+test_expect_success '"status.short=false" different from "-s"' '
+    >shortfalse_status &&
+    >status &&
+    git -c status.short=false status >shortfalse_status &&
+    git status -s >status &&
+    test_must_fail test_cmp status shortfalse_status
+'
+
+test_expect_success '"status.short=false" weaker than "-s"' '
+    >shortfalse_status &&
+    >status &&
+    git -c status.short=false status -s >shortfalse_status &&
+    git status -s >status &&
+    test_cmp status shortfalse_status
+'
+#
+# Copyright (c) 2013 Mathieu Lienard--Mayor et Jorge Garcia
+#
+
+test_description='Test status.branch'
+
+test_expect_success '"status.branch=true" same as "-b"' '
+    >branch_status &&
+    >status &&
+    git -c status.branch=true status -s >branch_status &&
+    git status -sb >status &&
+    test_cmp status branch_status
+'
+
+test_expect_success '"status.branch=true" different from "--no-branch"' '
+    >branch_status &&
+    >status &&
+    git -c status.branch=true status -s >branch_status &&
+    git status -s --no-branch  >status &&
+    test_must_fail test_cmp status branch_status
+'
+
+test_expect_success '"status.branch=true" weaker than "--no-branch"' '
+    >no-branch_status &&
+    >status &&
+    git -c status.branch=true status -s --no-branch >no-branch_status &&
+    git status -s --no-branch >status &&
+    test_cmp status no-branch_status
+'
+
+test_expect_success '"status.branch=false" same as "--no-branch"' '
+    >branchfalse_status &&
+    >status &&
+    git -c status.branch=false status -s >branchfalse_status &&
+    git status -s --no-branch >status &&
+    test_cmp status branchfalse_status
+'
+
+test_expect_success '"status.branch=false" different from "-b"' '
+    >branchfalse_status &&
+    >status &&
+    git -c status.branch=false status -s >branchfalse_status &&
+    git status -sb >status &&
+    test_must_fail test_cmp status branchfalse_status
+'
+
+test_expect_success '"status.branch=false" weaker than "-b"' '
+    >branchfalse_status &&
+    >status &&
+    git -c status.branch=false status -sb >branchfalse_status &&
+    git status -sb >status &&
+    test_cmp status branchfalse_status
+'
+
 test_done
