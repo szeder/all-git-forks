fa95d9f3614dce166bb07dec70f1648a1b3cbc3c
Junio C Hamano <gitster@pobox.com>
2013-06-06 19:13:50 -0700

    commit-queue: LIFO or priority queue of commits
    Traditionally we used a singly linked list of commits to hold a set
    of in-flight commits while traversing history.  The most typical use
    of the list is to add commits that are newly discovered to it, keep
    the list sorted by commit timestamp, pick up the newest one from the
    list, and keep digging.  The cost of keeping the singly linked list
    sorted is nontrivial, and this typical use pattern better matches a
    priority queue.
    
    Introduce a commit-queue structure, that can be used either as a
    LIFO stack, or a priority queue.  This will be used in the next
    patch to hold in-flight commits during sort-in-topological-order.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ commit-queue.h
@@ -0,0 +1,31 @@
+#ifndef COMMIT_QUEUE_H
+#define COMMIT_QUEUE_H
+
+/*
+ * Compare two commits; the third parameter is cb_data in the
+ * commit_queue structure.
+ */
+typedef int (*commit_compare_fn)(struct commit *, struct commit *, void *);
+
+struct commit_queue {
+	commit_compare_fn compare;
+	void *cb_data;
+	int alloc, nr;
+	struct commit **array;
+};
+
+/*
+ * Add the commit to the queue
+ */
+extern void commit_queue_put(struct commit_queue *, struct commit *);
+
+/*
+ * Extract the commit that compares the smallest out of the queue,
+ * or NULL.  If compare function is NULL, the queue acts as a LIFO
+ * stack.
+ */
+extern struct commit *commit_queue_get(struct commit_queue *);
+
+extern void clear_commit_queue(struct commit_queue *);
+
+#endif /* COMMIT_QUEUE_H */
