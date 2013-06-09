581bdec291f6365ef5aa52af6197d62963a95c21
Felipe Contreras <felipe.contreras@gmail.com>
2013-06-09 14:24:37 -0500

    rebase: split the cherry-pick stuff
    They do something completely different from 'git am', it belongs in a
    different file.
    
    No functional changes.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

---
+++ git-rebase--cherrypick.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Copyright (c) 2010 Junio C Hamano.
+#
+
+case "$action" in
+continue)
+	git am --resolved --resolvemsg="$resolvemsg" &&
+	move_to_original_branch
+	return
+	;;
+skip)
+	git am --skip --resolvemsg="$resolvemsg" &&
+	move_to_original_branch
+	return
+	;;
+esac
+
+ret=$?
+
+if test 0 != $ret
+then
+	test -d "$state_dir" && write_basic_state
+	return $ret
+fi
+
+move_to_original_branch
