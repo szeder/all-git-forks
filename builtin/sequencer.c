177267dea09ad303f76e809d5ec14a9a99348e6a
Felipe Contreras <felipe.contreras@gmail.com>
2013-06-09 14:24:25 -0500

    cherry-pick: don't barf when there's nothing to do
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- builtin/sequencer.c
+++ builtin/sequencer.c
@@ -571,8 +571,8 @@ static void prepare_revs(struct replay_opts *opts)
 	if (prepare_revision_walk(opts->revs))
 		die(_("revision walk setup failed"));
 
-	if (!opts->revs->commits)
-		die(_("empty commit set passed"));
+	if (!opts->revs->commits && !opts->quiet)
+		error(_("empty commit set passed"));
 }
 
 static void read_and_refresh_cache(struct replay_opts *opts)
