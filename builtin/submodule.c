a5c133c6ea5c36e9386821f8f95b604726cb125a
Fredrik Gustafsson <iveqy@iveqy.com>
2013-07-17 19:18:04 +0200

    wip submodule in c

--- builtin/submodule.c
+++ builtin/submodule.c
@@ -1,33 +1,52 @@
 /*
- * git
+ * Git submodule
+ *
+ * This is the submodule UI for git. The core submodule functionality can be
+ * found in submodule.c (you are reading builtin/submodule.c).
  *
  * Copyright Fredrik Gustafsson <iveqy@iveqy.com>
+ * See COPYING for license information.
+ *
  */
 
-/* Includes */
+/******** Includes ********/
 /*{*/
 #include "builtin.h"
 #include "submodule.h"
+
+#include <stdio.h>
 /*}*/
 
-/* Private functions */
+/******** Private functions ********/
 /*{*/
+
+/*
+ * \function
+ * Find submodules that is in the current commit.
+ *
+ * @return string_list of submodule paths.
+ */
+string_list collect_submodules()
+{ /*{*/
+} /*}*/
+
 /*}*/
 
-/* Public functions */
+/******** Public functions ********/
 /*{*/
 
 /*
  * \function
  * Take action depending on the arguments the user supplied to git submodule
  *
- * @param Number of command line arguments
- * @param List of arguments
- * @param
+ * @param number of command line arguments
+ * @param list of arguments
+ * @param relative path from GITDIR, null if we are in GITDIR
  * @return 0 on success, otherwise <0
  */
 int cmd_submodule(int argc, const char **argv, const char *prefix)
 { /*{*/
+	printf("Prefix: %s\n", prefix);
 	return 0;
 } /*}*/
 
