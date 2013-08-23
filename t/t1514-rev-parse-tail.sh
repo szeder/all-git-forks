50a32f783798d260d38dcf0f4f0c0ea16de07480
Felipe Contreras <felipe.contreras@gmail.com>
2013-08-23 16:47:01 -0500

    branch: add new 'tail' concept
    The 'tail' of a branch points to the first parent commit of the branch
    when it was created.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

---
+++ t/t1514-rev-parse-tail.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+test_description='test <branch>@{upstream} syntax'
+
+. ./test-lib.sh
+
+test_expect_success 'setup' '
+	echo one > content &&
+	git add content &&
+	git commit -m one &&
+	git checkout -b test master &&
+	echo two > new &&
+	git add new &&
+	git commit -a -m two
+'
+
+test_expect_success 'test tail creation' '
+	git rev-parse refs/tails/test > actual &&
+	git rev-parse master > expect &&
+	test_cmp expect actual
+'
+
+test_done
