dccb0cd8015eaed67708166d20f52c8bd6493d79
Stefan Beller <stefanbeller@googlemail.com>
2013-08-29 22:39:34 +0200

    repack: retain the return value of pack-objects
    During the review process of the previous commit (repack: rewrite the
    shell script in C), Johannes Sixt proposed to retain any exit codes from
    the sub-process, which makes it probably more obvious in case of failure.
    
    As the commit before should behave as close to the original shell
    script, the proposed change is put in this extra commit.
    The infrastructure however was already setup in the previous commit.
    (Having a local 'ret' variable)
    
    Signed-off-by: Stefan Beller <stefanbeller@googlemail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/repack.c
+++ builtin/repack.c
@@ -230,7 +230,7 @@ int cmd_repack(int argc, const char **argv, const char *prefix)
 
 	ret = start_command(&cmd);
 	if (ret)
-		return 1;
+		return ret;
 
 	nr_packs = 0;
 	out = xfdopen(cmd.out, "r");
@@ -243,7 +243,7 @@ int cmd_repack(int argc, const char **argv, const char *prefix)
 	fclose(out);
 	ret = finish_command(&cmd);
 	if (ret)
-		return 1;
+		return ret;
 	argv_array_clear(&cmd_args);
 
 	if (!nr_packs && !quiet)
