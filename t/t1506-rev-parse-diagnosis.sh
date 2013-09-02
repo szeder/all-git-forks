7fa9b55cce05298e21b483562433cfbafd69f5f8
Felipe Contreras <felipe.contreras@gmail.com>
2013-09-02 01:19:58 -0500

    sha1-name: refactor get_sha1() parsing
    Instead of parsing left to right, do it right do left, this way it is
    much more natural and probably efficient too, as there's less
    recursivity.
    
    In theory there shouldn't be any functional changes, although there's at
    least one error message that has changed.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- t/t1506-rev-parse-diagnosis.sh
+++ t/t1506-rev-parse-diagnosis.sh
@@ -104,7 +104,7 @@ test_expect_success 'correct relative file objects (6)' '
 
 test_expect_success 'incorrect revision id' '
 	test_must_fail git rev-parse foobar:file.txt 2>error &&
-	grep "Invalid object name '"'"'foobar'"'"'." error &&
+	grep "unknown revision or path not in the working tree." error
 	test_must_fail git rev-parse foobar 2> error &&
 	grep "unknown revision or path not in the working tree." error
 '
