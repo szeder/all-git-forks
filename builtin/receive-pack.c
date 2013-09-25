5e444b4e6705a7c1226a052239aa2dbd341794ed
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-09-25 14:15:40 +0700

    receive-pack: request for packv4 if it's the preferred version
    This is the only plumbing command that is controlled by
    core.preferredPackVersion so far.

--- builtin/receive-pack.c
+++ builtin/receive-pack.c
@@ -130,10 +130,11 @@ static void show_ref(const char *path, const unsigned char *sha1)
 	if (sent_capabilities)
 		packet_write(1, "%s %s\n", sha1_to_hex(sha1), path);
 	else
-		packet_write(1, "%s %s%c%s%s agent=%s\n",
+		packet_write(1, "%s %s%c%s%s%s agent=%s\n",
 			     sha1_to_hex(sha1), path, 0,
 			     " report-status delete-refs side-band-64k quiet",
 			     prefer_ofs_delta ? " ofs-delta" : "",
+			     core_default_pack_version == 4 ? " packv4" : "",
 			     git_user_agent_sanitized());
 	sent_capabilities = 1;
 }
