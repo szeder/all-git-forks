b348f2f48ee5141441a4f822903fcefd330d5c39
Felipe Contreras <felipe.contreras@gmail.com>
2013-09-28 07:03:38 -0500

    ruby: add simpler option parser
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- t/t9710-ruby.sh
+++ t/t9710-ruby.sh
@@ -108,4 +108,130 @@ test_expect_success 'test Commit' '
 	test_cmp expected actual
 '
 
+test_expect_success 'test ParseOpt' '
+	cat > parse-script <<"EOF"
+	$str = "default"
+	$num = 0
+	$bool = false
+
+	opts = ParseOpt.new
+	opts.usage = "git foo"
+
+	opts.on("b", "bool", help: "Boolean") do |v|
+	  $bool = v
+	end
+
+	opts.on("s", "string", help: "String") do |v|
+	  $str = v
+	end
+
+	opts.on("n", "number", help: "Number") do |v|
+	  $num = v.to_i
+	end
+
+	opts.parse
+
+	p(ARGV)
+	p({ :bool => $bool, :str => $str, :num => $num })
+	EOF
+
+	git ruby parse-script > actual &&
+	cat > expected <<-EOF &&
+	[]
+	{:bool=>false, :str=>"default", :num=>0}
+	EOF
+	test_cmp expected actual &&
+
+	git ruby parse-script --bool > actual &&
+	cat > expected <<-EOF &&
+	[]
+	{:bool=>true, :str=>"default", :num=>0}
+	EOF
+	test_cmp expected actual &&
+
+	git ruby parse-script -b > actual &&
+	cat > expected <<-EOF &&
+	[]
+	{:bool=>true, :str=>"default", :num=>0}
+	EOF
+	test_cmp expected actual &&
+
+	git ruby parse-script --string=foo > actual &&
+	cat > expected <<-EOF &&
+	[]
+	{:bool=>false, :str=>"foo", :num=>0}
+	EOF
+	test_cmp expected actual &&
+
+	git ruby parse-script -sfoo > actual &&
+	cat > expected <<-EOF &&
+	[]
+	{:bool=>false, :str=>"foo", :num=>0}
+	EOF
+	test_cmp expected actual &&
+
+	git ruby parse-script --number=10 > actual &&
+	cat > expected <<-EOF &&
+	[]
+	{:bool=>false, :str=>"default", :num=>10}
+	EOF
+	test_cmp expected actual &&
+
+	git ruby parse-script --bool --string=bar --number=-20 > actual &&
+	cat > expected <<-EOF &&
+	[]
+	{:bool=>true, :str=>"bar", :num=>-20}
+	EOF
+	test_cmp expected actual &&
+
+	git ruby parse-script --help > actual &&
+	cat > expected <<-EOF &&
+	usage: git foo
+	    -b, --bool            Boolean
+	    -s, --string          String
+	    -n, --number          Number
+	EOF
+	test_cmp expected actual &&
+
+	git ruby parse-script --help > actual &&
+	cat > expected <<-EOF &&
+	usage: git foo
+	    -b, --bool            Boolean
+	    -s, --string          String
+	    -n, --number          Number
+	EOF
+	test_cmp expected actual &&
+
+	test_must_fail git ruby parse-script --bad > actual &&
+	cat > expected <<-EOF &&
+	usage: git foo
+	    -b, --bool            Boolean
+	    -s, --string          String
+	    -n, --number          Number
+	EOF
+	test_cmp expected actual &&
+
+	git ruby parse-script one --bool two --string=bar three --number=-20 mambo > actual &&
+	cat > expected <<-EOF &&
+	["one", "two", "three", "mambo"]
+	{:bool=>true, :str=>"bar", :num=>-20}
+	EOF
+	test_cmp expected actual &&
+
+	git ruby parse-script one --bool two -- --three four > actual &&
+	cat > expected <<-EOF &&
+	["one", "two", "--three", "four"]
+	{:bool=>true, :str=>"default", :num=>0}
+	EOF
+	test_cmp expected actual &&
+
+	git ruby parse-script one --bool --no-bool > actual &&
+	cat > expected <<-EOF &&
+	["one"]
+	{:bool=>false, :str=>"default", :num=>0}
+	EOF
+	cat actual
+	test_cmp expected actual
+'
+
 test_done
