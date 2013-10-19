86bdaa8da0a70332ec733208e47601f150767c73
Carlos Martín Nieto <cmn@elego.de>
2013-10-19 18:19:11 +0200

    receive-pack: advertise thin-pack
    upload-pack has long advertised thin-pack, letting the clients request
    these smaller packs. The client however unconditionally assumes that a
    server is able to fix thin packs and there is no way of telling the
    client that this is in fact not the case.
    
    Make receive-pack advertise 'thin-pack' in anticipation of the client
    toggling the assumption and document this capability when used by
    receive-pack.

--- builtin/receive-pack.c
+++ builtin/receive-pack.c
@@ -132,7 +132,7 @@ static void show_ref(const char *path, const unsigned char *sha1)
 	else
 		packet_write(1, "%s %s%c%s%s agent=%s\n",
 			     sha1_to_hex(sha1), path, 0,
-			     " report-status delete-refs side-band-64k quiet",
+			     " report-status delete-refs side-band-64k quiet thin-pack",
 			     prefer_ofs_delta ? " ofs-delta" : "",
 			     git_user_agent_sanitized());
 	sent_capabilities = 1;
