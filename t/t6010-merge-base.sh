1ef6cac6809d2c7a6bbef1faaab5c35671b4b5f4
Junio C Hamano <gitster@pobox.com>
2013-10-23 16:47:32 -0700

    merge-base: teach "--fork-point" mode
    The "git pull --rebase" command computes the fork point of the
    branch being rebased using the reflog entries of the "base" branch
    (typically a remote-tracking branch) the branch's work was based on,
    in order to cope with the case in which the "base" branch has been
    rewound and rebuilt.  For example, if the history looked like this:
    
                         o---B1
                        /
        ---o---o---B2--o---o---o---Base
                \
                 B3
                  \
                   Derived
    
    where the current tip of the "base" branch is at Base, but earlier
    fetch observed that its tip used to be B3 and then B2 and then B1
    before getting to the current commit, and the branch being rebased
    on top of the latest "base" is based on commit B3, it tries to find
    B3 by going through the output of "git rev-list --reflog base" (i.e.
    Base, B1, B2, B3) until it finds a commit that is an ancestor of the
    current tip "Derived".
    
    Internally, we have get_merge_bases_many() that can compute this
    with one-go.  We would want a merge-base between Derived and a
    fictitious merge commit that would result by merging all the
    historical tips of "base".  When such a commit exist, we should get
    a single result, which exactly match one of the reflog entries of
    "base".
    
    Teach "git merge-base" a new mode, "--fork-point", to compute
    exactly that.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t6010-merge-base.sh
+++ t/t6010-merge-base.sh
@@ -230,4 +230,31 @@ test_expect_success 'criss-cross merge-base for octopus-step' '
 	test_cmp expected.sorted actual.sorted
 '
 
+test_expect_success 'using reflog to find the fork point' '
+	git reset --hard &&
+	git checkout -b base $E &&
+
+	(
+		for count in 1 2 3 4 5
+		do
+			git commit --allow-empty -m "Base commit #$count" &&
+			git rev-parse HEAD >expect$count &&
+			git checkout -B derived &&
+			git commit --allow-empty -m "Derived #$count" &&
+			git rev-parse HEAD >derived$count &&
+			git checkout base || exit 1
+		done
+
+		for count in 1 2 3 4 5
+		do
+			git merge-base --fork-point base $(cat derived$count) >actual &&
+			test_cmp expect$count actual || exit 1
+		done
+
+	) &&
+	# check that we correctly default to HEAD
+	git merge-base --fork-point base >actual &&
+	test_cmp expect5 actual
+'
+
 test_done
