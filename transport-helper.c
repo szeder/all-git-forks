32bebd4f08ee06d5941c5855803ae20e8d7a3b8f
Felipe Contreras <felipe.contreras@gmail.com>
2013-10-27 01:53:33 -0600

    transport-helper: remove old cruft
    Otherwise the push delete feature doesn't work.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- transport-helper.c
+++ transport-helper.c
@@ -834,9 +834,6 @@ static int push_refs_with_export(struct transport *transport,
 		char *private;
 		unsigned char sha1[20];
 
-		if (ref->deletion)
-			die("remote-helpers do not support ref deletion");
-
 		private = apply_refspecs(data->refspecs, data->refspec_nr, ref->name);
 		if (private && !get_sha1(private, sha1)) {
 			strbuf_addf(&buf, "^%s", private);
