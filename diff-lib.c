1c590efaf0d196e820c733c350cacdfccd896847
Heiko Voigt <hvoigt@hvoigt.net>
2013-11-28 22:31:19 +0100

    disable complete ignorance of submodules for index <-> HEAD diff
    If the value of ignore for submodules is set to "all" we would not show
    whats actually committed during status or diff. This can result in the
    user committing unexpected submodule references. Lets be nicer and always
    show whats in the index.
    
    Signed-off-by: Heiko Voigt <hvoigt@hvoigt.net>

--- diff-lib.c
+++ diff-lib.c
@@ -483,6 +483,8 @@ int run_diff_index(struct rev_info *revs, int cached)
 {
 	struct object_array_entry *ent;
 
+	enforce_no_complete_ignore_submodule(&revs->diffopt);
+
 	ent = revs->pending.objects;
 	if (diff_cache(revs, ent->item->sha1, ent->name, cached))
 		exit(128);
