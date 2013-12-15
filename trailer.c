ad8137cb1668e4ea56f4cb7a7665dc86e29dcb62
Christian Couder <chriscool@tuxfamily.org>
2013-12-15 17:04:11 +0100

    WIP conf trailer: add config functions

--- trailer.c
+++ trailer.c
@@ -22,6 +22,8 @@ struct trailer_item {
 	struct conf_info *conf;
 };
 
+struct trailer_item *first_conf_item;
+
 static int same_token(struct trailer_item *a, struct trailer_item *b, int alnum_len)
 {
 	return !strncasecmp(a->token, b->token, alnum_len);
@@ -223,3 +225,128 @@ void process_trailers(struct trailer_item *infile_tok_first,
 	}
 }
 		      
+static int set_where(struct conf_info *item, const char *value)
+{
+	if (!strcasecmp("after", value)) {
+		item->where = AFTER;
+	} else if (!strcasecmp("before", value)) {
+		item->where = BEFORE;
+	} else
+		return 1;
+	return 0;
+}
+
+static int set_if_exist(struct conf_info *item, const char *value)
+{
+	if (!strcasecmp("add_if_different", value)) {
+		item->if_exist = EXIST_ADD_IF_DIFFERENT;
+	} else if (!strcasecmp("add_if_different_neighbor", value)) {
+		item->if_exist = EXIST_ADD_IF_DIFFERENT_NEIGHBOR;
+	} else if (!strcasecmp("add", value)) {
+		item->if_exist = EXIST_ADD;
+	} else if (!strcasecmp("overwrite", value)) {
+		item->if_exist = EXIST_OVERWRITE;
+	} else if (!strcasecmp("do_nothing", value)) {
+		item->if_exist = EXIST_DO_NOTHING;
+	} else
+		return 1;
+	return 0;
+}
+
+static int set_if_missing(struct conf_info *item, const char *value)
+{
+	if (!strcasecmp("do_nothing", value)) {
+		item->if_missing = MISSING_DO_NOTHING;
+	} else if (!strcasecmp("add", value)) {
+		item->if_missing = MISSING_ADD;
+	} else
+		return 1;
+	return 0;
+}
+
+enum trailer_info_type { VALUE, COMMAND, WHERE, IF_EXIST, IF_MISSING };
+
+static int set_name_and_type(const char *conf_key, const char *suffix,
+			     enum trailer_info_type type,
+			     char **pname, enum trailer_info_type *ptype)
+{
+	int ret = ends_with(conf_key, suffix);
+	if (ret) {
+		*pname = xstrndup(conf_key, strlen(conf_key) - strlen(suffix));
+		*ptype = type;
+	}
+	return ret;
+}
+
+static struct trailer_item *get_conf_item(char *name)
+{
+	struct trailer_item *item;
+	struct trailer_item *previous;
+
+	/* Look up item with same name */
+	for (previous = NULL, item = first_conf_item;
+	     item;
+	     previous = item, item = item->next)
+	{
+		if (!strcasecmp(item->conf->name, name))
+			return item;
+	}
+
+	/* Item does not already exists, create it */
+	item = xcalloc(sizeof(struct trailer_item), 1);
+	item->conf = xcalloc(sizeof(struct conf_info), 1);
+	item->conf->name = xstrdup(name);
+
+	if (!previous)
+		first_conf_item = item;
+	else {
+		previous->next = item;
+		item->previous = previous;
+	}
+
+	return item;
+}
+
+static int git_trailer_config(const char *conf_key, const char *value, void *cb)
+{
+	if (starts_with(conf_key, "trailer.")) {
+		const char *orig_conf_key = conf_key;
+		struct trailer_item *item;
+		struct conf_info *conf;
+		char *name;
+		enum trailer_info_type type;
+
+		conf_key += 8;
+		if (!set_name_and_type(conf_key, ".key", VALUE, &name, &type) &&
+		    !set_name_and_type(conf_key, ".command", COMMAND, &name, &type) &&
+		    !set_name_and_type(conf_key, ".where", WHERE, &name, &type) &&
+		    !set_name_and_type(conf_key, ".if_exist", IF_EXIST, &name, &type) &&
+		    !set_name_and_type(conf_key, ".if_missing", IF_MISSING, &name, &type))
+			return 0;
+
+		item = get_conf_item(name);
+		conf = item->conf;
+
+		if (type == VALUE) {
+			if (conf->key)
+				warning(_("more than one %s"), orig_conf_key);
+			conf->key = xstrdup(value);
+		} else if (type == COMMAND) {
+			if (conf->command)
+				warning(_("more than one %s"), orig_conf_key);
+			conf->command = xstrdup(value);
+		} else if (type == WHERE) {
+			if (set_where(conf, value))
+				warning(_("unknow value '%s' for key '%s'"), value, orig_conf_key);
+		} else if (type == IF_EXIST) {
+			if (set_if_exist(conf, value))
+				warning(_("unknow value '%s' for key '%s'"), value, orig_conf_key);
+		} else if (type == IF_MISSING) {
+			if (set_if_missing(conf, value))
+				warning(_("unknow value '%s' for key '%s'"), value, orig_conf_key);
+		} else {
+			die("internal bug in trailer.c");
+		}
+	}
+	return 0;
+}
