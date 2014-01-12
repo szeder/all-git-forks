d35bf0085446a7f23c13819fbf53d5a094755a2f
Ramkumar Ramachandra <artagnon@gmail.com>
2014-01-12 22:41:04 +0530

    t1507 (rev-parse-upstream): fix typo in test title
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1507-rev-parse-upstream.sh
+++ t/t1507-rev-parse-upstream.sh
@@ -54,7 +54,7 @@ test_expect_success 'my-side@{upstream} resolves to correct full name' '
 	test refs/remotes/origin/side = "$(full_name my-side@{u})"
 '
 
-test_expect_success 'refs/heads/my-side@{upstream} does not resolve to my-side{upstream}' '
+test_expect_success 'refs/heads/my-side@{upstream} does not resolve to my-side@{upstream}' '
 	test_must_fail full_name refs/heads/my-side@{upstream}
 '
 
