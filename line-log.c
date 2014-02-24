1f020a450ca7c1601351d033d10741803aaea654
Heiko Voigt <hvoigt@hvoigt.net>
2014-02-24 12:23:16 +0100

    line-log: only output diff when requested
    Sometimes we might not want the diff output, e.g. for counting changes
    for a function that was specified by a regex. Lets behave the same as
    all other log output and only output the diff if -p is specified.
    
    Signed-off-by: Heiko Voigt <hvoigt@hvoigt.net>

--- line-log.c
+++ line-log.c
@@ -1142,7 +1142,8 @@ int line_log_print(struct rev_info *rev, struct commit *commit)
 	struct line_log_data *range = lookup_line_range(rev, commit);
 
 	show_log(rev);
-	dump_diff_hacky(rev, range);
+	if (rev->diff)
+		dump_diff_hacky(rev, range);
 	return 1;
 }
 
