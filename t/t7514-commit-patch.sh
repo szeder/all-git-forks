cb16b746b093fb966a72bd3da70c871d2ce3c99c
Junio C Hamano <gitster@pobox.com>
2014-03-17 12:23:38 -0700

    SQUASH to test patch hunk editing with "commit -p -m"

--- t/t7514-commit-patch.sh
+++ t/t7514-commit-patch.sh
@@ -17,15 +17,17 @@ test_expect_success 'setup (initial)' '
 
 test_expect_success 'edit hunk "commit -p -m message"' '
 	test_when_finished "rm -f editor_was_started" &&
+	rm -f editor_was_started &&
 	echo more >>file &&
-	echo e | env GIT_EDITOR="touch editor_was_started" git commit -p -m commit2 file &&
+	echo e | env GIT_EDITOR=": >editor_was_started" git commit -p -m commit2 file &&
 	test -r editor_was_started
 '
 
 test_expect_success 'edit hunk "commit --dry-run -p -m message"' '
 	test_when_finished "rm -f editor_was_started" &&
+	rm -f editor_was_started &&
 	echo more >>file &&
-	echo e | env GIT_EDITOR="touch editor_was_started" git commit -p -m commit3 file &&
+	echo e | env GIT_EDITOR=": >editor_was_started" git commit -p -m commit3 file &&
 	test -r editor_was_started
 '
 
