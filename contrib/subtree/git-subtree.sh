bd0700749e2ba32bb002b408a2984aaa711206e1
Ilya Lipnitskiy <ilya.y.lipnitskiy@hp.com>
2014-03-25 11:26:13 -0700

    subtree: allow --squash and --message with push

--- contrib/subtree/git-subtree.sh
+++ contrib/subtree/git-subtree.sh
@@ -752,12 +752,18 @@ cmd_push()
 	if [ $# -ne 2 ]; then
 	    die "You must provide <repository> <ref>"
 	fi
+
+	opts=
+	if [ -n "$squash" ]; then
+		opts="-squash"
+	fi
+
 	ensure_valid_ref_format "$2"
 	if [ -e "$dir" ]; then
 	    repository=$1
 	    refspec=$2
 	    echo "git push using: " $repository $refspec
-	    localrev=$(git subtree split --prefix="$prefix") || die
+	    localrev=$(git subtree split --prefix="$prefix" $opts --message="$message") || die
 	    git push $repository $localrev:refs/heads/$refspec
 	else
 	    die "'$dir' must already exist. Try 'git subtree add'."
