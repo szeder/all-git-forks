4fa6e16a639a06e4baeb58743ca0a2f0277dea3d
Michael Haggerty <mhagger@alum.mit.edu>
2014-04-01 16:53:51 +0200

    delete_ref_loose(): don't muck around in the lock_file's filename
    It's bad manners.  Especially since, if unlink_or_warn() failed, the
    memory wasn't restored to its original contents.
    
    So make our own copy to work with.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- refs.c
+++ refs.c
@@ -2485,12 +2485,14 @@ static int repack_without_ref(const char *refname)
 static int delete_ref_loose(struct ref_lock *lock, int flag)
 {
 	if (!(flag & REF_ISPACKED) || flag & REF_ISSYMREF) {
-		/* loose */
-		int err, i = strlen(lock->lk->filename) - 5; /* .lock */
-
-		lock->lk->filename[i] = 0;
-		err = unlink_or_warn(lock->lk->filename);
-		lock->lk->filename[i] = '.';
+		/*
+		 * loose.  The loose file name is the same as the
+		 * lockfile name, minus ".lock":
+		 */
+		char *loose_filename = xmemdupz(lock->lk->filename,
+						strlen(lock->lk->filename) - 5);
+		int err = unlink_or_warn(loose_filename);
+		free(loose_filename);
 		if (err && errno != ENOENT)
 			return 1;
 	}
