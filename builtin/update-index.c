072bbec59df3185209366aea79c9b416a79bf05e
Michael Haggerty <mhagger@alum.mit.edu>
2014-04-07 01:33:44 +0200

    unable_to_lock_die(): rename function from unable_to_lock_index_die()
    This function is used for other things besides the index, so rename it
    accordingly.
    
    Suggested-by: Jeff King <peff@peff.net>
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/update-index.c
+++ builtin/update-index.c
@@ -891,7 +891,7 @@ int cmd_update_index(int argc, const char **argv, const char *prefix)
 		if (newfd < 0) {
 			if (refresh_args.flags & REFRESH_QUIET)
 				exit(128);
-			unable_to_lock_index_die(get_index_file(), lock_error);
+			unable_to_lock_die(get_index_file(), lock_error);
 		}
 		if (write_cache(newfd, active_cache, active_nr) ||
 		    commit_locked_index(lock_file))
