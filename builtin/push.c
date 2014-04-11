ce1932971d8b65acd4295b379d4835306668899f
Felipe Contreras <felipe.contreras@gmail.com>
2014-04-11 09:28:33 -0500

    push: always use publish branch
    Regardless of push.default, and remote.push.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- builtin/push.c
+++ builtin/push.c
@@ -195,11 +195,7 @@ static void setup_push_current(struct remote *remote, struct branch *branch)
 static void setup_push_simple(struct remote *remote, struct branch *branch,
 		int triangular)
 {
-	if (branch->push_name) {
-		struct strbuf refspec = STRBUF_INIT;
-		strbuf_addf(&refspec, "%s:%s", branch->name, branch->push_name);
-		add_refspec(refspec.buf);
-	} else if (triangular) {
+	if (triangular) {
 		setup_push_current(remote, branch);
 	} else {
 		setup_push_upstream(remote, branch, triangular);
@@ -455,7 +451,14 @@ static int do_push(const char *repo, int flags)
 	}
 
 	if (!refspec && !(flags & TRANSPORT_PUSH_ALL)) {
-		if (remote->push_refspec_nr) {
+		struct branch *branch = branch_get(NULL);
+		/* Is there a publish branch */
+		if (branch->pushremote_name && !strcmp(remote->name, branch->pushremote_name) &&
+				branch && branch->push_name) {
+			struct strbuf refspec = STRBUF_INIT;
+			strbuf_addf(&refspec, "%s:%s", branch->name, branch->push_name);
+			add_refspec(refspec.buf);
+		} else if (remote->push_refspec_nr) {
 			refspec = remote->push_refspec;
 			refspec_nr = remote->push_refspec_nr;
 		} else if (!(flags & TRANSPORT_PUSH_MIRROR))
