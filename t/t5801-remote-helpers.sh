88630f6feffa2485396f5a4005a06f467370742e
Felipe Contreras <felipe.contreras@gmail.com>
2014-04-12 11:48:33 -0500

    transport-helper: fix sync issue on crashes
    When a remote helper crashes while pushing we should revert back to the
    state before the push, however, it's possible that `git fast-export`
    already finished its job, and therefore has exported the marks already.
    
    This creates a synchronization problem because from that moment on
    `git fast-{import,export}` will have marks that the remote helper is not
    aware of and all further commands fail (if those marks are referenced).
    
    The fix is to tell `git fast-export` to export to a temporary file, and
    only after the remote helper has finishes successfully, move to the
    final destination.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- t/t5801-remote-helpers.sh
+++ t/t5801-remote-helpers.sh
@@ -236,6 +236,17 @@ test_expect_success 'push update refs failure' '
 	)
 '
 
+clean_mark () {
+	cut -f 2 -d ' ' $1 | git cat-file --batch-check | grep commit | sort > $(basename $1)
+}
+
+cmp_marks () {
+	test_when_finished "rm -rf git.marks testgit.marks" &&
+	clean_mark .git/testgit/$1/git.marks &&
+	clean_mark .git/testgit/$1/testgit.marks &&
+	test_cmp git.marks testgit.marks
+}
+
 test_expect_success 'proper failure checks for fetching' '
 	(GIT_REMOTE_TESTGIT_FAILURE=1 &&
 	export GIT_REMOTE_TESTGIT_FAILURE &&
@@ -250,7 +261,11 @@ test_expect_success 'proper failure checks for pushing' '
 	(GIT_REMOTE_TESTGIT_FAILURE=1 &&
 	export GIT_REMOTE_TESTGIT_FAILURE &&
 	cd local &&
-	test_must_fail git push --all
+	git checkout -b crash master &&
+	echo crash >> file &&
+	git commit -a -m crash &&
+	test_must_fail git push --all &&
+	cmp_marks origin
 	)
 '
 
