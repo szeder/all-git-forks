a1b9f7592ff8fd415f367ad4251b4f16c2c20a97
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-04-18 14:28:08 +0700

    read-cache: be specific what part of the index has changed

--- cache.h
+++ cache.h
@@ -268,6 +268,10 @@ static inline unsigned int canon_mode(unsigned int mode)
 
 #define cache_entry_size(len) (offsetof(struct cache_entry,name) + (len) + 1)
 
+#define CE_ENTRY_CHANGED	(1 << 0)
+#define CE_ENTRY_REMOVED	(1 << 1)
+#define CE_ENTRY_ADDED		(1 << 2)
+
 struct index_state {
 	struct cache_entry **cache;
 	unsigned int version;
