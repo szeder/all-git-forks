7ee68ade0fb349e6c32991757b97ee0730775083
Felipe Contreras <felipe.contreras@gmail.com>
2014-04-20 16:27:03 -0500

    remote-helpers: move tests out of contrib
    They should be tested by default.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- t/t5810-remote-hg.sh
+++ t/t5810-remote-hg.sh
@@ -8,8 +8,7 @@
 
 test_description='Test remote-hg'
 
-test -n "$TEST_DIRECTORY" || TEST_DIRECTORY=${0%/*}/../../t
-. "$TEST_DIRECTORY"/test-lib.sh
+. ./test-lib.sh
 
 if ! test_have_prereq PYTHON
 then
