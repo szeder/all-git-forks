fef9ad7c1e07e50d9a7e2f9b9f4c4ba6e1383316
Felipe Contreras <felipe.contreras@gmail.com>
2014-05-04 01:03:58 -0500

    Revert "silence some -Wuninitialized false positives"
    In recent versions of gcc (4.9.0), we get a few of these:
    
      notes.c: In function ‘notes_display_config’:
      notes.c:970:28: warning: right-hand operand of comma expression has no effect [-Wunused-value]
          config_error_nonbool(k);
                                ^
    Previous commit explains the reason.
    
    This reverts commit a469a1019352b8efc4bd7003b0bd59eb60fc428c.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    
    Conflicts:
    	cache.h
    	parse-options.h

--- parse-options.h
+++ parse-options.h
@@ -175,10 +175,6 @@ extern NORETURN void usage_msg_opt(const char *msg,
 
 extern int optbug(const struct option *opt, const char *reason);
 extern int opterror(const struct option *opt, const char *reason, int flags);
-#if defined(__GNUC__)
-#define opterror(o,r,f) (opterror((o),(r),(f)), const_error())
-#endif
-
 /*----- incremental advanced APIs -----*/
 
 enum {
