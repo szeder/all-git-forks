299e34e356be925f9f109ef6fe410b2f0c842d8d
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-05-13 09:38:27 +0700

    daemonize(): set a flag before exiting the main process
    This allows signal handlers and atexit functions to realize this
    situation and not clean up.

--- setup.c
+++ setup.c
@@ -1015,7 +1015,7 @@ void sanitize_stdfds(void)
 		close(fd);
 }
 
-int daemonize(void)
+int daemonize(int *daemonized)
 {
 #ifdef NO_POSIX_GOODIES
 	errno = ENOSYS;
@@ -1027,6 +1027,8 @@ int daemonize(void)
 		case -1:
 			die_errno("fork failed");
 		default:
+			if (daemonized)
+				*daemonized = 1;
 			exit(0);
 	}
 	if (setsid() == -1)
