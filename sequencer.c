62276a2bdd850c1f73e2007e858c27e12460c90f
Junio C Hamano <gitster@pobox.com>
2014-06-02 13:14:47 -0700

    fixup! read-cache: new API write_locked_index instead of write_index/write_cache

--- sequencer.c
+++ sequencer.c
@@ -672,7 +672,7 @@ static void prepare_revs(struct replay_opts *opts)
 static void read_and_refresh_cache(struct replay_opts *opts)
 {
 	static struct lock_file index_lock;
-	hold_locked_index(&index_lock, 0);
+	int index_fd = hold_locked_index(&index_lock, 0);
 	if (read_index_preload(&the_index, NULL) < 0)
 		die(_("git %s: failed to read the index"), action_name(opts));
 	refresh_index(&the_index, REFRESH_QUIET|REFRESH_UNMERGED, NULL, NULL, NULL);
