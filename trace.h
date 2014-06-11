17a3b35bfcec14be1ca7f50271f5f9c79fa08db1
Karsten Blees <karsten.blees@gmail.com>
2014-06-11 10:00:36 +0200

    trace: add 'file:line' to all trace output
    This is useful to see where trace output came from.
    
    Add 'const char *file, int line' parameters to the printing functions and
    rename them to *_fl.
    
    Add trace_printf* and trace_strbuf macros resolving to the *_fl functions
    and let the preprocessor fill in __FILE__ and __LINE__.
    
    As the trace_printf* functions take a variable number of arguments, this
    requires variadic macros (i.e. '#define foo(...) foo_impl(__VA_ARGS__)'.
    Though part of C99, it is unclear whether older compilers support this.
    Thus keep the old functions and only enable variadic macros for GNUC and
    MSVC 2005+ (_MSC_VER 1400). This has the nice side effect that the old
    C-style declarations serve as documentation how the macros are to be used.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- trace.h
+++ trace.h
@@ -7,6 +7,8 @@
 extern void trace_repo_setup(const char *prefix);
 extern int trace_want(const char *key);
 
+#ifndef HAVE_VARIADIC_MACROS
+
 __attribute__((format (printf, 1, 2)))
 extern void trace_printf(const char *format, ...);
 
@@ -17,5 +19,52 @@ __attribute__((format (printf, 2, 3)))
 extern void trace_argv_printf(const char **argv, const char *format, ...);
 
 extern void trace_strbuf(const char *key, const struct strbuf *data);
+
+#else
+
+/*
+ * Macros to add file:line - see above for C-style declarations of how these
+ * should be used.
+ */
+
+/*
+ * Note: with C99 variadic macros, __VA_ARGS__ must include the last fixed
+ * parameter ('format' in this case). Otherwise, a call without variable
+ * arguments will have a surplus ','. E.g.:
+ *
+ *  #define foo(format, ...) bar(format, __VA_ARGS__)
+ *  foo("test");
+ *
+ * will expand to
+ *
+ *  bar("test",);
+ *
+ * which is invalid (note the ',)'). With GNUC, '##__VA_ARGS__' drops the
+ * comma, but this is non-standard.
+ */
+
+#define trace_printf(...) \
+	trace_printf_key_fl(__FILE__, __LINE__, "GIT_TRACE", __VA_ARGS__)
+
+#define trace_printf_key(key, ...) \
+	trace_printf_key_fl(__FILE__, __LINE__, key, __VA_ARGS__)
+
+#define trace_argv_printf(argv, ...) \
+	trace_argv_printf_fl(__FILE__, __LINE__, argv, __VA_ARGS__)
+
+#define trace_strbuf(key, data) \
+	trace_strbuf_fl(__FILE__, __LINE__, key, data)
+
+/* backend functions, use non-*fl macros instead */
+__attribute__((format (printf, 4, 5)))
+extern void trace_printf_key_fl(const char *file, int line, const char *key,
+				const char *format, ...);
+__attribute__((format (printf, 4, 5)))
+extern void trace_argv_printf_fl(const char *file, int line, const char **argv,
+				 const char *format, ...);
+extern void trace_strbuf_fl(const char *file, int line, const char *key,
+			    const struct strbuf *data);
+
+#endif /* HAVE_VARIADIC_MACROS */
 
 #endif /* TRACE_H */
