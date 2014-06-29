16199744d0fba418af708367c61afcc1ed0beb13
Heiko Voigt <hvoigt@hvoigt.net>
2014-06-29 21:30:50 +0200

    submodule: extract function for fetching a submodule
    This way we can switch more easily to clone when the submodule
    does not exist yet.

--- submodule.c
+++ submodule.c
@@ -636,40 +636,72 @@ static int get_fetch_recurse_config(const struct submodule *submodule, int comma
 	return config_fetch_recurse_submodules;
 }
 
+static int do_fetch_submodule(const char *prefix, const struct submodule *submodule,
+		const struct argv_array *options, const char *default_argv, int quiet)
+{
+	int result = 0;
+	struct strbuf submodule_path = STRBUF_INIT;
+	struct strbuf submodule_git_dir = STRBUF_INIT;
+	struct strbuf submodule_prefix = STRBUF_INIT;
+	const char *git_dir;
+	const char *work_tree = get_git_work_tree();
+	if (!work_tree)
+		return result;
+
+	strbuf_addf(&submodule_path, "%s/%s", work_tree, submodule->path);
+	strbuf_addf(&submodule_git_dir, "%s/.git", submodule_path.buf);
+	strbuf_addf(&submodule_prefix, "%s%s/", prefix, submodule->path);
+	git_dir = read_gitfile(submodule_git_dir.buf);
+	if (!git_dir)
+		git_dir = submodule_git_dir.buf;
+	if (is_directory(git_dir)) {
+		int i;
+		struct child_process cp;
+		struct argv_array argv = ARGV_ARRAY_INIT;
+
+		argv_array_push(&argv, "fetch");
+		for (i = 0; i < options->argc; i++)
+			argv_array_push(&argv, options->argv[i]);
+		argv_array_push(&argv, "--recurse-submodules-default");
+		argv_array_push(&argv, default_argv);
+		argv_array_push(&argv, "--submodule-prefix");
+		argv_array_push(&argv, submodule_prefix.buf);
+
+		memset(&cp, 0, sizeof(cp));
+		cp.env = local_repo_env;
+		cp.git_cmd = 1;
+		cp.no_stdin = 1;
+		cp.dir = submodule_path.buf;
+		cp.argv = argv.argv;
+
+		if (!quiet)
+			printf("Fetching submodule %s%s\n", prefix, submodule->path);
+		result = run_command(&cp) ? 1 : 0;
+
+		argv_array_clear(&argv);
+	}
+	strbuf_release(&submodule_path);
+	strbuf_release(&submodule_git_dir);
+	strbuf_release(&submodule_prefix);
+
+	return result;
+}
+
 int fetch_populated_submodules(const struct argv_array *options,
 			       const char *prefix, int command_line_option,
 			       int quiet)
 {
 	int i, result = 0;
-	struct child_process cp;
-	struct argv_array argv = ARGV_ARRAY_INIT;
-	const char *work_tree = get_git_work_tree();
-	if (!work_tree)
-		goto out;
 
 	if (read_cache() < 0)
 		die("index file corrupt");
 
-	argv_array_push(&argv, "fetch");
-	for (i = 0; i < options->argc; i++)
-		argv_array_push(&argv, options->argv[i]);
-	argv_array_push(&argv, "--recurse-submodules-default");
-	/* default value, "--submodule-prefix" and its value are added later */
-
-	memset(&cp, 0, sizeof(cp));
-	cp.env = local_repo_env;
-	cp.git_cmd = 1;
-	cp.no_stdin = 1;
-
 	calculate_changed_submodule_paths();
 
 	for (i = 0; i < active_nr; i++) {
-		struct strbuf submodule_path = STRBUF_INIT;
-		struct strbuf submodule_git_dir = STRBUF_INIT;
-		struct strbuf submodule_prefix = STRBUF_INIT;
 		const struct cache_entry *ce = active_cache[i];
-		const char *git_dir, *default_argv;
 		const struct submodule *submodule;
+		const char *default_argv;
 
 		if (!S_ISGITLINK(ce->ce_mode))
 			continue;
@@ -693,32 +725,9 @@ int fetch_populated_submodules(const struct argv_array *options,
 			continue;
 		}
 
-		strbuf_addf(&submodule_path, "%s/%s", work_tree, ce->name);
-		strbuf_addf(&submodule_git_dir, "%s/.git", submodule_path.buf);
-		strbuf_addf(&submodule_prefix, "%s%s/", prefix, ce->name);
-		git_dir = read_gitfile(submodule_git_dir.buf);
-		if (!git_dir)
-			git_dir = submodule_git_dir.buf;
-		if (is_directory(git_dir)) {
-			if (!quiet)
-				printf("Fetching submodule %s%s\n", prefix, ce->name);
-			cp.dir = submodule_path.buf;
-			argv_array_push(&argv, default_argv);
-			argv_array_push(&argv, "--submodule-prefix");
-			argv_array_push(&argv, submodule_prefix.buf);
-			cp.argv = argv.argv;
-			if (run_command(&cp))
-				result = 1;
-			argv_array_pop(&argv);
-			argv_array_pop(&argv);
-			argv_array_pop(&argv);
-		}
-		strbuf_release(&submodule_path);
-		strbuf_release(&submodule_git_dir);
-		strbuf_release(&submodule_prefix);
+		if (do_fetch_submodule(prefix, submodule, options, default_argv, quiet))
+			result = 1;
 	}
-	argv_array_clear(&argv);
-out:
 	string_list_clear(&changed_submodule_names, 1);
 	return result;
 }
