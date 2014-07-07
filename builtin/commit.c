c2693d54924c4ea11330cba9ac52b96d80111012
Junio C Hamano <gitster@pobox.com>
2014-07-07 16:06:37 -0700

    SQUASH???

--- builtin/commit.c
+++ builtin/commit.c
@@ -365,18 +365,10 @@ static char *prepare_index(int argc, const char **argv, const char *prefix,
 		fd = hold_locked_index(&index_lock, 1);
 		add_files_to_cache(also ? prefix : NULL, &pathspec, 0);
 		refresh_cache_or_die(refresh_flags);
-		if (active_cache_changed
-		    || !cache_tree_fully_valid(active_cache_tree)) {
-			update_main_cache_tree(WRITE_TREE_SILENT);
-			active_cache_changed = 1;
-		}
-		if (active_cache_changed) {
-			if (write_cache(fd, active_cache, active_nr) ||
-			    close_lock_file(&index_lock))
-				die(_("unable to write new_index file"));
-		} else {
-			rollback_lock_file(&index_lock);
-		}
+		update_main_cache_tree(WRITE_TREE_SILENT);
+		if (write_cache(fd, active_cache, active_nr) ||
+		    close_lock_file(&index_lock))
+			die(_("unable to write new_index file"));
 		commit_style = COMMIT_NORMAL;
 		return index_lock.filename;
 	}
@@ -393,10 +385,17 @@ static char *prepare_index(int argc, const char **argv, const char *prefix,
 	if (!only && !pathspec.nr) {
 		fd = hold_locked_index(&index_lock, 1);
 		refresh_cache_or_die(refresh_flags);
-		update_main_cache_tree(WRITE_TREE_SILENT);
-		if (write_cache(fd, active_cache, active_nr) ||
-		    commit_locked_index(&index_lock))
-			die(_("unable to write new_index file"));
+		if (!active_cache_changed &&
+		    !cache_tree_fully_valid(active_cache_tree))
+			active_cache_changed = 1;
+		if (active_cache_changed) {
+			update_main_cache_tree(WRITE_TREE_SILENT);
+			if (write_cache(fd, active_cache, active_nr) ||
+			    commit_locked_index(&index_lock))
+				die(_("unable to write new_index file"));
+		} else {
+			rollback_lock_file(&index_lock);
+		}
 		commit_style = COMMIT_AS_IS;
 		return get_index_file();
 	}
