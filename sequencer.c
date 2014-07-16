282edb28abe86374f0ea8242decf2252c167184a
Ronnie Sahlberg <sahlberg@google.com>
2014-07-16 15:23:12 -0700

    refs.c: fix handling of badly named refs
    We currently do not handle badly named refs well :
    $ cp .git/refs/heads/master .git/refs/heads/master.....@\*@\\.
    $ git branch
       fatal: Reference has invalid format: 'refs/heads/master.....@*@\.'
    $ git branch -D master.....@\*@\\.
      error: branch 'master.....@*@\.' not found.
    
    But we can not really recover from a badly named ref with less than
    manually deleting the .git/refs/heads/<refname> file.
    
    Change resolve_ref_unsafe to take a flags field instead of a 'reading'
    boolean and update all callers that used a non-zero value for reading
    to pass the flag RESOLVE_REF_READING instead.
    Add another flag RESOLVE_REF_ALLOW_BAD_NAME that will make
    resolve_ref_unsafe skip checking the refname for sanity and use this
    from branch.c so that we will be able to call resolve_ref_unsafe on such
    refs when trying to delete it.
    Add checks for refname sanity when updating (not deleting) a ref in
    ref_transaction_update and in ref_transaction_create to make the transaction
    fail if an attempt is made to create/update a badly named ref.
    Since all ref changes will later go through the transaction layer this means
    we no longer need to check for and fail for bad refnames in
    lock_ref_sha1_basic.
    
    Change lock_ref_sha1_basic to not fail for bad refnames. Just check the
    refname, and print an error, and remember that the refname is bad so that
    we can skip calling verify_lock().
    
    Allow reading refs with bad names from loose refs and packed refs
    but flag them as broken. This means that the refs will at least show up in
    git branch even if their name is invalid/broken.
    
    Since we now do the refname checks, when we need to, before the call
    to create_ref_entry we can remove the check_name argument to the function.
    
    Signed-off-by: Ronnie Sahlberg <sahlberg@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sequencer.c
+++ sequencer.c
@@ -365,7 +365,7 @@ static int is_index_unchanged(void)
 	unsigned char head_sha1[20];
 	struct commit *head_commit;
 
-	if (!resolve_ref_unsafe("HEAD", head_sha1, 1, NULL))
+	if (!resolve_ref_unsafe("HEAD", head_sha1, RESOLVE_REF_READING, NULL))
 		return error(_("Could not resolve HEAD commit\n"));
 
 	head_commit = lookup_commit(head_sha1);
