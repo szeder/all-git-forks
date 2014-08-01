863572694713f1d1f7dfbf7fb51850325d5219ee
Tanay Abhra <tanayabh@gmail.com>
2014-08-01 10:05:52 -0700

    change `git_config()` return value to void
    Currently `git_config()` returns an integer signifying an error code.
    During rewrites of the function most of the code was shifted to
    `git_config_with_options()`. `git_config_with_options()` normally
    returns positive values if its `config_source` parameter is set as NULL,
    as most errors are fatal, and non-fatal potential errors are guarded
    by "if" statements that are entered only when no error is possible.
    
    Still a negative value can be returned in case of race condition between
    `access_or_die()` & `git_config_from_file()`. Also, all callers of
    `git_config()` ignore the return value except for one case in branch.c.
    
    Change `git_config()` return value to void and make it die if it receives
    a negative value from `git_config_with_options()`.
    
    Original-patch-by: Matthieu Moy <Matthieu.Moy@imag.fr>
    Signed-off-by: Tanay Abhra <tanayabh@gmail.com>
    Reviewed-by: Matthieu Moy <Matthieu.Moy@imag.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- config.c
+++ config.c
@@ -1237,9 +1237,21 @@ struct key_value_info {
 	int linenr;
 };
 
-int git_config(config_fn_t fn, void *data)
+void git_config(config_fn_t fn, void *data)
 {
-	return git_config_with_options(fn, data, NULL, 1);
+	if (git_config_with_options(fn, data, NULL, 1) < 0)
+		/*
+		 * git_config_with_options() normally returns only
+		 * positive values, as most errors are fatal, and
+		 * non-fatal potential errors are guarded by "if"
+		 * statements that are entered only when no error is
+		 * possible.
+		 *
+		 * If we ever encounter a non-fatal error, it means
+		 * something went really wrong and we should stop
+		 * immediately.
+		 */
+		die(_("unknown error occured while reading the configuration files"));
 }
 
 static struct config_set_element *configset_find_element(struct config_set *cs, const char *key)
