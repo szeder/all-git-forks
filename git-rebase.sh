8a3fe174ce0bdc3d083187d2258c16352e450652
Fabian Ruch <bafain@gmail.com>
2014-08-18 22:58:40 +0200

    rebase -i: hide interactive command messages in verbose mode
    git-rebase--interactive prints summary messages of the commits it
    creates in the final history only if the `--verbose` option is
    specified by the user and suppresses them otherwise. This behaviour
    is implemented by wrapping git-commit calls in a shell function named
    `output` which redirects stderr to stdout, captures stdout in a shell
    variable and ignores its contents unless the command exits with an
    error status.
    
    The command lines used to implement the to-do list commands `reword`
    and `squash` print diagnostic messages even in non-verbose mode. The
    reason for this inconsistency is that both commands launch the log
    message editor `GIT_EDITOR` which usually requires a working terminal
    attached to stdin. Wrapping the `reword` and `squash` command lines
    in `output` would seemingly freeze the terminal (see commit 7725cb5,
    "rebase -i: fix reword when using a terminal editor"). Temporarily
    redirect the `GIT_EDITOR` output to a third file descriptor in order
    to ship it around the capture stream. Wrap the remaining git-commit
    command lines in the new `output`. At the moment, it is still no
    alternative to pass the `--quiet` option in non-verbose mode because
    git-merge-recursive for instance prints some messages regardless of
    the verbosity level.
    
    In order to temporarily redirect the editor output, the new
    definition of `output` creates a wrapper script in the state
    directory to be used as `GIT_EDITOR`. Make sure the state directory
    exists before `output` is called for the first time.

--- git-rebase.sh
+++ git-rebase.sh
@@ -131,9 +131,18 @@ write_basic_state () {
 output () {
 	case "$verbose" in
 	'')
-		output=$("$@" 2>&1 )
+		cat >"$state_dir"/editor.sh <<-EOF
+		#!/bin/sh
+		$(git var GIT_EDITOR) "\$@" >&3
+		EOF
+		chmod +x "$state_dir"/editor.sh
+		(
+			GIT_EDITOR=$(git rev-parse --sq-quote "$state_dir"/editor.sh)
+			export GIT_EDITOR
+			"$@" 3>&1 1>"$state_dir"/output 2>&1
+		)
 		status=$?
-		test $status != 0 && printf "%s\n" "$output"
+		test $status != 0 && cat "$state_dir"/output
 		return $status
 		;;
 	*)
