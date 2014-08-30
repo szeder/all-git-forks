d0aa6e862296bd6b8c07cdaa5d6622798215e2c2
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-08-30 15:33:53 +0700

    checkout: clean up half-prepared directories in --to mode
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t2025-checkout-to.sh
+++ t/t2025-checkout-to.sh
@@ -17,6 +17,12 @@ test_expect_success 'checkout --to an existing worktree' '
 	test_must_fail git checkout --detach --to existing master
 '
 
+test_expect_success 'checkout --to refuses to checkout locked branch' '
+	test_must_fail git checkout --to zere master &&
+	! test -d zere &&
+	! test -d .git/repos/zere
+'
+
 test_expect_success 'checkout --to a new worktree' '
 	git checkout --to here master &&
 	(
