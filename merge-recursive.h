f8d9df67ec8b300787448516734883a5cdae6d43
Thomas Rast <trast@inf.ethz.ch>
2014-09-06 19:57:00 +0200

    merge-recursive: internal flag to avoid touching the worktree
    o->call_depth has a double function: a nonzero call_depth means we
    want to construct virtual merge bases, but it also means we want to
    avoid touching the worktree.  Introduce a new flag o->no_worktree to
    trigger only the latter.
    
    Signed-off-by: Thomas Rast <trast@inf.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- merge-recursive.h
+++ merge-recursive.h
@@ -15,6 +15,8 @@ struct merge_options {
 	const char *subtree_shift;
 	unsigned buffer_output : 1;
 	unsigned renormalize : 1;
+	unsigned gently : 1;
+	unsigned no_worktree : 1; /* do not touch worktree */
 	long xdl_opts;
 	int verbosity;
 	int detect_rename;
