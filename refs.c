1463da181d5d87948efae587448c125144c3218f
Michael Haggerty <mhagger@alum.mit.edu>
2014-09-06 09:50:38 +0200

    struct lock_file: declare some fields volatile
    The function remove_lock_file_on_signal() is used as a signal handler.
    It is not realistic to make the signal handler conform strictly to the
    C standard, which is very restrictive about what a signal handler is
    allowed to do.  But let's increase the likelihood that it will work:
    
    The lock_file_list global variable and several fields from struct
    lock_file are used by the signal handler.  Declare those values
    "volatile" to (1) force the main process to write the values to RAM
    promptly, and (2) prevent updates to these fields from being reordered
    in a way that leaves an opportunity for a jump to the signal handler
    while the object is in an inconsistent state.
    
    We don't mark the filename field volatile because that would prevent
    the use of strcpy(), and it is anyway unlikely that a compiler
    re-orders a strcpy() call across other expressions.  So in practice it
    should be possible to get away without "volatile" in the "filename"
    case.
    
    Suggested-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -2260,15 +2260,16 @@ int commit_packed_refs(void)
 		get_packed_ref_cache(&ref_cache);
 	int error = 0;
 	int save_errno = 0;
+	int fd;
 
 	if (!packed_ref_cache->lock)
 		die("internal error: packed-refs not locked");
 	write_or_die(packed_ref_cache->lock->fd,
 		     PACKED_REFS_HEADER, strlen(PACKED_REFS_HEADER));
 
+	fd = packed_ref_cache->lock->fd;
 	do_for_each_entry_in_dir(get_packed_ref_dir(packed_ref_cache),
-				 0, write_packed_entry_fn,
-				 &packed_ref_cache->lock->fd);
+				 0, write_packed_entry_fn, &fd);
 	if (commit_lock_file(packed_ref_cache->lock)) {
 		save_errno = errno;
 		error = -1;
