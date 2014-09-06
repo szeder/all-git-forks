b5e97c4af231e2d70bc62e82afb9212c9bc70465
Thomas Rast <tr@thomasrast.ch>
2014-09-06 19:57:04 +0200

    merge-recursive: allow storing conflict hunks in index
    Add a --conflicts-in-index option to merge-recursive, which instructs
    it to always store the 3-way merged result in the index.  (Normally it
    only does so in recursive invocations, but not for the final result.)
    
    This serves as a building block for the "remerge diff" feature coming
    up in a subsequent patch.  The external option lets us easily use it
    from tests, where we'd otherwise need a new test-* helper to access
    the feature.
    
    Furthermore, it might occasionally be useful for scripts that want to
    look at the result of invoking git-merge without tampering with the
    worktree.  They could already get the _conflicts_ with --index-only,
    but not (conveniently) the conflict-hunk formatted files that would
    normally be written to the worktree.
    
    Signed-off-by: Thomas Rast <tr@thomasrast.ch>

--- merge-recursive.h
+++ merge-recursive.h
@@ -16,6 +16,7 @@ struct merge_options {
 	unsigned buffer_output; /* 1: output at end, 2: keep buffered */
 	unsigned renormalize : 1;
 	unsigned no_worktree : 1; /* do not touch worktree */
+	unsigned conflicts_in_index : 1; /* index will contain conflict hunks */
 	long xdl_opts;
 	int verbosity;
 	int detect_rename;
