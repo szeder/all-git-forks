0f4ae6296845dfc7246217d706fd0431bf65829e
Max Kirillov <max@max630.net>
2014-09-16 01:54:29 +0300

    TRY: remove setenv at all (SPARTA!!!)

--- environment.c
+++ environment.c
@@ -265,8 +265,6 @@ char *get_graft_file(void)
 
 int set_git_dir(const char *path)
 {
-	if (setenv(GIT_DIR_ENVIRONMENT, path, 1))
-		return error("Could not set GIT_DIR to '%s'", path);
 	setup_git_env();
 	return 0;
 }
