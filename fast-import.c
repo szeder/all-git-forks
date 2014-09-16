d3cbc03e2979b8d98f88b595771e90d81e26938c
Michael Haggerty <mhagger@alum.mit.edu>
2014-09-16 21:33:42 +0200

    dump_marks(): remove a redundant call to rollback_lock_file()
    When commit_lock_file() fails, it now always calls
    rollback_lock_file() internally, so there is no need to call that
    function here.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- fast-import.c
+++ fast-import.c
@@ -1828,10 +1828,8 @@ static void dump_marks(void)
 	}
 
 	if (commit_lock_file(&mark_lock)) {
-		int saved_errno = errno;
-		rollback_lock_file(&mark_lock);
 		failure |= error("Unable to commit marks file %s: %s",
-			export_marks_file, strerror(saved_errno));
+			export_marks_file, strerror(errno));
 		return;
 	}
 }
