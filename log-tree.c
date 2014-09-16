d0d96fe79e6543566c6d7facb8d94662f460ecf0
Harry Jeffery <harry@exec64.co.uk>
2014-09-16 17:37:38 +0000

    pretty: add %D format specifier
    Add a new format specifier, '%D' that is identical in behaviour to '%d',
    except that it does not include the ' (' prefix or ')' suffix provided
    by '%d'.
    
    Signed-off-by: Harry Jeffery <harry@exec64.co.uk>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- log-tree.c
+++ log-tree.c
@@ -179,14 +179,16 @@ static void show_children(struct rev_info *opt, struct commit *commit, int abbre
 }
 
 /*
- * The caller makes sure there is no funny color before
- * calling. format_decorations makes sure the same after return.
+ * The caller makes sure there is no funny color before calling.
+ * format_decorations_extended makes sure the same after return.
  */
-void format_decorations(struct strbuf *sb,
+void format_decorations_extended(struct strbuf *sb,
 			const struct commit *commit,
-			int use_color)
+			int use_color,
+			const char *prefix,
+			const char *separator,
+			const char *suffix)
 {
-	const char *prefix;
 	struct name_decoration *decoration;
 	const char *color_commit =
 		diff_get_color(use_color, DIFF_COMMIT);
@@ -196,20 +198,20 @@ void format_decorations(struct strbuf *sb,
 	decoration = lookup_decoration(&name_decoration, &commit->object);
 	if (!decoration)
 		return;
-	prefix = " (";
+	strbuf_addstr(sb, color_commit);
+	strbuf_addstr(sb, prefix);
 	while (decoration) {
-		strbuf_addstr(sb, color_commit);
-		strbuf_addstr(sb, prefix);
 		strbuf_addstr(sb, decorate_get_color(use_color, decoration->type));
 		if (decoration->type == DECORATION_REF_TAG)
 			strbuf_addstr(sb, "tag: ");
 		strbuf_addstr(sb, decoration->name);
 		strbuf_addstr(sb, color_reset);
-		prefix = ", ";
+		strbuf_addstr(sb, color_commit);
+		if (decoration->next)
+			strbuf_addstr(sb, separator);
 		decoration = decoration->next;
 	}
-	strbuf_addstr(sb, color_commit);
-	strbuf_addch(sb, ')');
+	strbuf_addstr(sb, suffix);
 	strbuf_addstr(sb, color_reset);
 }
 
