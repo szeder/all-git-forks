80ce8b3c1c81498494c70e15dad127d64cb26d8b
Michael Haggerty <mhagger@alum.mit.edu>
2014-09-16 21:33:45 +0200

    struct lock_file: declare some fields volatile
    The function remove_lock_file_on_signal() is used as a signal handler.
    It is not realistic to make the signal handler conform strictly to the
    C standard, which is very restrictive about what a signal handler is
    allowed to do.  But let's increase the likelihood that it will work:
    
    The lock_file_list global variable and several fields from struct
    lock_file are used by the signal handler.  Declare those values
    "volatile" to (1) force the main process to write the values to RAM
    promptly, and (2) prevent updates to these fields from being reordered
    in a way that leaves an opportunity for a jump to the signal handler
    while the object is in an inconsistent state.
    
    We don't mark the filename field volatile because that would prevent
    the use of strcpy(), and it is anyway unlikely that a compiler
    re-orders a strcpy() call across other expressions.  So in practice it
    should be possible to get away without "volatile" in the "filename"
    case.
    
    Suggested-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- lockfile.c
+++ lockfile.c
@@ -70,7 +70,7 @@
  * See Documentation/api-lockfile.txt for more information.
  */
 
-static struct lock_file *lock_file_list;
+static struct lock_file *volatile lock_file_list;
 
 static void remove_lock_file(void)
 {
