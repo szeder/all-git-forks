8a37d684491c590b4d4dfb6ef2b169754e9e6c34
Ronnie Sahlberg <sahlberg@google.com>
2014-11-03 11:52:09 -0800

    refs.c: move update_ref to refs.c
    This change moves update_ref() to the refs.c file since this function
    does not contain any backend specific code.
    
    Based on Ronnie Sahlberg's patch
    
    Signed-off-by: Ronnie Sahlberg <sahlberg@google.com>
    Signed-off-by: David Turner <dturner@twitter.com>

--- refs.c
+++ refs.c
@@ -1,3 +1,38 @@
 /*
  * Common refs code for all backends.
  */
+#include "cache.h"
+#include "refs.h"
+
+int update_ref(const char *msg, const char *refname,
+	       const unsigned char *new_sha1, const unsigned char *old_sha1,
+	       unsigned int flags, enum action_on_err onerr)
+{
+	struct ref_transaction *t;
+	struct strbuf err = STRBUF_INIT;
+
+	t = ref_transaction_begin(&err);
+	if (!t ||
+	    ref_transaction_update(t, refname, new_sha1, old_sha1,
+				   flags, msg, &err) ||
+	    ref_transaction_commit(t, &err)) {
+		const char *str = "update_ref failed for ref '%s': %s";
+
+		ref_transaction_free(t);
+		switch (onerr) {
+		case UPDATE_REFS_MSG_ON_ERR:
+			error(str, refname, err.buf);
+			break;
+		case UPDATE_REFS_DIE_ON_ERR:
+			die(str, refname, err.buf);
+			break;
+		case UPDATE_REFS_QUIET_ON_ERR:
+			break;
+		}
+		strbuf_release(&err);
+		return 1;
+	}
+	strbuf_release(&err);
+	ref_transaction_free(t);
+	return 0;
+}
