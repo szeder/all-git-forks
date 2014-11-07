669cc8595e5f42bcde61538997989f6e476d25a1
Max Kirillov <max@max630.net>
2014-11-08 00:35:33 +0200

    gh-6: add tests

---
+++ test-addtoend.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+
+TEST_DIR=`mktemp -d`
+
+t_git() {
+    git --work-tree="$TEST_DIR" --git-dir="$TEST_DIR/.git" "$@"
+}
+
+t_git init
+cat >"$TEST_DIR/file" <<EOF
+line1
+line2
+line3
+EOF
+t_git add file
+t_git commit -m 'file added'
+cat >>"$TEST_DIR/file" <<EOF
+line3
+line4
+EOF
+cat >"$TEST_DIR/file2" <<EOF
+line1
+line2
+line3
+line4
+line5
+EOF
+t_git add file file2
+t_git commit -m 'lines added, file2 added'
+cat >"$TEST_DIR/file2" <<EOF
+line1
+line2
+line3 line4
+line5
+EOF
+t_git commit -m '-newline' file2
+
+echo "Run GIT_DIR=$TEST_DIR/.git ./gitk"
+
