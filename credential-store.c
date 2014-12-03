5da4e9d65ffa4ff8b02aad9ceee8edfd8cb69169
Jonathan Nieder <jrnieder@gmail.com>
2014-12-02 21:20:44 -0800

    credentials: use message from lockfile API when locking ~/.git-credentials fails
    If writing $HOME/.git-credentials.lock (or locking another file
    specified with the --file option) fails due to the lock file already
    existing, chances are that it is because another git process already
    locked the file.  Use the message from the lockfile API that says so,
    to help the user to look for running git processes and remove the
    stray .lock file when it is safe.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- credential-store.c
+++ credential-store.c
@@ -55,8 +55,8 @@ static void print_line(struct strbuf *buf)
 static void rewrite_credential_file(const char *fn, struct credential *c,
 				    struct strbuf *extra)
 {
-	if (hold_lock_file_for_update(&credential_lock, fn, 0) < 0)
-		die_errno("unable to get credential storage lock");
+	hold_lock_file_for_update(&credential_lock, fn,
+				  LOCK_DIE_ON_ERROR | LOCK_OUTSIDE_REPOSITORY);
 	if (extra)
 		print_line(extra);
 	parse_credential_file(fn, c, NULL, print_line);
