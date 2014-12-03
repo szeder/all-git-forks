7b71bc2ddefb9a5db8f4fc4821420048ea7f9950
Jonathan Nieder <jrnieder@gmail.com>
2014-12-02 21:12:20 -0800

    add_to_alternates_file: respect GIT_OBJECT_DIRECTORY
    The objects directory is spelled as get_object_directory(), not
    git_path("objects").  Some other code still hard-codes the objects/
    directory name, so in the long term we may want to get rid of the
    pretense of support for GIT_OBJECT_DIRECTORY altogether, but this
    makes the code more consistent for now.
    
    While at it, split variable declarations from the rest of the
    function.  This makes the function a little easier to read, at the
    cost of some vertical space.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1_file.c
+++ sha1_file.c
@@ -403,9 +403,15 @@ void read_info_alternates(const char * relative_base, int depth)
 
 void add_to_alternates_file(const char *reference)
 {
-	struct lock_file *lock = xcalloc(1, sizeof(struct lock_file));
-	int fd = hold_lock_file_for_append(lock, git_path("objects/info/alternates"), LOCK_DIE_ON_ERROR);
-	char *alt = mkpath("%s\n", reference);
+	struct lock_file *lock;
+	int fd;
+	char *alt;
+
+	lock = xcalloc(1, sizeof(*lock));
+	fd = hold_lock_file_for_append(lock, mkpath("%s/info/alternates",
+						    get_object_directory()),
+				       LOCK_DIE_ON_ERROR);
+	alt = mkpath("%s\n", reference);
 	write_or_die(fd, alt, strlen(alt));
 	if (commit_lock_file(lock))
 		die("could not close alternates file");
