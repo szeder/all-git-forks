git/git

Teach 'git pull' to handle --rebase=interactive
Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

git-pull.sh
@@ -43,6 +43,7 @@ log_arg= verbosity= progress= recurse_submodules=
 merge_args=
 curr_branch=$(git symbolic-ref -q HEAD)
 curr_branch_short="${curr_branch#refs/heads/}"
+rebase_options=
 rebase=$(git config --bool branch.$curr_branch_short.rebase)
 dry_run=
 while :
@@ -105,7 +106,12 @@ do
 	-r|--r|--re|--reb|--reba|--rebas|--rebase)
 		rebase=true
 		;;
+	--rebase=i|--rebase=interactive)
+		rebase_options=-i
+		rebase=true
+		;;
 	--no-r|--no-re|--no-reb|--no-reba|--no-rebas|--no-rebase)
+		rebase_options=
 		rebase=false
 		;;
 	--recurse-submodules)
@@ -270,7 +276,7 @@ fi
 merge_name=$(git fmt-merge-msg $log_arg <"$GIT_DIR/FETCH_HEAD") || exit
 case "$rebase" in
 true)
-	eval="git-rebase $diffstat $strategy_args $merge_args"
+	eval="git-rebase $rebase_options $diffstat $strategy_args $merge_args"
 	eval="$eval --onto $merge_head ${oldremoteref:-$merge_head}"
 	;;
 *)

szeder/all-git-forks

git-pull.sh
@@ -122,6 +122,7 @@ do
 		;;
 	--no-r|--no-re|--no-reb|--no-reba|--no-rebas|--no-rebase)
 		rebase=false
+
 		;;
 	--recurse-submodules)
 		recurse_submodules=--recurse-submodules
@@ -153,6 +154,10 @@ do
 done
 
 case "$rebase" in
+i|interactive)
+	rebase=true
+	rebase_args=-i
+	;;
 preserve)
 	rebase=true
 	rebase_args=--preserve-merges
@@ -160,7 +165,7 @@ preserve)
 true|false|'')
 	;;
 *)
-	echo "Invalid value for --rebase, should be true, false, or preserve"
+	echo "Invalid value for --rebase, should be true, false, interactive or preserve"
 	usage
 	exit 1
 	;;