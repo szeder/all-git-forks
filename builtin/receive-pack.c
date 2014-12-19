84c70bd24a0082a7dee4bb56779743eda8da7672
Stefan Beller <sbeller@google.com>
2014-12-19 11:38:59 -0800

    receive-pack: move execute_commands_non_atomic before execute_commands
    This way we don't need to declare the function first and implement it
    later, but rather we put the implementation directly at the place where
    the function was declared.
    
    The reason I did not move it up in the first place is better readability
    of the diff as the execute_commands_non_atomic function is taking lots of
    code that was once in execute_commands.
    
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/receive-pack.c
+++ builtin/receive-pack.c
@@ -1047,10 +1047,6 @@ static void reject_updates_to_hidden(struct command *commands)
 	}
 }
 
-static void execute_commands_non_atomic(struct command *commands,
-				    struct shallow_info *si);
-
-
 static void execute_commands_atomic(struct command *commands,
 				    struct shallow_info *si)
 {
@@ -1109,47 +1105,6 @@ failure:
 		      "the reported refs above");
 }
 
-static void execute_commands(struct command *commands,
-			     const char *unpacker_error,
-			     struct shallow_info *si)
-{
-	struct command *cmd;
-	unsigned char sha1[20];
-	struct iterate_data data;
-
-	if (unpacker_error) {
-		for (cmd = commands; cmd; cmd = cmd->next)
-			cmd->error_string = "unpacker error";
-		return;
-	}
-
-	data.cmds = commands;
-	data.si = si;
-	if (check_everything_connected(iterate_receive_command_list, 0, &data))
-		set_connectivity_errors(commands, si);
-
-	reject_updates_to_hidden(commands);
-
-	if (run_receive_hook(commands, "pre-receive", 0)) {
-		for (cmd = commands; cmd; cmd = cmd->next) {
-			if (!cmd->error_string)
-				cmd->error_string = "pre-receive hook declined";
-		}
-		return;
-	}
-
-	check_aliased_updates(commands);
-
-	free(head_name_to_free);
-	head_name = head_name_to_free = resolve_refdup("HEAD", 0, sha1, NULL);
-
-	if (use_atomic) {
-		execute_commands_atomic(commands, si);
-	} else {
-		execute_commands_non_atomic(commands, si);
-	}
-}
-
 static void execute_commands_non_atomic(struct command *commands,
 					struct shallow_info *si)
 {
@@ -1193,6 +1148,47 @@ static void execute_commands_non_atomic(struct command *commands,
 		      "If there are errors, try to remove "
 		      "the reported refs above");
 	strbuf_release(&err);
+}
+
+static void execute_commands(struct command *commands,
+			     const char *unpacker_error,
+			     struct shallow_info *si)
+{
+	struct command *cmd;
+	unsigned char sha1[20];
+	struct iterate_data data;
+
+	if (unpacker_error) {
+		for (cmd = commands; cmd; cmd = cmd->next)
+			cmd->error_string = "unpacker error";
+		return;
+	}
+
+	data.cmds = commands;
+	data.si = si;
+	if (check_everything_connected(iterate_receive_command_list, 0, &data))
+		set_connectivity_errors(commands, si);
+
+	reject_updates_to_hidden(commands);
+
+	if (run_receive_hook(commands, "pre-receive", 0)) {
+		for (cmd = commands; cmd; cmd = cmd->next) {
+			if (!cmd->error_string)
+				cmd->error_string = "pre-receive hook declined";
+		}
+		return;
+	}
+
+	check_aliased_updates(commands);
+
+	free(head_name_to_free);
+	head_name = head_name_to_free = resolve_refdup("HEAD", 0, sha1, NULL);
+
+	if (use_atomic) {
+		execute_commands_atomic(commands, si);
+	} else {
+		execute_commands_non_atomic(commands, si);
+	}
 }
 
 static struct command **queue_command(struct command **tail,
