12432d38a58058a51cff07d80d3e6c0c1c215d5c
Michael Haggerty <mhagger@alum.mit.edu>
2015-02-08 17:14:03 +0100

    ref_transaction_delete(): check that old_sha1 is not null_sha1
    It makes no sense to delete a reference that is already known not to
    exist.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Reviewed-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -3703,6 +3703,8 @@ int ref_transaction_delete(struct ref_transaction *transaction,
 			   int flags, const char *msg,
 			   struct strbuf *err)
 {
+	if (old_sha1 && is_null_sha1(old_sha1))
+		die("BUG: delete called with old_sha1 set to zeros");
 	return ref_transaction_update(transaction, refname,
 				      null_sha1, old_sha1,
 				      flags, msg, err);
