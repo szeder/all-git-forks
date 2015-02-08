b01980d1e03d033768dadf543925cf641555d328
Michael Haggerty <mhagger@alum.mit.edu>
2015-02-08 17:14:00 +0100

    commit: add tests of commit races
    Committing involves the following steps:
    
    1. Determine the current value of HEAD (if any).
    2. Create the new commit object.
    3. Update HEAD.
    
    Please note that step 2 can take arbitrarily long, because it might
    involve the user editing a commit message.
    
    If a second process sneaks in a commit during step 2, then the first
    commit process should fail. This is usually done correctly, because
    step 3 verifies that HEAD still points at the same commit that it
    pointed to during step 1.
    
    However, if there is a race when creating an *orphan* commit, then the
    test in step 3 is skipped.
    
    Add tests for proper handling of such races. One of the new tests
    fails. It will be fixed in a moment.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ t/t7516-commit-races.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+# Copyright (c) 2014 Michael Haggerty <mhagger@alum.mit.edu>
+#
+
+test_description='git commit races'
+. ./test-lib.sh
+
+test_tick
+
+test_expect_success 'set up editor' '
+	cat >editor <<-\EOF &&
+	#!/bin/sh
+	git commit --allow-empty -m hare
+	echo tortoise >"$1"
+	EOF
+	chmod +x editor
+'
+
+test_expect_failure 'race to create orphan commit' '
+	test_must_fail env EDITOR=./editor git commit --allow-empty &&
+	git show -s --pretty=format:%s >subject &&
+	grep -q hare subject &&
+	test -z "$(git show -s --pretty=format:%P)"
+'
+
+test_expect_success 'race to create non-orphan commit' '
+	git checkout --orphan branch &&
+	git commit --allow-empty -m base &&
+	git rev-parse HEAD >base &&
+	test_must_fail env EDITOR=./editor git commit --allow-empty &&
+	git show -s --pretty=format:%s >subject &&
+	grep -q hare subject &&
+	git rev-parse HEAD^ >parent &&
+	test_cmp base parent
+'
+
+test_done
