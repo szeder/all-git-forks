01e93beed6111f1681346d9a89c0c01abe7c4c5a
Karsten Blees <blees@dcon.de>
2015-02-11 18:04:05 +0100

    Win32: replace MSVCRT's fstat() with a Win32-based implementation
    fstat() is the only stat-related CRT function for which we don't have a
    full replacement yet (and thus the only reason to stick with MSVCRT's
    'struct stat' definition).
    
    Fully implement fstat(), in preparation of implementing a POSIX 2013
    compatible 'struct stat' with nanosecond-precision file times.
    
    [jes: rebased to newest Git for Windows version]
    
    Signed-off-by: Karsten Blees <blees@dcon.de>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/mingw.c
+++ compat/mingw.c
@@ -775,16 +775,39 @@ int mingw_stat(const char *file_name, struct stat *buf)
 int mingw_fstat(int fd, struct stat *buf)
 {
 	HANDLE fh = (HANDLE)_get_osfhandle(fd);
+	DWORD avail;
+
 	if (fh == INVALID_HANDLE_VALUE) {
 		errno = EBADF;
 		return -1;
 	}
-	/* direct non-file handles to MS's fstat() */
-	if (GetFileType(fh) != FILE_TYPE_DISK)
-		return _fstati64(fd, buf);
 
-	if (!get_file_info_by_handle(fh, buf))
+	/* initialize stat fields */
+	memset(buf, 0, sizeof(*buf));
+	buf->st_nlink = 1;
+
+	switch (GetFileType(fh) & ~FILE_TYPE_REMOTE) {
+	case FILE_TYPE_DISK:
+		if (!get_file_info_by_handle(fh, buf))
+			break;
+		buf->st_mode = file_attr_to_st_mode(fdata.dwFileAttributes);
+		buf->st_size = fdata.nFileSizeLow |
+			(((off_t)fdata.nFileSizeHigh)<<32);
+		buf->st_atime = filetime_to_time_t(&(fdata.ftLastAccessTime));
+		buf->st_mtime = filetime_to_time_t(&(fdata.ftLastWriteTime));
+		buf->st_ctime = filetime_to_time_t(&(fdata.ftCreationTime));
 		return 0;
+
+	case FILE_TYPE_CHAR:
+		buf->st_mode = _S_IFCHR;
+		return 0;
+
+	case FILE_TYPE_PIPE:
+		buf->st_mode = _S_IFIFO;
+		if (PeekNamedPipe(fh, NULL, 0, NULL, &avail, NULL))
+			buf->st_size = avail;
+		return 0;
+	}
 	errno = EBADF;
 	return -1;
 }
