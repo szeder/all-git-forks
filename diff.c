1024ac67438d214b6e737ec28aa65e628c2ed725
Michael Haggerty <mhagger@alum.mit.edu>
2015-03-13 18:07:33 +0100

    stat_opt(): use convert_i() and improve error handling
    Check and report when arguments to the "--stat-*" options are not
    valid non-negative integers.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- diff.c
+++ diff.c
@@ -3426,72 +3426,65 @@ static int stat_opt(struct diff_options *options, const char **av)
 {
 	const char *arg = av[0];
 	char *end;
-	int width = options->stat_width;
-	int name_width = options->stat_name_width;
-	int graph_width = options->stat_graph_width;
-	int count = options->stat_count;
 	int argcount = 1;
 
 	if (!skip_prefix(arg, "--stat", &arg))
 		die("BUG: stat option does not begin with --stat: %s", arg);
 	end = (char *)arg;
 
-	switch (*arg) {
-	case '-':
-		if (skip_prefix(arg, "-width", &arg)) {
-			if (*arg == '=')
-				width = strtoul(arg + 1, &end, 10);
-			else if (!*arg && !av[1])
-				die("Option '--stat-width' requires a value");
-			else if (!*arg) {
-				width = strtoul(av[1], &end, 10);
-				argcount = 2;
-			}
-		} else if (skip_prefix(arg, "-name-width", &arg)) {
-			if (*arg == '=')
-				name_width = strtoul(arg + 1, &end, 10);
-			else if (!*arg && !av[1])
-				die("Option '--stat-name-width' requires a value");
-			else if (!*arg) {
-				name_width = strtoul(av[1], &end, 10);
-				argcount = 2;
-			}
-		} else if (skip_prefix(arg, "-graph-width", &arg)) {
-			if (*arg == '=')
-				graph_width = strtoul(arg + 1, &end, 10);
-			else if (!*arg && !av[1])
-				die("Option '--stat-graph-width' requires a value");
-			else if (!*arg) {
-				graph_width = strtoul(av[1], &end, 10);
-				argcount = 2;
-			}
-		} else if (skip_prefix(arg, "-count", &arg)) {
-			if (*arg == '=')
-				count = strtoul(arg + 1, &end, 10);
-			else if (!*arg && !av[1])
-				die("Option '--stat-count' requires a value");
-			else if (!*arg) {
-				count = strtoul(av[1], &end, 10);
-				argcount = 2;
-			}
-		}
-		break;
-	case '=':
+	if (*arg == '=') {
+		int width = options->stat_width;
+		int name_width = options->stat_name_width;
+		int count = options->stat_count;
+
 		width = strtoul(arg+1, &end, 10);
 		if (*end == ',')
 			name_width = strtoul(end+1, &end, 10);
 		if (*end == ',')
 			count = strtoul(end+1, &end, 10);
+		if (*end)
+			return 0;
+
+		options->stat_name_width = name_width;
+		options->stat_width = width;
+		options->stat_count = count;
+	} else if (*arg == '-') {
+		const char *value, *name;
+		int *var;
+
+		if (skip_prefix(arg, "-width", &arg)) {
+			name = "stat-width";
+			var = &options->stat_width;
+		} else if (skip_prefix(arg, "-name-width", &arg)) {
+			name = "stat-name-width";
+			var = &options->stat_name_width;
+		} else if (skip_prefix(arg, "-graph-width", &arg)) {
+			name = "stat-graph-width";
+			var = &options->stat_graph_width;
+		} else if (skip_prefix(arg, "-count", &arg)) {
+			name = "stat-count";
+			var = &options->stat_count;
+		} else {
+			return 0;
+		}
+
+		if (*arg == '=')
+			value = arg + 1;
+		else if (*arg)
+			return 0;
+		else {
+			value = av[argcount++];
+			if (!value)
+				die("Option '--%s' requires a value", name);
+		}
+
+		if (convert_i(value, 10, var))
+			die("%s must be a non-negative integer", name);
+	} else if (*arg) {
+		return 0;
 	}
 
-	/* Important! This checks all the error cases! */
-	if (*end)
-		return 0;
 	options->output_format |= DIFF_FORMAT_DIFFSTAT;
-	options->stat_name_width = name_width;
-	options->stat_graph_width = graph_width;
-	options->stat_width = width;
-	options->stat_count = count;
 	return argcount;
 }
 
