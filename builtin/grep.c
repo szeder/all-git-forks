81cbb0f9554b7dc03c2f87b7e412e89d0e1a8d2d
Michael Haggerty <mhagger@alum.mit.edu>
2015-03-13 21:41:07 +0100

    builtin/grep.c: context_callback(): use convert_i()
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- builtin/grep.c
+++ builtin/grep.c
@@ -4,6 +4,7 @@
  * Copyright (c) 2006 Junio C Hamano
  */
 #include "cache.h"
+#include "numparse.h"
 #include "blob.h"
 #include "tree.h"
 #include "commit.h"
@@ -536,17 +537,14 @@ static int context_callback(const struct option *opt, const char *arg,
 {
 	struct grep_opt *grep_opt = opt->value;
 	int value;
-	const char *endp;
 
 	if (unset) {
 		grep_opt->pre_context = grep_opt->post_context = 0;
 		return 0;
 	}
-	value = strtol(arg, (char **)&endp, 10);
-	if (*endp) {
-		return error(_("switch `%c' expects a numerical value"),
+	if (convert_i(arg, 10, &value))
+		return error(_("option -%c requires a non-negative integer value"),
 			     opt->short_name);
-	}
 	grep_opt->pre_context = grep_opt->post_context = value;
 	return 0;
 }
