68bb70863df70ab407a1bdb6c01aef83cf275643
Michael Haggerty <mhagger@alum.mit.edu>
2015-03-15 15:10:07 +0100

    option_cat_blob_fd(): use convert_i() directly rather than ulong_arg()
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- fast-import.c
+++ fast-import.c
@@ -3185,10 +3185,8 @@ static void option_export_marks(const char *marks)
 
 static void option_cat_blob_fd(const char *fd)
 {
-	unsigned long n = ulong_arg("--cat-blob-fd", fd);
-	if (n > (unsigned long) INT_MAX)
-		die("--cat-blob-fd cannot exceed %d", INT_MAX);
-	cat_blob_fd = (int) n;
+	if (convert_i(fd, 10, &cat_blob_fd))
+		die("--cat-blob-fd: argument must be a non-negative integer");
 }
 
 static void option_export_pack_edges(const char *edges)
