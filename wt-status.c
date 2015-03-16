a782cb197516cd09333fa02e2aeb8b4e4d9d753f
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2015-03-16 20:56:46 +0700

    Resurrect "diff-lib.c: adjust position of i-t-a entries in diff"
    The original commit d95d728aba06a34394d15466045cbdabdada58a2 was
    reverted in commit 78cc1a540ba127b13f2f3fd531777b57f3a9cd46 because we
    were (and still are) not ready for a new world order. A lot more
    investigation must be done to see what is impacted. See the 78cc1a5 for
    details.
    
    This patch takes a smaller and safer step. The new behavior is
    controlled by SHIFT_INTENT_TO_ADD flag. We can gradually move more diff
    users to the new behavior after we are sure it's safe to do so. This
    flag is exposed to outside as "--shift-ita".
    
    "git status" sets this flag unconditionally to display i-t-a entries
    correctly. See d95d728 for details about "git status" and i-t-a entries.

--- wt-status.c
+++ wt-status.c
@@ -497,6 +497,7 @@ static void wt_status_collect_changes_worktree(struct wt_status *s)
 	setup_revisions(0, NULL, &rev, NULL);
 	rev.diffopt.output_format |= DIFF_FORMAT_CALLBACK;
 	DIFF_OPT_SET(&rev.diffopt, DIRTY_SUBMODULES);
+	DIFF_OPT_SET(&rev.diffopt, SHIFT_INTENT_TO_ADD);
 	if (!s->show_untracked_files)
 		DIFF_OPT_SET(&rev.diffopt, IGNORE_UNTRACKED_IN_SUBMODULES);
 	if (s->ignore_submodule_arg) {
@@ -520,6 +521,7 @@ static void wt_status_collect_changes_index(struct wt_status *s)
 	setup_revisions(0, NULL, &rev, &opt);
 
 	DIFF_OPT_SET(&rev.diffopt, OVERRIDE_SUBMODULE_CONFIG);
+	DIFF_OPT_SET(&rev.diffopt, SHIFT_INTENT_TO_ADD);
 	if (s->ignore_submodule_arg) {
 		handle_ignore_submodules_arg(&rev.diffopt, s->ignore_submodule_arg);
 	} else {
