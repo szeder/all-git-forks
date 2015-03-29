457618b954582480328f3a14047c2361c22c1153
Max Kirillov <max@max630.net>
2015-03-30 00:15:32 +0300

    TEST rc: rewrite tests to automatically compare all combinations
    TODO: add descriptive comment

--- t/t4099-diff-merge-with-base.sh
+++ t/t4099-diff-merge-with-base.sh
@@ -90,82 +90,34 @@ test_expect_success setup '
 	git branch merge
 '
 
-test_expect_success 'diff with mergebase shows discarded change from parent 2 in merged file' '
-	git diff --cc merge branch1 branch2 mergebase -- long/win1 >actual &&
-	test -s actual
-'
+for fn in win1 win2 merge delete base only1 only2 only1discard only2discard; do
+	if git diff --cc merge branch1 branch2 mergebase -- long/$fn | grep -q '^[ +-]\{3\}2\(change[12]|merge\)\?$'
+	then
+		test_expect_success "diff --cc contains short/$fn" '
+			git diff --cc merge branch1 branch2 mergebase -- short/'"$fn"' >actual &&
+			test -s actual
+		'
+	else
+		test_expect_success "diff --cc does not contain short/$fn" '
+			git diff --cc merge branch1 branch2 mergebase -- short/'"$fn"' >actual &&
+			! test -s actual
+		'
+	fi
+done
 
-test_expect_success 'diff with mergebase shows discarded change from parent 1 in merged file' '
-	git diff --cc merge branch1 branch2 mergebase -- long/win2 >actual &&
-	test -s actual
-'
-
-test_expect_success 'diff with mergebase shows fully discarded file from parent 2' '
-	git diff --cc merge branch1 branch2 mergebase -- short/win1 >actual &&
-	test -s actual
-'
-
-test_expect_success 'diff with mergebase shows fully discarded file from parent 1' '
-	git diff --cc merge branch1 branch2 mergebase -- short/win2 >actual &&
-	test -s actual
-'
-
-test_expect_success 'regression test for diff --cc -- short' '
-	cat >expect <<-\EOF &&
-	diff --cc short/base
-	index 8d4717e,ef72a3c,01e79c3..01e79c3
-	--- a/short/base
-	+++ b/short/base
-	@@@@ -1,3 -1,3 -1,3 +1,3 @@@@
-	   1
-	-  2change1
-	 - 2change2
-	++ 2
-	   3
-	diff --cc short/delete
-	index 8d4717e,ef72a3c,01e79c3..2b2f2e1
-	--- a/short/delete
-	+++ b/short/delete
-	@@@@ -1,3 -1,3 -1,3 +1,2 @@@@
-	   1
-	-  2change1
-	 - 2change2
-	  -2
-	   3
-	diff --cc short/merge
-	index 8d4717e,ef72a3c,01e79c3..c5ce1e6
-	--- a/short/merge
-	+++ b/short/merge
-	@@@@ -1,3 -1,3 -1,3 +1,3 @@@@
-	   1
-	-  2change1
-	 - 2change2
-	  -2
-	+++2merged
-	   3
-	diff --cc short/win1
-	index 8d4717e,ef72a3c,01e79c3..8d4717e
-	--- a/short/win1
-	+++ b/short/win1
-	@@@@ -1,3 -1,3 -1,3 +1,3 @@@@
-	   1
-	 - 2change2
-	  -2
-	 ++2change1
-	   3
-	diff --cc short/win2
-	index 8d4717e,ef72a3c,01e79c3..ef72a3c
-	--- a/short/win2
-	+++ b/short/win2
-	@@@@ -1,3 -1,3 -1,3 +1,3 @@@@
-	   1
-	-  2change1
-	  -2
-	+ +2change2
-	   3
-	EOF
-	git diff --cc merge branch1 branch2 mergebase -- short >actual &&
-	compare_diff_patch expect actual
-'
+for fn in win1 win2 merge delete base only1 only2 only1discard only2discard; do
+	if git diff -c merge branch1 branch2 mergebase -- long/$fn | grep -q '^[ +-]\{3\}2\(change[12]|merge\)\?$'
+	then
+		test_expect_success "diff -c contains short/$fn" '
+			git diff -c merge branch1 branch2 mergebase -- short/'"$fn"' >actual &&
+			test -s actual
+		'
+	else
+		test_expect_success "diff -c does not contain short/$fn" '
+			git diff -c merge branch1 branch2 mergebase -- short/'"$fn"' >actual &&
+			! test -s actual
+		'
+	fi
+done
 
 test_done
