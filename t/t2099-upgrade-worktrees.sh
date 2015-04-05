712cec5841c3921d2dc7d4101aa4ab3263271615
Max Kirillov <max@max630.net>
2015-04-06 01:09:49 +0300

    fixup: add config.wotktree handling

--- t/t2099-upgrade-worktrees.sh
+++ t/t2099-upgrade-worktrees.sh
@@ -24,4 +24,35 @@ test_expect_success 'checkout busy branch' '
     test_must_fail git checkout newmaster
 '
 
+wt_here_orig="$new_trash/here"
+wt_root="$new_trash/wt_root"
+wt_here="$new_trash/wt_here"
+
+test_expect_success 'verify core.worktree' '
+    printf "%s\n" "$wt_here_orig" >wt_here_expect &&
+    ( cd here && git config core.worktree ) >wt_here_actual &&
+    test_cmp wt_here_expect wt_here_actual
+'
+
+test_expect_success 'set variables' '
+    mkdir -p "$wt_root" "$wt_here" &&
+    git config config.var configvar &&
+    git config core.worktree "$wt_root" &&
+    ( cd here && git config core.worktree "$wt_here" )
+'
+
+test_expect_success 'verify variables' '
+    echo configvar >configvar_expect &&
+    git config config.var >configvar_actual &&
+    printf "%s\n" "$wt_root" >wt_root_expect &&
+    git config core.worktree >wt_root_actual &&
+    printf "%s\n" "$wt_here" >wt_here_expect &&
+    ( cd here && git config core.worktree ) >wt_here_actual &&
+    ( cd here && git config config.var ) >configvar_here_actual &&
+    test_cmp configvar_expect configvar_actual &&
+    test_cmp configvar_expect configvar_here_actual &&
+    test_cmp wt_here_expect wt_here_actual &&
+    test_cmp wt_root_expect wt_root_actual
+'
+
 test_done
