348cdf6e6e05677afca089965b726e25e97696f5
Max Kirillov <max@max630.net>
2015-04-08 00:39:51 +0300

    TRY: git push

---
+++ t/t5598-push-merge-ui.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+test_description='test push-merge feature UI experiments'
+
+. ./test-lib.sh
+
+test_expect_success 'setup upstream' '
+	git config receive.denyCurrentBranch ignore &&
+	test_commit init
+'
+
+test_expect_success 'setup clone' '
+	git clone --single-branch .git clone &&
+	(
+		cd clone &&
+		test_commit commit1_client
+	)
+'
+
+test_expect_success 'advance upstream' '
+	test_commit commit1
+'
+
+commit1_hash=$(git rev-parse commit1)
+
+test_expect_failure 'push-merge smoke: see ref in push-hook' '
+	git reset --hard commit1 &&
+	(
+		cd clone &&
+                cat >.git/hooks/pre-push <<-"EOF" &&
+#!/bin/sh
+
+set -e
+
+git log "$commit1_hash"
+EOF
+                chmod +x .git/hooks/pre-push &&
+                export commit1_hash &&
+                git push origin master:master
+	)
+'
+
+test_done
