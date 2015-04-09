0d4459b911d5898278389fe788a5f81c8d5a41db
Max Kirillov <max@max630.net>
2015-04-09 05:09:35 +0300

    TRY: use low-level commands to avoid touching HEAD

--- git-push-merge.sh
+++ git-push-merge.sh
@@ -8,16 +8,6 @@ remote="$2"
 remote_remote_ref="$3"
 remote_local_ref="$4"
 
-tmpdir=$(mktemp -d)
-if test -z "$tmpdir" || test "$tmpdir" = /
-then
-	echo "Invalid tmpdir" >/dev/stderr
-	exit 1
-fi
-old_head=$(git rev-parse HEAD)
-trap 'git reset --soft "$old_head" && rm -rf "$tmpdir"' 0
-GIT_INDEX_FILE="$tmpdir/index"
-
 git fetch "$remote" "$remote_remote_ref:$remote_local_ref"
 
 if git merge-base --is-ancestor HEAD "$remote_local_ref"
@@ -49,7 +39,16 @@ secondParent)
 	;;
 esac
 
-git reset --quiet --mixed "$remote_local_ref"
-# TODO: nice message
-git merge --edit -Xindex-only "$old_head"
-git rev-parse HEAD >&3
+tmpdir=$(mktemp -d)
+if test -z "$tmpdir" || test "$tmpdir" = /
+then
+	echo "Invalid tmpdir" >/dev/stderr
+	exit 1
+fi
+trap 'rm -rf "$tmpdir"' 0
+GIT_INDEX_FILE="$tmpdir/index"
+
+git read-tree "$remote_local_ref"
+git recursive-merge --index-only $(git merge-base -a "$remote_local_ref" HEAD) -- "$remote_local_ref" HEAD
+new_head=$(git write-tree | git-commit-tree)
+echo "$new_head" >&3
