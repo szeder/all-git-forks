1e56ee5c4581e18e6751b698306e764201c4efc7
Max Kirillov <max@max630.net>
2015-04-11 21:56:15 +0300

    Revert "CHANGE: use file instead of fd#3 for passing head"
    This reverts commit 9b40fa31be57987060a5a0a9aa61a7cb4035756c.

--- git-push-merge.sh
+++ git-push-merge.sh
@@ -8,7 +8,6 @@ remote="$2"
 remote_remote_ref="$3"
 remote_local_ref="$4"
 
-push_merge_head_file=$(git rev-parse --git-path PUSH_MERGE_HEAD)
 git fetch "$remote" "$remote_remote_ref:$remote_local_ref"
 
 if git merge-base --is-ancestor HEAD "$remote_local_ref"
@@ -24,7 +23,7 @@ diverged)
 	if git merge-base --is-ancestor "$remote_local_ref" HEAD
 	then
 		# can be pushed cleanly; no need to merge
-		git rev-parse HEAD >"$push_merge_head_file"
+		git rev-parse HEAD >&3
 		exit 0
 	fi
 	;;
@@ -52,4 +51,4 @@ GIT_INDEX_FILE="$tmpdir/index"
 git read-tree "$remote_local_ref"
 git recursive-merge --index-only $(git merge-base -a "$remote_local_ref" HEAD) -- "$remote_local_ref" HEAD
 new_head=$(git write-tree | git-commit-tree)
-echo "$new_head" >"$push_merge_head_file"
+echo "$new_head" >&3
