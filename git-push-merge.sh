bf1f5d7c9129984eae5414e182ee8d34a42a98eb
Max Kirillov <max@max630.net>
2015-04-12 15:20:21 +0300

    IMPL: tune push-merge to work

--- git-push-merge.sh
+++ git-push-merge.sh
@@ -8,16 +8,22 @@ remote="$2"
 remote_remote_ref="$3"
 remote_local_ref="$4"
 
+push_merge_head_file=$(git rev-parse --git-path PUSH_MERGE_HEAD)
+
 tmpdir=$(mktemp -d)
 if test -z "$tmpdir" || test "$tmpdir" = /
 then
 	echo "Invalid tmpdir" >/dev/stderr
 	exit 1
 fi
-old_head=$(git rev-parse HEAD)
-trap 'git reset --soft "$old_head" && rm -rf "$tmpdir"' 0
-GIT_INDEX_FILE="$tmpdir/index"
+old_head=$(git rev-parse --symbolic-full-name HEAD)
+if test "$old_head" = HEAD
+then
+	old_head=$(git rev-parse HEAD)
+fi
+trap 'git update-ref --no-deref HEAD "$old_head" && rm -rf "$tmpdir"' 0
 
+# TODO: activate it with parameter; should be run only between push attempts
 git fetch "$remote" "$remote_remote_ref:$remote_local_ref"
 
 if git merge-base --is-ancestor HEAD "$remote_local_ref"
@@ -32,8 +38,9 @@ in
 diverged)
 	if git merge-base --is-ancestor "$remote_local_ref" HEAD
 	then
+		echo "diverged: can ff" >/dev/stderr
 		# can be pushed cleanly; no need to merge
-		git rev-parse HEAD >&3
+		echo "$old_head" >"$push_merge_head_file"
 		exit 0
 	fi
 	;;
@@ -49,7 +56,12 @@ secondParent)
 	;;
 esac
 
-git reset --quiet --mixed "$remote_local_ref"
+echo "push_merge: merging" >/dev/stderr
+GIT_INDEX_FILE="$tmpdir/index"
+export GIT_INDEX_FILE
+git update-ref --no-deref HEAD "$remote_local_ref"'^'
+git read-tree HEAD
 # TODO: nice message
-git merge --edit -Xindex-only "$old_head"
-git rev-parse HEAD >&3
+echo "push_merge: merging2" >/dev/stderr
+git merge --log --edit -Xindex-only "$old_head"
+git rev-parse HEAD "$push_merge_head_file"
