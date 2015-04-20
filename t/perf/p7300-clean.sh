e5c3441151fb86bf37f18f3362123958733872bb
Thomas Gummerer <t.gummerer@gmail.com>
2015-04-20 23:06:01 +0200

    p7300: convert to modern test form
    Using --setup and --cleanup for the performance tests allows for a more
    accurate measurement of how much time clean will actually take, instead
    of factoring in the time rm and cp need for setting up the test.
    
    Signed-off-by: Thomas Gummerer <t.gummerer@gmail.com>

--- t/perf/p7300-clean.sh
+++ t/perf/p7300-clean.sh
@@ -20,18 +20,18 @@ test_expect_success 'setup untracked directory with many sub dirs' '
 	done
 '
 
-test_perf 'clean many untracked sub dirs, check for nested git' '
-	rm -rf clean_test_dir/50000_sub_dirs_cpy &&
-	cp -r 50000_sub_dirs clean_test_dir/50000_sub_dirs_cpy &&
-	git clean -q -f -d  clean_test_dir/ &&
-	test_dir_is_empty clean_test_dir
+test_perf --setup 'rm -rf clean_test_dir/50000_sub_dirs_cpy &&
+		cp -r 50000_sub_dirs clean_test_dir/50000_sub_dirs_cpy' \
+	  --cleanup 'test_dir_is_empty clean_test_dir' \
+	'clean many untracked sub dirs, check for nested git' '
+	git clean -q -f -d  clean_test_dir/
 '
 
-test_perf 'clean many untracked sub dirs, ignore nested git' '
-	rm -rf clean_test_dir/50000_sub_dirs_cpy &&
-	cp -r 50000_sub_dirs clean_test_dir/50000_sub_dirs_cpy &&
-	git clean -q -f -f -d  clean_test_dir/ &&
-	test_dir_is_empty clean_test_dir
+test_perf --setup 'rm -rf clean_test_dir/50000_sub_dirs_cpy &&
+		cp -r 50000_sub_dirs clean_test_dir/50000_sub_dirs_cpy' \
+	  --cleanup 'test_dir_is_empty clean_test_dir' \
+	  'clean many untracked sub dirs, check for nested git' '
+	git clean -q -f -f -d  clean_test_dir/
 '
 
 test_done
