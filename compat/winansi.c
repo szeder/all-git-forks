513084c593f6e2f2f69dcb2af462a638b4b3e3fa
Max Kirillov <max@max630.net>
2015-05-24 23:19:12 +0300

    Fix conditiona for substute CONSOLE_FONT_INFOEX definitions
    CONSOLE_FONT_INFOEX and using it GetCurrentConsoleFontEx
    Is a part of Vista support [1]. The function presence is checked dynamically,
    but to compile its call there must be the structure definition. So define
    it if the header version is less than Vista. This should allows both code
    built on newer and older system work.
    
    The presence of the definitions in various mingw versions is:
    
    * for mingw32 never defined
    * for mingw64-1 never defined
    * for mingw64-2 defined according to version [1]
    * for mingw64>2 always defined [2]
    
    Fix the "#if" condition so that it corresponds this.
    
    Also, debian port of mingw-2.0 (for wheezy) differs from upstream
    mingw-2.0 - it defines the unconditionally [3]. This is too
    specific rule - just add the option to override this.
    
    [1] git://git.code.sf.net/p/mingw-w64/mingw-w64@cf6afc57179
    [2] git://git.code.sf.net/p/mingw-w64/mingw-w64@a828c093245
    [3] git://anonscm.debian.org/collab-maint/mingw-w64.git@ebde5d0801
    
    Helped-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/winansi.c
+++ compat/winansi.c
@@ -21,8 +21,9 @@ static int non_ascii_used = 0;
 static HANDLE hthread, hread, hwrite;
 static HANDLE hconsole1, hconsole2;
 
-#ifdef __MINGW32__
-#if !defined(__MINGW64_VERSION_MAJOR) || __MINGW64_VERSION_MAJOR < 5
+#if !defined(__MINGW64_VERSION_MAJOR) \
+    || __MINGW64_VERSION_MAJOR < 2 \
+    || __MINGW64_VERSION_MAJOR == 2 && _WIN32_WINNT < 0x0600 && !defined(SKIP_CONSOLE_FONT_INFOEX)
 typedef struct _CONSOLE_FONT_INFOEX {
 	ULONG cbSize;
 	DWORD nFont;
@@ -31,7 +32,6 @@ typedef struct _CONSOLE_FONT_INFOEX {
 	UINT FontWeight;
 	WCHAR FaceName[LF_FACESIZE];
 } CONSOLE_FONT_INFOEX, *PCONSOLE_FONT_INFOEX;
-#endif
 #endif
 
 typedef BOOL (WINAPI *PGETCURRENTCONSOLEFONTEX)(HANDLE, BOOL,
