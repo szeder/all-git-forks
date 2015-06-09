8a1b12c13d27cad8bde54a4f95c4b4eb4797f9e8
brian m. carlson <sandals@crustytoothpaste.net>
2015-06-09 16:28:32 +0000

    Add a utility function to make parsing hex values easier.
    get_oid_hex is already available for parsing hex object IDs into struct
    object_id, but parsing code still must hard-code the number of bytes
    read.  Introduce parse_oid_hex, which accepts an optional length, and
    also returns the number of bytes parsed on success, or 0 on failure.
    This makes it easier for code not to assume fixed values when parsing,
    and to move to larger hash functions in the future.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache.h
+++ cache.h
@@ -1012,6 +1012,15 @@ extern int for_each_abbrev(const char *prefix, each_abbrev_fn, void *);
 extern int get_sha1_hex(const char *hex, unsigned char *sha1);
 extern int get_oid_hex(const char *hex, struct object_id *sha1);
 
+/*
+ * Like get_oid_hex, but accepts an optional length argument, which may be -1
+ * if the string is terminated by a non-hex character.  As with get_oid_hex,
+ * reading stops if a NUL is encountered.  Returns the number of characters
+ * read (40) on success and 0 on failure.  This is designed to be easier to
+ * use for parsing data than get_oid_hex.
+ */
+extern int parse_oid_hex(const char *hex, int len, struct object_id *oid);
+
 extern char *sha1_to_hex(const unsigned char *sha1);	/* static buffer result! */
 extern char *oid_to_hex(const struct object_id *oid);	/* same static buffer as sha1_to_hex */
 extern int read_ref_full(const char *refname, int resolve_flags,
