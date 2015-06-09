4e6ec29f016690ccf541d8f4feec229633570051
Louis Stuber <stuberl@ensimag.grenoble-inp.fr>
2015-06-09 19:15:58 +0200

    bisect: change read_bisect_terms parameters
    The function reads BISECT_TERMS and stores it at the adress given in
    parameters (instead of global variables name_bad and name_good).
    
    This allows to use the function outside bisect.c.
    
    Signed-off-by: Antoine Delaite <antoine.delaite@ensimag.grenoble-inp.fr>
    Signed-off-by: Louis Stuber <stuberl@ensimag.grenoble-inp.fr>

--- bisect.c
+++ bisect.c
@@ -904,27 +904,26 @@ static void show_diff_tree(const char *prefix, struct commit *commit)
 }
 
 /*
- * The terms used for this bisect session are stored in
- * BISECT_TERMS: it can be bad/good or new/old.
- * We read them and store them to adapt the messages
- * accordingly. Default is bad/good.
+ * The terms used for this bisect session are stored in BISECT_TERMS.
+ * We read them and store them to adapt the messages accordingly.
+ * Default is bad/good.
  */
-void read_bisect_terms(void)
+void read_bisect_terms(const char **read_bad, const char **read_good)
 {
 	struct strbuf str = STRBUF_INIT;
 	const char *filename = git_path("BISECT_TERMS");
 	FILE *fp = fopen(filename, "r");
 
 	if (!fp) {
-		name_bad = "bad";
-		name_good = "good";
+		*read_bad = "bad";
+		*read_good = "good";
 		strbuf_release(&str);
 		return;
 	} else {
 		strbuf_getline(&str, fp, '\n');
-		name_bad = strbuf_detach(&str, NULL);
+		*read_bad = strbuf_detach(&str, NULL);
 		strbuf_getline(&str, fp, '\n');
-		name_good = strbuf_detach(&str, NULL);
+		*read_good = strbuf_detach(&str, NULL);
 	}
 	strbuf_release(&str);
 	fclose(fp);
@@ -946,7 +945,7 @@ int bisect_next_all(const char *prefix, int no_checkout)
 	const unsigned char *bisect_rev;
 	char bisect_rev_hex[GIT_SHA1_HEXSZ + 1];
 
-	read_bisect_terms();
+	read_bisect_terms(&name_bad, &name_good);
 	if (read_bisect_refs())
 		die("reading bisect refs failed");
 
