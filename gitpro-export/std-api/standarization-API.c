3ecc6965c40e46c8b78f8b2e1c05e7da60f944dd
brunosantiagovazquez <brunosantiagovazquez@gmail.com>
2015-06-12 22:20:45 +0200

    #40 Añadir soporte a importacion de datos de GanttProject

--- gitpro-export/std-api/standarization-API.c
+++ gitpro-export/std-api/standarization-API.c
@@ -1,10 +1,32 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "standarization-API.h"
 
 #define std_file "standard-file"
 
 int check = 0;
+
+char *tid;
+char *tname;
+char *tstate;
+char *tdesc;
+char *tnotes;
+char *test_ini;
+char *test_end;
+char *tini;
+char *tend;
+char *tprior;
+char *ttype;
+char *test_time;
+char *ttime;
+
+char *uname;
+char *urole;
+
+char *aname;
+char *atid;
 
 /*	Private functions 	*/
 void write(char *txt,char *var){
@@ -21,74 +43,91 @@ void write(char *txt,char *var){
 
 /* 	Public 	functions 	*/
 
-/* TASK DATA STANDARIZATION */
-void std_id(char *id){
-	write("id\n%s\n",id);
+void set_id(char *id){ tid = strdup(id); }
+void set_name(char *name){ tname = strdup(name); }
+void set_state(char *state){ tstate = strdup(state); }
+void set_desc(char *desc){ tdesc = strdup(desc); }
+void set_notes(char *notes){ tnotes = strdup(notes); }
+void set_est_ini(char *est_ini){ test_ini = strdup(est_ini); }
+void set_est_end(char *est_end){ test_end = strdup(est_end); }
+void set_ini(char *ini){ tini = strdup(ini); }
+void set_end(char *end){ tend = strdup(end); }
+void set_priority(char *prior){ tprior = strdup(prior); }
+void set_type(char *type){ ttype = strdup(type); }
+void set_est_time(char *est_time){ test_time = strdup(est_time); }
+void set_time(char *time){ ttime = strdup(time); }
+
+void set_uname(char *username){ uname = strdup(username); }
+void set_urole(char *rolename){ urole = strdup(rolename); }
+
+void set_auname(char *name){ aname = strdup(name); }
+void set_atid(char *id){ atid = strdup(id); }
+
+void std_task()
+{
+	if(tid!=NULL){ 
+		write("id\n%s\n",tid); free(tid);
+	}	
+	if(tname!=NULL){ 
+		write("name\n%s\n",tname); free(tname);
+	}
+	if(tstate!=NULL){ 
+		write("state\n%s\n",tstate); free(tstate);
+	}
+	if(tdesc!=NULL){ 
+		write("desc\n%s\n",tdesc); free(tdesc);
+	}
+	if(tnotes!=NULL){ 
+		write("notes\n%s\n",tnotes); free(tnotes);
+	}
+	if(test_ini!=NULL){ 
+		write("est-date-ini\n%s\n",test_ini); free(test_ini);
+	}
+	if(test_end!=NULL){ 
+		write("est-date-end\n%s\n",test_end); free(test_end);
+	}
+	if(tini!=NULL){ 
+		write("real-date-ini\n%s\n",tini); free(tini);
+	}
+	if(tend!=NULL){ 
+		write("real-date-end\n%s\n",tend); free(tend);
+	}
+	if(tprior!=NULL){ 
+		write("priority\n%s\n",tprior); free(tprior);
+	}
+	if(ttype!=NULL){ 
+		write("type\n%s\n",ttype); free(ttype);
+	}
+	if(ttime!=NULL){ 
+		write("time\n%s\n",ttime); free(ttime);
+	}
+	if(test_time!=NULL){ 
+		write("est-time\n%s\n",test_time); free(test_time);
+	}
+	tid = NULL;tname = NULL;tstate = NULL;tdesc = NULL;
+	tnotes = NULL;test_ini = NULL;test_end = NULL;
+	tini = NULL;tend = NULL;tprior = NULL;ttype = NULL;
+	ttime = NULL;test_time = NULL;
 }
 
-void std_name(char *name){
-	write("name\n%s\n",name);
+void std_user()
+{
+	if(uname!=NULL){
+		write("uname\n%s\n",uname); free(uname);
+	}
+	if(urole!=NULL){
+		write("urole\n%s\n",urole); free(urole);
+	}
+	uname = NULL;urole = NULL;
 }
 
-void std_state(char *state){
-	write("state\n%s\n",state);
-}
-
-void std_desc(char *desc){
-	write("desc\n%s\n",desc);
-}
-
-void std_notes(char *notes){
-	write("notes\n%s\n",notes);
-}
-
-void std_est_ini(char *est_ini){
-	write("est-date-ini\n%s\n",est_ini);
-}
-
-void std_est_end(char *est_end){
-	write("est-date-end\n%s\n",est_end);
-}
-
-void std_ini(char *ini){
-	write("real-date-ini\n%s\n",ini);
-}
-
-void std_end(char *end){
-	write("real-date-end\n%s\n",end);
-}
-
-void std_priority(char *prior){
-	write("priority\n%s\n",prior);
-}
-
-void std_type(char *type){
-	write("type\n%s\n",type);
-}
-
-void std_est_time(char *est_time){
-	write("est-time\n%s\n",est_time);
-}
-
-void std_time(char *time){
-	write("time\n%s\n",time);
-}
-
-/* USER DATA STANDARIZATION */
-
-void std_uname(char *uname){
-	write("uname\n%s\n",uname);
-}
-
-void std_urole(char *urole){
-	write("urole\n%s\n",urole);
-}
-
-/* ASIG DATA STANDARIZATION */
-void std_atid(char *atid){
-	write("atid\n%s\n",atid);
-}
-
-void std_auname(char *auname){
-	write("auname\n%s\n",auname);
+void std_asig()
+{
+	if(aname!=NULL){
+		write("auname\n%s\n",aname); free(aname);
+	}
+	if(atid!=NULL){
+		write("atid\n%s\n",atid); free(atid);
+	}
+	aname = NULL;atid = NULL;
 }
--- gitpro-export/std-api/standarization-API.h
+++ gitpro-export/std-api/standarization-API.h
@@ -1,4 +1,4 @@
-
+/* This constants is only to make easier analyzer implementation */
 #define TASK -3
 #define USER -2
 #define ASIG -1
@@ -16,37 +16,42 @@
 #define END 8
 #define PRIOR 9
 #define TYPE 10
-#define EST_TIME 11
-#define TIME 12
+#define TIME 11
+#define EST_TIME 12
 
+#define UID 13
 #define UNAME 14
 #define UROLE 15
 
-#define ATID 16
-#define AUNAME 17
+#define AUNAME 16
+#define ATID 17
 
-/* TASK DATA STANDARIZATION */
+/***********************************************/
+/*****	       PUBLIC FUNCTIONS           ******/
+/***********************************************/
 
-void std_id(char *id);
-void std_name(char *name);
-void std_state(char *state);
-void std_desc(char *desc);
-void std_notes(char *notes);
-void std_est_ini(char *est_ini);
-void std_est_end(char *est_end);
-void std_ini(char *ini);
-void std_end(char *end);
-void std_priority(char *prior);
-void std_type(char *type);
-void std_est_time(char *est_time);
-void std_time(char *time);
+void set_id(char *id);
+void set_name(char *name);
+void set_state(char *state);
+void set_desc(char *desc);
+void set_notes(char *notes);
+void set_est_ini(char *est_ini);
+void set_est_end(char *est_end);
+void set_ini(char *ini);
+void set_end(char *end);
+void set_priority(char *prior);
+void set_type(char *type);
+void set_est_time(char *est_time);
+void set_time(char *time);
 
-/* USER DATA STANDARIZATION */
-/* Generates an id from the uname */
-void std_uname(char *uname);
-void std_urole(char *urole);
+void set_uname(char *uname);
+void set_urole(char *urole);
 
-/* ASIG DATA STANDARIZATION */
-void std_atid(char *atid);
-void std_auname(char *auname);
+void set_auname(char *auname);
+void set_atid(char *atid);
 
+/* Call this functions when all parameters you want standarize is
+ * set with its set_xxx function */
+void std_task();
+void std_user();
+void std_asig();
