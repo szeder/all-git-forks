6c59fb3ff6009c777cede5ec69d27b661e48dd5c
Joey Walker <walkerj1@wit.edu>
2015-06-15 01:06:21 -0400

    Initial start of git tutor project

--- builtin.h
+++ builtin.h
@@ -138,5 +138,6 @@ extern int cmd_verify_pack(int argc, const char **argv, const char *prefix);
 extern int cmd_show_ref(int argc, const char **argv, const char *prefix);
 extern int cmd_pack_refs(int argc, const char **argv, const char *prefix);
 extern int cmd_replace(int argc, const char **argv, const char *prefix);
+extern int cmd_tutor(int argc, const char **argv, const char *prefix);
 
 #endif
---
+++ builtin/tutor.c
@@ -0,0 +1,10 @@
+// This is the main file to altr.
+
+#include "builtin.h"
+
+
+int cmd_tutor(int argc, const char **argv, const char *prefix)
+{
+	printf("Welcome to Git Tutor, HELLO WORLD!");
+	return 1;
+}
