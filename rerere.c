d1b460c698047eee6782db953a7fd3d40cff37a5
Junio C Hamano <gitster@pobox.com>
2015-06-30 13:03:36 -0700

    rerere: stop leaking conflict hash
    The merge_rr string list stores the conflict hash (hexadecimal
    string that is used to index into $GIT_DIR/rr-cache) in the .util
    field of its elements, and when do_plain_rerere() resolves a
    conflict, the field is cleared.  We forgot to free the conflict hash
    when we did so.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- rerere.c
+++ rerere.c
@@ -559,6 +559,7 @@ static int do_plain_rerere(struct string_list *rr, int fd)
 		fprintf(stderr, "Recorded resolution for '%s'.\n", path);
 		copy_file(rerere_path(name, "postimage"), path, 0666);
 	mark_resolved:
+		free(rr->items[i].util);
 		rr->items[i].util = NULL;
 	}
 
