e021630ed6b270b63bb08ad23ced426805611e43
Greg Price <gregprice@dropbox.com>
2015-07-08 15:00:13 -0700

    fixup lib-rebase fix for BSD sed

--- t/lib-rebase.sh
+++ t/lib-rebase.sh
@@ -55,7 +55,7 @@ set_fake_editor () {
 		">")
 			echo >> "$1";;
 		*)
-			sed -n "${line}s/^pick/$action/p; /^ref/p" < "$1".tmp >> "$1"
+			sed -n "${line}{s/^pick/$action/p"$'\n'"/^ref/p"$'\n'"}" < "$1".tmp >> "$1"
 			action=pick;;
 		esac
 	done
