6d56103e6a4823db229dbeab42cbeb36c0c45cc3
Eric Sunshine <sunshine@sunshineco.com>
2015-07-10 20:05:33 -0400

    checkout: improve die_if_checked_out() robustness
    die_if_checked_out() is intended to check if the branch about to be
    checked out is already checked out either in the main worktree or in a
    linked worktree. However, if .git/worktrees directory does not exist,
    then it never bothers checking the main worktree, even though the
    specified branch might indeed be checked out there, which is fragile
    behavior.
    
    This hasn't been a problem in practice since the current implementation
    of "git worktree add" (and, earlier, "git checkout --to") always creates
    .git/worktrees before die_if_checked_out() is called by the child "git
    checkout" invocation which populates the new worktree.
    
    However, git-worktree will eventually be retrofitted to populate the new
    worktree via "git reset --hard" rather than "git checkout", and it will
    want to use die_if_checked_out() to perform this check as well. But,
    the reliance upon order of operations (creating .git/worktrees before
    checking if a branch is already checked out) is fragile, and, as a
    general function, callers should not be expected to abide by this
    undocumented and unwarranted restriction. Therefore, make
    die_if_checked_out() more robust by checking the main worktree whether
    .git/worktrees exists or not.
    
    Signed-off-by: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/checkout.c
+++ builtin/checkout.c
@@ -916,18 +916,18 @@ static void die_if_checked_out(struct branch_info *new)
 	DIR *dir;
 	struct dirent *d;
 
-	strbuf_addf(&path, "%s/worktrees", get_git_common_dir());
-	if ((dir = opendir(path.buf)) == NULL) {
-		strbuf_release(&path);
-		return;
-	}
-
 	/*
 	 * $GIT_COMMON_DIR/HEAD is practically outside
 	 * $GIT_DIR so resolve_ref_unsafe() won't work (it
 	 * uses git_path). Parse the ref ourselves.
 	 */
 	check_linked_checkout(new, NULL);
+
+	strbuf_addf(&path, "%s/worktrees", get_git_common_dir());
+	if ((dir = opendir(path.buf)) == NULL) {
+		strbuf_release(&path);
+		return;
+	}
 
 	while ((d = readdir(dir)) != NULL) {
 		if (!strcmp(d->d_name, ".") || !strcmp(d->d_name, ".."))
