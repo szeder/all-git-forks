289538962da959b96c80cfaf2af55e1544014791
David Turner <dturner@twopensource.com>
2015-07-17 17:19:27 -0400

    unpack-trees: don't update files with CE_WT_REMOVE set
    Don't update files in the worktree from cache entries which are
    flagged with CE_WT_REMOVE.
    
    When a user does a sparse checkout, git removes files that are marked
    with CE_WT_REMOVE (because they are out-of-scope for the sparse
    checkout). If those files are also marked CE_UPDATE (for instance,
    because they differ in the branch that is being checked out and the
    outgoing branch), git would previously recreate them.  This patch
    prevents them from being recreated.
    
    These erroneously-created files would also interfere with merges,
    causing pre-merge revisions of out-of-scope files to appear in the
    worktree.
    
    Signed-off-by: Anatole Shaw <git-devel@omni.poc.net>
    Signed-off-by: David Turner <dturner@twopensource.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- unpack-trees.c
+++ unpack-trees.c
@@ -226,7 +226,7 @@ static int check_updates(struct unpack_trees_options *o)
 	for (i = 0; i < index->cache_nr; i++) {
 		struct cache_entry *ce = index->cache[i];
 
-		if (ce->ce_flags & CE_UPDATE) {
+		if (ce->ce_flags & CE_UPDATE && !(ce->ce_flags & CE_WT_REMOVE)) {
 			display_progress(progress, ++cnt);
 			ce->ce_flags &= ~CE_UPDATE;
 			if (o->update && !o->dry_run) {
