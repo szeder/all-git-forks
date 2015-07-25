f5dff1ca5eec729b12689baaebbfdfbdc27bc3a2
Geetesh Kalakoti <kalakotig@gmail.com>
2015-07-25 13:09:49 +0100

    Use 32-bit ndk instead

--- ndk.sh
+++ ndk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf android-ndk-r10e
-wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
+wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86.bin
 chmod a+x *.bin
 ./*.bin > null
 rm *.bin
