8d4c1f2dbaf7c6154397faa18df34687ce715164
Joey Walker <walkerj1@wit.edu>
2015-08-02 23:35:23 -0400

    quiet some of the commands

--- builtin/tutor.c
+++ builtin/tutor.c
@@ -1,50 +1,55 @@
 #include <stdio.h>
-
 #include <string.h>
 #include <stdlib.h> // exit ()
-
-//#define First "\
-//#/bin/bash \n \
-//echo \"HEEEEELLLOOO BASH SCRIPT\" \n\
-//"
+#include <unistd.h>
 
 int cmd_tutor(int argc, const char **argv, const char *prefix)
 {
         char begin_end[4];
         char yes1[] = "Yes";
         char yes2[] = "yes";
-        
-        printf("Welcome to Git Tutor!!\nGit Tutor is a tutorial that seeks to teach by example \nand supply the information to complete tasks that Git tutor assigns.");
+        int loopi=0;
+		FILE *dummyfile;
+		
+		for (; loopi<50; loopi++){
+			printf("\n");
+		}
+		
+        printf("Welcome to Git Tutor.\n\n\n\nGit Tutor is a tutorial that seeks to teach Git through a step-by step approach.");
         printf("\nDo you want to continue? Yes or No: ");
         scanf("%s", begin_end);
         if ((strcmp(yes1, begin_end) == 0)  || (strcmp(begin_end, yes2) == 0)) {
                 printf("You are beginning the tutorial now!\n");
         }
         else {
+				printf("Exiting tutorial.\n");
                 exit(-1);
         }
         //      system("First.sh>>clear"); //add checker
 		mkdir("./upstream/");
-		mkdir("./origin/");
+		mkdir("./master/");
+		//mkdir("./origin/");
 		//system("mkdir ./upstream/");
 		//system("mkdir ./origin/");
-		system("git init ./origin/");
-		system("git init ./upstream/");
-		system("git clone ./origin master");
-		system("touch ./upstream/test.txt");
+		system("git init -q --bare ./origin.git/");
+		system("git init -q ./upstream/");
+		system("git clone -q ./origin.git ./master");
+		//system("touch ./upstream/test.txt");
+		dummyfile = fopen("./upstream/test.txt","w");
+		fclose(dummyfile);
 		//system("cd ./upstream");
 		chdir("./upstream");
 
 		//there must a check for account's default identity for git being already set.
 		system("git add .");
-		system("git commit -m \"Initializing contents of repository\"");
+		system("git commit -qm \"Initializing contents of repository\"");
 		chdir("..");
 
-		system("git clone --bare ./origin/ origin.git");
-		system("rm -r ./origin/");
-
+		//system("git clone --bare -q ./origin/ origin.git");
+		//system("rm -rf ./origin/");
+		//rmdir("./origin");
 		chdir("./master");
-		system("git remote rm origin");
+		//system("git remote rm origin");
 		//system("cd ..");
 		printf("Following three repositories are created:\n1. master \n2. origin \n3. upstream");
 		printf("\nRemote is created for origin and upstream...");
