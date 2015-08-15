49f5072df4f9d9fd82711f4d5f32ac84d35839ba
Michael Haggerty <mhagger@alum.mit.edu>
2015-08-15 12:30:22 +0200

    load_subtree(): avoid confusion due to ill-formed non-notes
    The old code didn't correctly handle non-notes with paths that look
    like hexadecimal numbers with lengths different than 2, like
    "dead/beef". The reason is that the parsing code was sloppy, treating
    the prefix "dead/" the same as "de/ad/" even though the former can not
    be part of a valid notes filename.
    
    But we know that there are only two legitimate notes path components:
    
    * A hexadecimal string that fills the rest of the SHA-1
    * A two-digit hexadecimal string that constitutes another internal
      node.
    
    So handle only those two cases as potential notes filenames; treat
    anything else as a non-note.
    
    This means that we can unambiguously reconstruct the path leading up
    to a non-notes entry, so remove the cautionary comment.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- notes.c
+++ notes.c
@@ -426,58 +426,59 @@ static void load_subtree(struct notes_tree *t, struct leaf_node *subtree,
 	memcpy(object_sha1, subtree->key_sha1, prefix_len);
 	while (tree_entry(&desc, &entry)) {
 		unsigned char type;
-		int len, path_len = strlen(entry.path);
+		int path_len = strlen(entry.path);
 		struct leaf_node *l;
 
-		len = get_sha1_hex_segment(entry.path, path_len,
-				object_sha1 + prefix_len, 20 - prefix_len);
-		if (len < 0)
-			goto handle_non_note; /* entry.path is not a SHA1 */
-		len += prefix_len;
+		if (path_len == 2 * (20 - prefix_len)) {
+			/* This is potentially the remainder of the SHA-1 */
+			if (get_sha1_hex_segment(entry.path, path_len,
+						 object_sha1 + prefix_len,
+						 20 - prefix_len) < 0)
+				goto handle_non_note; /* entry.path is not a SHA1 */
 
-		/*
-		 * If object SHA1 is complete (len == 20), assume note object
-		 * If object SHA1 is incomplete (len < 20), and current
-		 * component consists of 2 hex chars, assume note subtree
-		 */
-		type = PTR_TYPE_NOTE;
-		l = (struct leaf_node *)
-			xcalloc(1, sizeof(struct leaf_node));
-		hashcpy(l->key_sha1, object_sha1);
-		hashcpy(l->val_sha1, entry.sha1);
-		if (len < 20) {
-			if (!S_ISDIR(entry.mode) || path_len != 2)
-				goto handle_non_note; /* not subtree */
-			l->key_sha1[19] = (unsigned char) len;
+			type = PTR_TYPE_NOTE;
+			l = (struct leaf_node *)
+				xcalloc(1, sizeof(struct leaf_node));
+			hashcpy(l->key_sha1, object_sha1);
+			hashcpy(l->val_sha1, entry.sha1);
+		} else if (path_len == 2) {
+			/* This is potentially an internal node */
+			if (get_sha1_hex_segment(entry.path, 2,
+						 object_sha1 + prefix_len,
+						 20 - prefix_len) < 0)
+				goto handle_non_note; /* entry.path is not a SHA1 */
+
 			type = PTR_TYPE_SUBTREE;
+			l = (struct leaf_node *)
+				xcalloc(1, sizeof(struct leaf_node));
+			hashcpy(l->key_sha1, object_sha1);
+			hashcpy(l->val_sha1, entry.sha1);
+			if (!S_ISDIR(entry.mode))
+				goto handle_non_note; /* not subtree */
+			l->key_sha1[19] = (unsigned char) (prefix_len + 1);
+		} else {
+			/* This can't be part of a note */
+			goto handle_non_note;
 		}
+
 		if (note_tree_insert(t, node, n, l, type,
 				     combine_notes_concatenate))
 			die("Failed to load %s %s into notes tree "
 			    "from %s",
 			    type == PTR_TYPE_NOTE ? "note" : "subtree",
 			    sha1_to_hex(l->key_sha1), t->ref);
+
 		continue;
 
 handle_non_note:
 		/*
-		 * Determine full path for this non-note entry:
-		 * The filename is already found in entry.path, but the
-		 * directory part of the path must be deduced from the subtree
-		 * containing this entry. We assume here that the overall notes
-		 * tree follows a strict byte-based progressive fanout
-		 * structure (i.e. using 2/38, 2/2/36, etc. fanouts, and not
-		 * e.g. 4/36 fanout). This means that if a non-note is found at
-		 * path "dead/beef", the following code will register it as
-		 * being found on "de/ad/beef".
-		 * On the other hand, if you use such non-obvious non-note
-		 * paths in the middle of a notes tree, you deserve what's
-		 * coming to you ;). Note that for non-notes that are not
-		 * SHA1-like at the top level, there will be no problems.
-		 *
-		 * To conclude, it is strongly advised to make sure non-notes
-		 * have at least one non-hex character in the top-level path
-		 * component.
+		 * Determine full path for this non-note entry: The
+		 * filename is already found in entry.path, but the
+		 * directory part of the path must be deduced from the
+		 * subtree containing this entry based on our
+		 * knowledge that the overall notes tree follows a
+		 * strict byte-based progressive fanout structure
+		 * (i.e. using 2/38, 2/2/36, etc. fanouts).
 		 */
 		{
 			char non_note_path[PATH_MAX];
