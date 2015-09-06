2d0437423e11013e8c3c608505d88e24d5abd902
danagilliann <danalee96@gmail.com>
2015-09-06 09:24:56 -0400

    twilio things

---
+++ builtin/send-sms.py
@@ -0,0 +1,13 @@
+from twilio.rest import TwilioRestClient
+
+account_sid = "AC2323da24819dfe91a84a3a9c8632d770"
+auth_token  = ""
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.messages.create(body="Hi! Just wanted to let you know that I'm thinking about you. Love you!",
+	to="+19177517628",
+	from_="+16464806816")
+print message.sid
+
+
+
