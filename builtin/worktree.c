9c0b9f6daa933a1e70bc510ad54e38d9f6b1ad43
Michael Rappazzo <rappazzo@gmail.com>
2015-09-18 09:30:27 -0400

    worktree: add 'list' command
    'git worktree list' iterates through the worktree list, and outputs
    details of the worktree including the path to the worktree, the currently
    checked out revision and branch, and if the work tree is bare.  There is
    also porcelain format option available.
    
    Signed-off-by: Michael Rappazzo <rappazzo@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/worktree.c
+++ builtin/worktree.c
@@ -8,10 +8,13 @@
 #include "run-command.h"
 #include "sigchain.h"
 #include "refs.h"
+#include "utf8.h"
+#include "worktree.h"
 
 static const char * const worktree_usage[] = {
 	N_("git worktree add [<options>] <path> <branch>"),
 	N_("git worktree prune [<options>]"),
+	N_("git worktree list [<options>]"),
 	NULL
 };
 
@@ -359,6 +362,79 @@ static int add(int ac, const char **av, const char *prefix)
 	return add_worktree(path, branch, &opts);
 }
 
+static void show_worktree_porcelain(struct worktree *worktree)
+{
+	struct strbuf sb = STRBUF_INIT;
+
+	strbuf_addf(&sb, "worktree %s\n", worktree->path);
+	if (worktree->is_bare)
+		strbuf_addstr(&sb, "bare");
+	else {
+		if (worktree->is_detached)
+			strbuf_addf(&sb, "detached at %s", find_unique_abbrev(worktree->head_sha1, DEFAULT_ABBREV));
+		else
+			strbuf_addf(&sb, "branch %s", shorten_unambiguous_ref(worktree->head_ref, 0));
+	}
+
+	printf("%s\n\n", sb.buf);
+
+	strbuf_release(&sb);
+}
+static void show_worktree(struct worktree *worktree, int path_maxlen)
+{
+	struct strbuf sb = STRBUF_INIT;
+
+	int cur_len = strlen(worktree->path);
+	int utf8_adj = cur_len - utf8_strwidth(worktree->path);
+	strbuf_addf(&sb, "%-*s ", 1 + path_maxlen + utf8_adj, worktree->path);
+	if (worktree->is_bare)
+		strbuf_addstr(&sb, "(bare)");
+	else {
+		strbuf_addf(&sb, "%s ", find_unique_abbrev(worktree->head_sha1, DEFAULT_ABBREV));
+		if (!worktree->is_detached)
+			strbuf_addf(&sb, "[%s]", shorten_unambiguous_ref(worktree->head_ref, 0));
+		else
+			strbuf_addstr(&sb, "(detached HEAD)");
+	}
+	printf("%s\n", sb.buf);
+
+	strbuf_release(&sb);
+}
+
+static int list(int ac, const char **av, const char *prefix)
+{
+	int porcelain = 0;
+
+	struct option options[] = {
+		OPT_BOOL(0, "porcelain", &porcelain, N_("machine-readable output")),
+		OPT_END()
+	};
+
+	ac = parse_options(ac, av, prefix, options, worktree_usage, 0);
+	if (ac)
+		usage_with_options(worktree_usage, options);
+	else {
+		struct worktree **worktrees = get_worktrees();
+		int path_maxlen = 0;
+
+		if (!porcelain) {
+			for (int i = 0; worktrees[i]; i++) {
+				int len = strlen(worktrees[i]->path);
+				if (len > path_maxlen)
+					path_maxlen = len;
+			}
+		}
+		for (int i = 0; worktrees[i]; i++) {
+			if (porcelain)
+				show_worktree_porcelain(worktrees[i]);
+			else
+				show_worktree(worktrees[i], path_maxlen);
+		}
+		free_worktrees(worktrees);
+	}
+	return 0;
+}
+
 int cmd_worktree(int ac, const char **av, const char *prefix)
 {
 	struct option options[] = {
@@ -371,5 +447,7 @@ int cmd_worktree(int ac, const char **av, const char *prefix)
 		return add(ac - 1, av + 1, prefix);
 	if (!strcmp(av[1], "prune"))
 		return prune(ac - 1, av + 1, prefix);
+	if (!strcmp(av[1], "list"))
+		return list(ac - 1, av + 1, prefix);
 	usage_with_options(worktree_usage, options);
 }
