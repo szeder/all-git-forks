ca5c10d19bb587b4b68e0e4523220ce69b200ce2
Jeff King <peff@peff.net>
2015-09-24 17:07:36 -0400

    init: use strbufs to store paths
    The init code predates strbufs, and uses PATH_MAX-sized
    buffers along with many manual checks on intermediate sizes
    (some of which make magic assumptions, such as that init
    will not create a path inside .git longer than 50
    characters).
    
    We can simplify this greatly by using strbufs, which drops
    some hard-to-verify strcpy calls.  Note that we need to
    update probe_utf8_pathname_composition, too, as it assumes
    we are passing a buffer large enough to append its probe
    filenames (it now just takes a strbuf, which also gets rid
    of the confusing "len" parameter, which was not the length of
    "path" but rather the offset to start writing).
    
    Some of the conversion makes new calls to git_path_buf.
    While we're in the area, let's also convert existing calls
    to git_path to the safer git_path_buf (our existing calls
    were passed to pretty tame functions, and so were not a
    problem, but it's easy to be consistent and safe here).
    
    Note that we had an explicit test that "git init" rejects
    long template directories. This comes from 32d1776 (init: Do
    not segfault on big GIT_TEMPLATE_DIR environment variable,
    2009-04-18). We can drop the test_must_fail here, as we now
    accept this and need only confirm that we don't segfault,
    which was the original point of the test.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/precompose_utf8.c
+++ compat/precompose_utf8.c
@@ -36,24 +36,28 @@ static size_t has_non_ascii(const char *s, size_t maxlen, size_t *strlen_c)
 }
 
 
-void probe_utf8_pathname_composition(char *path, int len)
+void probe_utf8_pathname_composition(struct strbuf *path)
 {
 	static const char *auml_nfc = "\xc3\xa4";
 	static const char *auml_nfd = "\x61\xcc\x88";
+	size_t baselen = path->len;
 	int output_fd;
 	if (precomposed_unicode != -1)
 		return; /* We found it defined in the global config, respect it */
-	strcpy(path + len, auml_nfc);
+	strbuf_addstr(path, auml_nfc);
 	output_fd = open(path, O_CREAT|O_EXCL|O_RDWR, 0600);
 	if (output_fd >= 0) {
 		close(output_fd);
-		strcpy(path + len, auml_nfd);
+		strbuf_setlen(path, baselen);
+		strbuf_addstr(path, auml_nfd);
 		precomposed_unicode = access(path, R_OK) ? 0 : 1;
 		git_config_set("core.precomposeunicode", precomposed_unicode ? "true" : "false");
-		strcpy(path + len, auml_nfc);
+		strbuf_setlen(path, baselen);
+		strbuf_addstr(path, auml_nfc);
 		if (unlink(path))
 			die_errno(_("failed to unlink '%s'"), path);
 	}
+	strbuf_setlen(path, baselen);
 }
 
 
