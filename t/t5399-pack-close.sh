53632f6707e5f34c4f4277b73f1c43ec89efb194
Max Kirillov <max@max630.net>
2015-10-01 07:32:18 +0300

    fixup: correctly test for file being opened

--- t/t5399-pack-close.sh
+++ t/t5399-pack-close.sh
@@ -6,7 +6,10 @@ test_description='closing packs'
 . "$TEST_DIRECTORY"/lib-terminal.sh
 
 test_expect_success setup '
-	test_commit commit1 &&
+	for i in $(test_seq 100)
+	do
+	    test_commit commit$i || return
+	done &&
 	git repack
 '
 
@@ -15,11 +18,13 @@ pack_path=$(find "$current_dir/.git/objects/pack" -name "pack-*.pack")
 
 test_expect_success 'pack exists' 'test_path_is_file "$pack_path"'
 
-test_expect_success TTY 'pack removed by pager' '
+test_expect_success TTY 'pack is closed after all output passed to pager' '
 	test_terminal env pack_path="$pack_path" \
 		PAGER="/bin/sh -c '\''cat >/dev/null && lsof -- \"\$pack_path\" >pack_opened'\''" \
-		git log &&
+		git -c core.packedGitWindowSize=100 log &&
 	test_must_be_empty pack_opened
 '
+
+test_expect_success false false
 
 test_done
