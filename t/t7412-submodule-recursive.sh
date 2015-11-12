90580b4f98b2ad299693e4fca49469791df9dbca
Lars Schneider <larsxschneider@gmail.com>
2015-11-12 09:58:56 +0100

    add test to demonstrate that shallow recursive clones fail
    "git clone --recursive --depth 1 --single-branch <url>" clones the
    submodules successfully. However, it does not obey "--depth 1" for
    submodule cloning.
    
    The following workaround does only work if the used submodule pointer
    is on the default branch. Otherwise "git submodule update" fails with
    "fatal: reference is not a tree:" and "Unable to checkout".
    git clone --depth 1 --single-branch <url>
    cd <repo-name>
    git submodule update --init --recursive --depth 1
    
    Signed-off-by: Lars Schneider <larsxschneider@gmail.com>

---
+++ t/t7412-submodule-recursive.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+test_description='Test shallow cloning of repos with submodules'
+
+. ./test-lib.sh
+
+test_expect_success setup '
+	git checkout -b master &&
+	echo file >file &&
+	git add file &&
+	test_tick &&
+	git commit -m "master commit" &&
+
+	git checkout -b branch &&
+	echo file >branch-file &&
+	git add branch-file &&
+	test_tick &&
+	git commit -m "branch commit" &&
+
+	git checkout master &&
+
+	git clone . repo &&
+	(
+		cd repo &&
+		git submodule add ../. submodule &&
+		(
+			cd submodule &&
+			git checkout branch
+		) &&
+		git add submodule &&
+		test_tick &&
+		git commit -m "submodule"
+	)
+'
+
+test_expect_failure shallow-clone-recursive '
+	URL="file://$(pwd | sed "s/[[:space:]]/%20/g")/repo" &&
+	echo $URL &&
+	git clone --recursive --depth 1 --single-branch $URL clone-recursive &&
+	(
+		cd "clone-recursive" &&
+		git log --oneline >lines &&
+		test_line_count = 1 lines
+	) &&
+	(
+		cd "clone-recursive/submodule" &&
+		git log --oneline >lines &&
+		test_line_count = 1 lines
+	)
+'
+
+test_expect_failure shallow-clone-recursive-workaround '
+	URL="file://$(pwd | sed "s/[[:space:]]/%20/g")/repo" &&
+	echo $URL &&
+	git clone --depth 1 --single-branch $URL clone-recursive-workaround &&
+	(
+		cd "clone-recursive-workaround" &&
+		git log --oneline >lines &&
+		test_line_count = 1 lines &&
+		git submodule update --init --recursive --depth 1
+	)
+'
+
+test_done
