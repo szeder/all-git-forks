95a3a5ced49382ff3daadce4a244215d19b5af22
Christian Couder <chriscool@tuxfamily.org>
2015-11-24 18:33:50 +0100

    config: add core.untrackedCache
    When we know that mtime is fully supported by the environment, we
    might want the untracked cache to be always used by default without
    any mtime test or kernel version check being performed.
    
    Also when we know that mtime is not supported by the environment,
    for example because the repo is shared over a network file system,
    then we might want 'git update-index --untracked-cache' to fail
    immediately instead of it testing if it works (because it might
    work on some systems using the repo over the network file system
    but not others).
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- config.c
+++ config.c
@@ -691,6 +691,16 @@ static int git_default_core_config(const char *var, const char *value)
 		trust_ctime = git_config_bool(var, value);
 		return 0;
 	}
+	if (!strcmp(var, "core.untrackedcache")) {
+		if (!strcasecmp(value, "default") || !strcasecmp(value, "check"))
+			use_untracked_cache = -1;
+		else {
+			use_untracked_cache = git_config_maybe_bool(var, value);
+			if (use_untracked_cache == -1)
+				error("unknown core.untrackedCache value '%s'; using default", value);
+		}
+		return 0;
+	}
 	if (!strcmp(var, "core.checkstat")) {
 		if (!strcasecmp(value, "default"))
 			check_stat = 1;
