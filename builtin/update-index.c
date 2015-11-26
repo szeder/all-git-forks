802ee2518ba0f95f574523c1eb5c57c2860095e2
Christian Couder <chriscool@tuxfamily.org>
2015-11-26 17:30:54 +0100

    update-index: add untracked cache notifications
    Doing:
    
      cd /tmp
      git --git-dir=/git/somewhere/else/.git update-index --untracked-cache
    
    doesn't work how one would expect. It hardcodes "/tmp" as the directory
    that "works" into the index, so if you use the working tree, you'll
    never use the untracked cache.
    
    With this patch "git update-index --untracked-cache" tells the user in
    which directory tests are performed and in which working directory the
    untracked cache is allowed.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- builtin/update-index.c
+++ builtin/update-index.c
@@ -121,7 +121,7 @@ static int test_if_untracked_cache_is_supported(void)
 	if (!mkdtemp(mtime_dir.buf))
 		die_errno("Could not make temporary directory");
 
-	fprintf(stderr, _("Testing "));
+	fprintf(stderr, _("Testing mtime in '%s' "), xgetcwd());
 	atexit(remove_test_directory);
 	xstat_mtime_dir(&st);
 	fill_stat_data(&base, &st);
@@ -1122,9 +1122,11 @@ int cmd_update_index(int argc, const char **argv, const char *prefix)
 		}
 		add_untracked_ident(the_index.untracked);
 		the_index.cache_changed |= UNTRACKED_CHANGED;
+		fprintf(stderr, _("Untracked cache enabled for '%s'\n"), get_git_work_tree());
 	} else if (!untracked_cache && the_index.untracked) {
 		the_index.untracked = NULL;
 		the_index.cache_changed |= UNTRACKED_CHANGED;
+		fprintf(stderr, _("Untracked disabled\n"));
 	}
 
 	if (active_cache_changed) {
