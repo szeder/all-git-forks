2812c9f2a872474baf576fdae64b4f7257a86c67
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-13 17:27:17 +0000

    format-patch: add an option to suppress commit hash
    Oftentimes, patches created by git format-patch will be stored in
    version control or compared with diff.  In these cases, two otherwise
    identical patches can have different commit hashes, leading to diff
    noise.  Teach git format-patch a --zero-commit option that instead
    produces an all-zero hash to avoid this diff noise.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t4014-format-patch.sh
+++ t/t4014-format-patch.sh
@@ -1431,4 +1431,10 @@ test_expect_success 'cover letter auto user override' '
 	test_line_count = 2 list
 '
 
+test_expect_success 'format-patch --zero-commit' '
+	git format-patch --zero-commit --stdout v2..v1 >patch2 &&
+	cnt=$(egrep "^From 0{40} Mon Sep 17 00:00:00 2001" patch2 | wc -l) &&
+	test $cnt = 3
+'
+
 test_done
