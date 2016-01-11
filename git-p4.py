854e531454f8f9c72e580ab587a8a98074dd1859
Lars Schneider <lars.schneider@autodesk.com>
2016-01-11 16:24:39 +0100

    fix

--- git-p4.py
+++ git-p4.py
@@ -1090,7 +1090,7 @@ class GitLFS(LargeFileSystem):
             die('git-lfs push command failed. Did you define a remote?')
 
     def escapeGitAttributePath(self, path):
-        return path.replace(' ', '[[:space:]]').replace('[', '\[').replace(']', '\]')
+        return path.replace('[', '\[').replace(']', '\]').replace(' ', '[[:space:]]')
 
     def generateGitAttributes(self):
         return (
