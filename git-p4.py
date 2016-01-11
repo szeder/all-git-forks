8a41837e4638b4e995dae1980637f625cff9b0c9
Lars Schneider <lars.schneider@autodesk.com>
2016-01-11 15:38:41 +0100

    git-p4: escape GitLFS attribute paths properly

--- git-p4.py
+++ git-p4.py
@@ -1103,6 +1103,9 @@ class GitLFS(LargeFileSystem):
         if uploadProcess.wait():
             die('git-lfs push command failed. Did you define a remote?')
 
+    def escapeGitAttributePath(self, path):
+        return path.replace('[', '\[').replace(']', '\]').replace(' ', '[[:space:]]')
+
     def generateGitAttributes(self):
         return (
             self.baseGitAttributes +
@@ -1112,10 +1115,10 @@ class GitLFS(LargeFileSystem):
                 '# Git LFS (see https://git-lfs.github.com/)\n',
                 '#\n',
             ] +
-            ['*.' + f.replace(' ', '[[:space:]]') + ' filter=lfs -text\n'
+            ['*.' + self.escapeGitAttributePath(f) + ' filter=lfs -text\n'
                 for f in sorted(gitConfigList('git-p4.largeFileExtensions'))
             ] +
-            ['/' + f.replace(' ', '[[:space:]]') + ' filter=lfs -text\n'
+            ['/' + self.escapeGitAttributePath(f) + ' filter=lfs -text\n'
                 for f in sorted(self.largeFiles) if not self.hasLargeFileExtension(f)
             ]
         )
