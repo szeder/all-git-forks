c07e5b34745571a21ee82d209d40b1a0b4541d1f
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-01-27 14:44:05 +0700

    read-cache: realign some statements

--- read-cache.c
+++ read-cache.c
@@ -1414,13 +1414,13 @@ static struct cache_entry *cache_entry_from_ondisk(struct ondisk_cache_entry *on
 {
 	struct cache_entry *ce = xmalloc(cache_entry_size(len));
 
-	ce->ce_stat_data.sd_ctime.sec = get_be32(&ondisk->ctime.sec);
-	ce->ce_stat_data.sd_mtime.sec = get_be32(&ondisk->mtime.sec);
+	ce->ce_stat_data.sd_ctime.sec  = get_be32(&ondisk->ctime.sec);
+	ce->ce_stat_data.sd_mtime.sec  = get_be32(&ondisk->mtime.sec);
 	ce->ce_stat_data.sd_ctime.nsec = get_be32(&ondisk->ctime.nsec);
 	ce->ce_stat_data.sd_mtime.nsec = get_be32(&ondisk->mtime.nsec);
 	ce->ce_stat_data.sd_dev   = get_be32(&ondisk->dev);
 	ce->ce_stat_data.sd_ino   = get_be32(&ondisk->ino);
-	ce->ce_mode  = get_be32(&ondisk->mode);
+	ce->ce_mode               = get_be32(&ondisk->mode);
 	ce->ce_stat_data.sd_uid   = get_be32(&ondisk->uid);
 	ce->ce_stat_data.sd_gid   = get_be32(&ondisk->gid);
 	ce->ce_stat_data.sd_size  = get_be32(&ondisk->size);
