16c9a3df4b339746f91dfcd0eef982d7713255ea
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-02-02 19:57:42 +0700

    bisect.c: use die_errno()

--- bisect.c
+++ bisect.c
@@ -910,8 +910,7 @@ void read_bisect_terms(const char **read_bad, const char **read_good)
 			*read_good = "good";
 			return;
 		} else {
-			die("could not read file '%s': %s", filename,
-				strerror(errno));
+			die_errno("could not read file '%s'", filename);
 		}
 	} else {
 		strbuf_getline_lf(&str, fp);
