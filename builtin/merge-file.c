3c353c8592df26d875b496ccacf45271b491b316
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-02-02 19:59:35 +0700

    builtin/merge-file.c: use sys_error()

--- builtin/merge-file.c
+++ builtin/merge-file.c
@@ -62,8 +62,7 @@ int cmd_merge_file(int argc, const char **argv, const char *prefix)
 		usage_with_options(merge_file_usage, options);
 	if (quiet) {
 		if (!freopen("/dev/null", "w", stderr))
-			return error("failed to redirect stderr to /dev/null: "
-				     "%s", strerror(errno));
+			return sys_error("failed to redirect stderr to /dev/null");
 	}
 
 	if (prefix)
