76e1a73741719e5a1aab5c6ede35363b9e3f486b
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-02-02 20:07:38 +0700

    vcs-svn: use sys_error()

--- vcs-svn/line_buffer.c
+++ vcs-svn/line_buffer.c
@@ -53,9 +53,9 @@ long buffer_tmpfile_prepare_to_read(struct line_buffer *buf)
 {
 	long pos = ftell(buf->infile);
 	if (pos < 0)
-		return error("ftell error: %s", strerror(errno));
+		return sys_error("ftell error");
 	if (fseek(buf->infile, 0, SEEK_SET))
-		return error("seek error: %s", strerror(errno));
+		return sys_error("seek error");
 	return pos;
 }
 
