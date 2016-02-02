60095d1f232e8bc81e2124a9134a1db7cfebd273
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-02-02 19:59:04 +0700

    builtin/fetch.c: use sys_error()

--- builtin/fetch.c
+++ builtin/fetch.c
@@ -602,7 +602,7 @@ static int store_updated_refs(const char *raw_url, const char *remote_name,
 
 	fp = fopen(filename, "a");
 	if (!fp)
-		return error(_("cannot open %s: %s\n"), filename, strerror(errno));
+		return sys_error(_("cannot open %s"), filename);
 
 	if (raw_url)
 		url = transport_anonymize_url(raw_url);
@@ -843,7 +843,7 @@ static int truncate_fetch_head(void)
 	FILE *fp = fopen_for_writing(filename);
 
 	if (!fp)
-		return error(_("cannot open %s: %s\n"), filename, strerror(errno));
+		return sys_error(_("cannot open %s"), filename);
 	fclose(fp);
 	return 0;
 }
