3474ad07ccbfe037e093cfa881c5950b719e2838
David Turner <dturner@twopensource.com>
2016-02-09 18:51:58 -0500

    sha1-array: add methods
    Add sha1_array_remove, sha1_array_for_each_nonunique and
    sha1_array_count_unique to sha1-array.  Later, we will use these to
    compute connectivity data for the journal.
    
    Signed-off-by: David Turner <dturner@twopensource.com>

--- sha1-array.c
+++ sha1-array.c
@@ -33,6 +33,25 @@ int sha1_array_lookup(struct sha1_array *array, const unsigned char *sha1)
 	return sha1_pos(sha1, array->sha1, array->nr, sha1_access);
 }
 
+int sha1_array_remove(struct sha1_array *array, const unsigned char *sha1)
+{
+	int pos;
+
+	if (!array->sorted)
+		sha1_array_sort(array);
+	pos = sha1_pos(sha1, array->sha1, array->nr, sha1_access);
+	if (pos < 0)
+		return pos;
+
+	array->nr--;
+
+	for (; pos < array->nr; ++pos) {
+		hashcpy(array->sha1[pos], array->sha1[pos + 1]);
+	}
+
+	return 0;
+}
+
 void sha1_array_clear(struct sha1_array *array)
 {
 	free(array->sha1);
@@ -56,4 +75,30 @@ void sha1_array_for_each_unique(struct sha1_array *array,
 			continue;
 		fn(array->sha1[i], data);
 	}
+}
+
+void sha1_array_for_each_nonunique(struct sha1_array *array,
+				   for_each_sha1_fn fn,
+				   void *data)
+{
+	int i;
+	for (i = 0; i < array->nr; i++) {
+		fn(array->sha1[i], data);
+	}
+}
+
+static void increment_int(const unsigned char *sha1, void *data)
+{
+	int *count = data;
+	(*count)++;
+}
+
+int sha1_array_count_unique(struct sha1_array *array)
+{
+	int count = 0;
+	if (!array->sorted)
+		sha1_array_sort(array);
+
+	sha1_array_for_each_unique(array, increment_int, &count);
+	return count;
 }
