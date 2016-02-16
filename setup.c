95051d3760e0645c47133df208cf5da4ba1c8428
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-02-16 20:06:03 +0700

    stuff

--- setup.c
+++ setup.c
@@ -386,7 +386,7 @@ static int check_repo_format(const char *var, const char *value, void *cb)
 		 * we fall through to recording it as unknown, and
 		 * check_repository_format will complain
 		 */
-		if (!strcmp(ext, "noop"))
+		if (!strcmp(ext, "noop") || !strcmp(ext, "worktree"))
 			;
 		else if (!strcmp(ext, "preciousobjects"))
 			repository_format_precious_objects = git_config_bool(var, value);
