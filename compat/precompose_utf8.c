541ace24ae16fab31f2420d49a5789cac6f0c529
Michael Blume <blume.mike@gmail.com>
2016-02-17 13:03:14 -0800

    fix git_config_set

--- compat/precompose_utf8.c
+++ compat/precompose_utf8.c
@@ -50,7 +50,7 @@ void probe_utf8_pathname_composition(void)
 		close(output_fd);
 		git_path_buf(&path, "%s", auml_nfd);
 		precomposed_unicode = access(path.buf, R_OK) ? 0 : 1;
-		git_config_set_or_die("core.precomposeunicode",
+		git_config_set("core.precomposeunicode",
 				      precomposed_unicode ? "true" : "false");
 		git_path_buf(&path, "%s", auml_nfc);
 		if (unlink(path.buf))
