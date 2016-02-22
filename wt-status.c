208e1ff6a1ee7afb4ca108dab0f18ce7cba11bb4
Moritz Neeb <lists@moritzneeb.de>
2016-02-22 02:11:47 +0100

    wt-status: read rebase todolist with strbuf_getline()
    In read_rebase_todolist() the files $GIT_DIR/rebase-merge/done and
    $GIT_DIR/rebase-merge/git-rebase-todo are read to collect status
    information.
    
    The access to this file should always happen via git rebase, e.g. via
    "git rebase -i" or "git rebase --edit-todo". We can assume, that this
    interface handles the preprocessing of whitespaces, especially CRLFs
    correctly. Thus in this codepath we can remove the call to strbuf_trim().
    
    For documenting the input as expecting "text" input, strbuf_getline_lf()
    is still replaced by strbuf_getline().

--- wt-status.c
+++ wt-status.c
@@ -1076,10 +1076,9 @@ static void read_rebase_todolist(const char *fname, struct string_list *lines)
 	if (!f)
 		die_errno("Could not open file %s for reading",
 			  git_path("%s", fname));
-	while (!strbuf_getline_lf(&line, f)) {
+	while (!strbuf_getline(&line, f)) {
 		if (line.len && line.buf[0] == comment_line_char)
 			continue;
-		strbuf_trim(&line);
 		if (!line.len)
 			continue;
 		abbrev_sha1_in_line(&line);
