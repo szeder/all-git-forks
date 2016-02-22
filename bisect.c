e80f810f47ee29a19711b32deb9cf3d841505fe4
Moritz Neeb <lists@moritzneeb.de>
2016-02-22 02:11:47 +0100

    bisect: read bisect paths with strbuf_getline()
    The file BISECT_NAMES is written by "git rev-parse --sq-quote" via
    sq_quote_argv() when starting a bisection. It can contain pathspecs
    to narrow down the search. When reading it back, it should be expected that
    sq_dequote_to_argv_array() is able to parse this file. In fact, the
    previous commit ensures this.
    
    As the content is of type "text", that means there is no logic expecting
    CR, strbuf_getline_lf() will be replaced by strbuf_getline().
    
    Apart from whitespace added and removed in quote.c, no other whitespaces
    are expected. While it is technically possible, we have never advertised
    this file to be editable by user, or encouraged them to do so. As a
    consequence, the parsing of BISECT_NAMES is tightened by removing
    strbuf_trim().
    
    For the case that this file is modified nonetheless, in an invalid way
    such that dequoting fails, the error message is broadened to both cases:
    bad quoting and unexpected whitespace.
    
    Helped-by: Junio C Hamano <gitster@pobox.com>

--- bisect.c
+++ bisect.c
@@ -440,10 +440,9 @@ static void read_bisect_paths(struct argv_array *array)
 	if (!fp)
 		die_errno("Could not open file '%s'", filename);
 
-	while (strbuf_getline_lf(&str, fp) != EOF) {
-		strbuf_trim(&str);
+	while (strbuf_getline(&str, fp) != EOF) {
 		if (sq_dequote_to_argv_array(str.buf, array))
-			die("Badly quoted content in file '%s': %s",
+			die("Badly quoted content or unexpected whitespace in file '%s': %s",
 			    filename, str.buf);
 	}
 
