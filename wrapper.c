5cef359c6bca688b4646da6b3913b1e85a585629
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-02-27 13:42:13 +0700

    wrapper.c: mark strings for translation
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wrapper.c
+++ wrapper.c
@@ -91,10 +91,10 @@ static void *do_xmallocz(size_t size, int gentle)
 	void *ret;
 	if (unsigned_add_overflows(size, 1)) {
 		if (gentle) {
-			error("Data too large to fit into virtual memory space.");
+			error(_("Data too large to fit into virtual memory space."));
 			return NULL;
 		} else
-			die("Data too large to fit into virtual memory space.");
+			die(_("Data too large to fit into virtual memory space."));
 	}
 	ret = do_xmalloc(size + 1, gentle);
 	if (ret)
