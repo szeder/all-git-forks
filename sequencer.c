482a86d33dca664ab1de2f8a6d4c9808dc40b70d
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-03-09 15:32:18 +0100

    sequencer: lib'ify save_head()
    Instead of dying there, let the caller high up in the callchain notice
    the error and handle it (by dying, still).
    
    The only caller of save_head(), sequencer_pick_revisions() can already
    return errors, so its caller must be already prepared to handle error
    returns, and with this step, we make it notice an error return from
    this function.
    
    So this is a safe conversion to make save_head() callable from new
    callers that want it not to die, without changing the external
    behaviour of anything existing.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- sequencer.c
+++ sequencer.c
@@ -859,14 +859,20 @@ static int save_head(const char *head)
 	int fd;
 
 	fd = hold_lock_file_for_update(&head_lock, git_path_head_file(), 0);
-	if (fd < 0)
+	if (fd < 0) {
+		rollback_lock_file(&head_lock);
 		return error_errno(_("Could not lock HEAD"));
+	}
 	strbuf_addf(&buf, "%s\n", head);
-	if (write_in_full(fd, buf.buf, buf.len) < 0)
+	if (write_in_full(fd, buf.buf, buf.len) < 0) {
+		rollback_lock_file(&head_lock);
 		return error_errno(_("Could not write to %s"),
 				   git_path_head_file());
-	if (commit_lock_file(&head_lock) < 0)
+	}
+	if (commit_lock_file(&head_lock) < 0) {
+		rollback_lock_file(&head_lock);
 		return error(_("Error wrapping up %s."), git_path_head_file());
+	}
 	return 0;
 }
 
