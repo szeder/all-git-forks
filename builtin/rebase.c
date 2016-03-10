5128861b550864cd1720aa2fd2f38ad8ac785b40
Paul Tan <pyokagan@gmail.com>
2016-03-10 20:27:18 +0800

    builtin-rebase: implement skeletal builtin rebase
    Signed-off-by: Paul Tan <pyokagan@gmail.com>

---
+++ builtin/rebase.c
@@ -0,0 +1,28 @@
+/*
+ * Builtin "git rebase"
+ */
+#include "cache.h"
+#include "builtin.h"
+#include "parse-options.h"
+
+static int git_rebase_config(const char *k, const char *v, void *cb)
+{
+	return git_default_config(k, v, NULL);
+}
+
+int cmd_rebase(int argc, const char **argv, const char *prefix)
+{
+	const char * const usage[] = {
+		N_("git rebase [options]"),
+		NULL
+	};
+	struct option options[] = {
+		OPT_END()
+	};
+
+	git_config(git_rebase_config, NULL);
+
+	argc = parse_options(argc, argv, prefix, options, usage, 0);
+
+	return 0;
+}
