bdb15e75b7516525dc16bfb36605baac86cc2e70
Paul Tan <pyokagan@gmail.com>
2016-03-10 16:37:22 +0800

    builtin/am.c: use ident_script_from_line()
    Signed-off-by: Paul Tan <pyokagan@gmail.com>

--- builtin/am.c
+++ builtin/am.c
@@ -1260,40 +1260,18 @@ static int get_mail_commit_sha1(unsigned char *commit_id, const char *mail)
  */
 static void get_commit_info(struct am_state *state, struct commit *commit)
 {
-	const char *buffer, *ident_line, *author_date, *msg;
+	const char *buffer, *ident_line, *msg;
 	size_t ident_len;
-	struct ident_split ident_split;
 	struct strbuf sb = STRBUF_INIT;
 
 	buffer = logmsg_reencode(commit, NULL, get_commit_output_encoding());
 
 	ident_line = find_commit_header(buffer, "author", &ident_len);
 
-	if (split_ident_line(&ident_split, ident_line, ident_len) < 0) {
+	if (ident_script_from_line(&state->author, ident_line, ident_len) < 0) {
 		strbuf_add(&sb, ident_line, ident_len);
 		die(_("invalid ident line: %s"), sb.buf);
 	}
-
-	assert(!state->author.name);
-	if (ident_split.name_begin) {
-		strbuf_add(&sb, ident_split.name_begin,
-			ident_split.name_end - ident_split.name_begin);
-		state->author.name = strbuf_detach(&sb, NULL);
-	} else
-		state->author.name = xstrdup("");
-
-	assert(!state->author.email);
-	if (ident_split.mail_begin) {
-		strbuf_add(&sb, ident_split.mail_begin,
-			ident_split.mail_end - ident_split.mail_begin);
-		state->author.email = strbuf_detach(&sb, NULL);
-	} else
-		state->author.email = xstrdup("");
-
-	author_date = show_ident_date(&ident_split, DATE_MODE(NORMAL));
-	strbuf_addstr(&sb, author_date);
-	assert(!state->author.date);
-	state->author.date = strbuf_detach(&sb, NULL);
 
 	assert(!state->msg);
 	msg = strstr(buffer, "\n\n");
@@ -1301,6 +1279,7 @@ static void get_commit_info(struct am_state *state, struct commit *commit)
 		die(_("unable to parse commit %s"), oid_to_hex(&commit->object.oid));
 	state->msg = xstrdup(msg + 2);
 	state->msg_len = strlen(state->msg);
+	unuse_commit_buffer(commit, buffer);
 }
 
 /**
