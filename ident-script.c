0b9f8f4d112e55553a4383f7c9def31b842105b4
Paul Tan <pyokagan@gmail.com>
2016-03-10 16:27:32 +0800

    ident-script: libify read_author_script()
    Signed-off-by: Paul Tan <pyokagan@gmail.com>

--- ident-script.c
+++ ident-script.c
@@ -1,5 +1,6 @@
 #include "cache.h"
 #include "ident-script.h"
+#include "quote.h"
 
 void ident_script_init(struct ident_script *ident)
 {
@@ -14,4 +15,80 @@ void ident_script_release(struct ident_script *ident)
 	free(ident->email);
 	free(ident->date);
 	ident_script_init(ident);
+}
+
+/**
+ * Reads a KEY=VALUE shell variable assignment from `fp`, returning the VALUE
+ * as a newly-allocated string. VALUE must be a quoted string, and the KEY must
+ * match `key`. Returns NULL on failure.
+ *
+ * This is used by read_author_script() to read the GIT_AUTHOR_* variables from
+ * the author-script.
+ */
+static char *read_shell_var(FILE *fp, const char *key)
+{
+	struct strbuf sb = STRBUF_INIT;
+	const char *str;
+
+	if (strbuf_getline_lf(&sb, fp))
+		goto fail;
+
+	if (!skip_prefix(sb.buf, key, &str))
+		goto fail;
+
+	if (!skip_prefix(str, "=", &str))
+		goto fail;
+
+	strbuf_remove(&sb, 0, str - sb.buf);
+
+	str = sq_dequote(sb.buf);
+	if (!str)
+		goto fail;
+
+	return strbuf_detach(&sb, NULL);
+
+fail:
+	strbuf_release(&sb);
+	return NULL;
+}
+
+int read_author_script(struct ident_script *ident, const char *filename)
+{
+	FILE *fp;
+
+	fp = fopen(filename, "r");
+	if (!fp) {
+		if (errno == ENOENT)
+			return 0;
+		die_errno(_("could not open '%s' for reading"), filename);
+	}
+
+	free(ident->name);
+	ident->name = read_shell_var(fp, "GIT_AUTHOR_NAME");
+	if (!ident->name) {
+		fclose(fp);
+		return -1;
+	}
+
+	free(ident->email);
+	ident->email = read_shell_var(fp, "GIT_AUTHOR_EMAIL");
+	if (!ident->email) {
+		fclose(fp);
+		return -1;
+	}
+
+	free(ident->date);
+	ident->date = read_shell_var(fp, "GIT_AUTHOR_DATE");
+	if (!ident->date) {
+		fclose(fp);
+		return -1;
+	}
+
+	if (fgetc(fp) != EOF) {
+		fclose(fp);
+		return -1;
+	}
+
+	fclose(fp);
+	return 0;
 }
--- ident-script.h
+++ ident-script.h
@@ -13,4 +13,21 @@ void ident_script_init(struct ident_script *);
 
 void ident_script_release(struct ident_script *);
 
+/**
+ * Reads and parses an author-script file, setting the ident_script fields
+ * accordingly. Returns 0 on success, -1 if the file could not be parsed.
+ *
+ * The author script is of the format:
+ *
+ *	GIT_AUTHOR_NAME='$author_name'
+ *	GIT_AUTHOR_EMAIL='$author_email'
+ *	GIT_AUTHOR_DATE='$author_date'
+ *
+ * where $author_name, $author_email and $author_date are quoted. We are strict
+ * with our parsing, as the file was meant to be eval'd in shell scripts, and
+ * thus if the file differs from what this function expects, it is better to
+ * bail out than to do something that the user does not expect.
+ */
+int read_author_script(struct ident_script *, const char *);
+
 #endif /* IDENT_SCRIPT_H */
