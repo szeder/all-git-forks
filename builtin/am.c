edbd5da6af44a9d60378ceedcf532b43a0059ea0
Paul Tan <pyokagan@gmail.com>
2016-03-10 11:36:28 +0800

    am_state_init(): remove unneeded whitespace
    The original author of am_state_init() was a bit too overzealous with
    whitespace. Remove the whitespace to make the code easier to read and
    edit.
    
    While we are here, remove the unneeded assert() as well.
    
    Signed-off-by: Paul Tan <pyokagan@gmail.com>

--- builtin/am.c
+++ builtin/am.c
@@ -139,22 +139,13 @@ static void am_state_init(struct am_state *state, const char *dir)
 	int gpgsign;
 
 	memset(state, 0, sizeof(*state));
-
-	assert(dir);
 	state->dir = xstrdup(dir);
-
 	state->prec = 4;
-
 	git_config_get_bool("am.threeway", &state->threeway);
-
 	state->utf8 = 1;
-
 	git_config_get_bool("am.messageid", &state->message_id);
-
 	state->scissors = SCISSORS_UNSET;
-
 	argv_array_init(&state->git_apply_opts);
-
 	if (!git_config_get_bool("commit.gpgsign", &gpgsign))
 		state->sign_commit = gpgsign ? "" : NULL;
 }
