2c49dd94368fa82c5de4b0269213c8c7b6edd7cc
Christian Couder <chriscool@tuxfamily.org>
2016-03-14 13:56:05 +0100

    builtin/apply: make find_header() return -2 instead of die()ing
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- builtin/apply.c
+++ builtin/apply.c
@@ -1547,6 +1547,14 @@ static int parse_fragment_header(const char *line, int len, struct fragment *fra
 	return offset;
 }
 
+/*
+ * Find file diff header
+ *
+ * Returns:
+ *  -2 in case of error
+ *  -1 if no header was found
+ *   the size of the header in bytes (called "offset") otherwise
+ */
 static int find_header(struct apply_state *state,
 		       const char *line,
 		       unsigned long size,
@@ -1580,8 +1588,9 @@ static int find_header(struct apply_state *state,
 			struct fragment dummy;
 			if (parse_fragment_header(line, len, &dummy) < 0)
 				continue;
-			die(_("patch fragment without header at line %d: %.*s"),
-			    state->linenr, (int)len-1, line);
+			error(_("patch fragment without header at line %d: %.*s"),
+			      state->linenr, (int)len-1, line);
+			return -2;
 		}
 
 		if (size < len + 6)
@@ -1596,19 +1605,23 @@ static int find_header(struct apply_state *state,
 			if (git_hdr_len <= len)
 				continue;
 			if (!patch->old_name && !patch->new_name) {
-				if (!patch->def_name)
-					die(Q_("git diff header lacks filename information when removing "
-					       "%d leading pathname component (line %d)",
-					       "git diff header lacks filename information when removing "
-					       "%d leading pathname components (line %d)",
-					       state->p_value),
-					    state->p_value, state->linenr);
+				if (!patch->def_name) {
+					error(Q_("git diff header lacks filename information when removing "
+						 "%d leading pathname component (line %d)",
+						 "git diff header lacks filename information when removing "
+						 "%d leading pathname components (line %d)",
+						 state->p_value),
+					      state->p_value, state->linenr);
+					return -2;
+				}
 				patch->old_name = xstrdup(patch->def_name);
 				patch->new_name = xstrdup(patch->def_name);
 			}
-			if (!patch->is_delete && !patch->new_name)
-				die("git diff header lacks filename information "
-				    "(line %d)", state->linenr);
+			if (!patch->is_delete && !patch->new_name) {
+				error("git diff header lacks filename information "
+				      "(line %d)", state->linenr);
+				return -2;
+			}
 			patch->is_toplevel_relative = 1;
 			*hdrsize = git_hdr_len;
 			return offset;
