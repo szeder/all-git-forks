666d587db630d837d02c95be2f2a3e5d89a7b3ca
Kazuki Yamaguchi <k@rhe.jp>
2016-03-25 18:18:10 +0900

    refs: extract file lock logic from lock_ref_sha1_basic
    Extract the actual file locking logic and create a new function
    lock_ref_file. The ref resolution and the lock validation remain in
    lock_ref_sha1_basic.
    We will use this lock_ref_file directly.

--- refs/files-backend.c
+++ refs/files-backend.c
@@ -1881,6 +1881,51 @@ static int remove_empty_directories(struct strbuf *path)
 	return remove_dir_recursively(path, REMOVE_DIR_EMPTY_ONLY);
 }
 
+static struct lock_file *lock_ref_file(const char *file_path, unsigned lflags,
+				       struct strbuf *err)
+{
+	int attempts_remaining = 3;
+	int last_errno = 0;
+	struct lock_file *lk = xcalloc(1, sizeof(struct lock_file));
+
+ retry:
+	switch (safe_create_leading_directories_const(file_path)) {
+	case SCLD_OK:
+		break; /* success */
+	case SCLD_VANISHED:
+		if (--attempts_remaining > 0)
+			goto retry;
+		/* fall through */
+	default:
+		last_errno = errno;
+		strbuf_addf(err, "unable to create directory for %s",
+			    file_path);
+		goto error_return;
+	}
+
+	if (hold_lock_file_for_update(lk, file_path, lflags) < 0) {
+		last_errno = errno;
+		if (errno == ENOENT && --attempts_remaining > 0)
+			/*
+			 * Maybe somebody just deleted one of the
+			 * directories leading to file_path.  Try
+			 * again:
+			 */
+			goto retry;
+		else {
+			unable_to_lock_message(file_path, errno, err);
+			goto error_return;
+		}
+	}
+
+	return lk;
+
+ error_return:
+	/* no free(lk), because atexit() still use it */
+	errno = last_errno;
+	return NULL;
+}
+
 /*
  * Locks a ref returning the lock on success and NULL on failure.
  * On failure errno is set to something meaningful.
@@ -1892,7 +1937,6 @@ static struct ref_lock *lock_ref_sha1_basic(const char *refname,
 					    unsigned int flags, int *type_p,
 					    struct strbuf *err)
 {
-	struct strbuf ref_file = STRBUF_INIT;
 	struct strbuf orig_ref_file = STRBUF_INIT;
 	const char *orig_refname = refname;
 	struct ref_lock *lock;
@@ -1901,7 +1945,6 @@ static struct ref_lock *lock_ref_sha1_basic(const char *refname,
 	int lflags = 0;
 	int mustexist = (old_sha1 && !is_null_sha1(old_sha1));
 	int resolve_flags = 0;
-	int attempts_remaining = 3;
 
 	assert(err);
 
@@ -1966,41 +2009,15 @@ static struct ref_lock *lock_ref_sha1_basic(const char *refname,
 		goto error_return;
 	}
 
-	lock->lk = xcalloc(1, sizeof(struct lock_file));
-
-	lock->ref_name = xstrdup(refname);
-	lock->orig_ref_name = xstrdup(orig_refname);
-	strbuf_git_path(&ref_file, "%s", refname);
-
- retry:
-	switch (safe_create_leading_directories_const(ref_file.buf)) {
-	case SCLD_OK:
-		break; /* success */
-	case SCLD_VANISHED:
-		if (--attempts_remaining > 0)
-			goto retry;
-		/* fall through */
-	default:
+	lock->lk = lock_ref_file(git_path("%s", refname), lflags, err);
+	if (!lock->lk) {
 		last_errno = errno;
-		strbuf_addf(err, "unable to create directory for %s",
-			    ref_file.buf);
 		goto error_return;
 	}
 
-	if (hold_lock_file_for_update(lock->lk, ref_file.buf, lflags) < 0) {
-		last_errno = errno;
-		if (errno == ENOENT && --attempts_remaining > 0)
-			/*
-			 * Maybe somebody just deleted one of the
-			 * directories leading to ref_file.  Try
-			 * again:
-			 */
-			goto retry;
-		else {
-			unable_to_lock_message(ref_file.buf, errno, err);
-			goto error_return;
-		}
-	}
+	lock->ref_name = xstrdup(refname);
+	lock->orig_ref_name = xstrdup(orig_refname);
+
 	if (verify_lock(lock, old_sha1, mustexist, err)) {
 		last_errno = errno;
 		goto error_return;
@@ -2012,7 +2029,6 @@ static struct ref_lock *lock_ref_sha1_basic(const char *refname,
 	lock = NULL;
 
  out:
-	strbuf_release(&ref_file);
 	strbuf_release(&orig_ref_file);
 	errno = last_errno;
 	return lock;
