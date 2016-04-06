d3d36154499070b8f78daaa3bea1e50fa6335f71
David Turner <dturner@twopensource.com>
2016-04-06 18:11:58 -0400

    index-helper: kill mode
    Add a new command (and command-line arg) to allow index-helpers to
    exit cleanly.
    
    This is mainly useful for tests.
    
    Signed-off-by: David Turner <dturner@twopensource.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- index-helper.c
+++ index-helper.c
@@ -276,6 +276,8 @@ static void loop(int fd, int idle_in_seconds)
 					 * alive, nothing to do.
 					 */
 				}
+			} else if (!strcmp(command.buf, "die")) {
+				break;
 			} else {
 				warning("BUG: Bogus command %s", command.buf);
 			}
@@ -358,10 +360,29 @@ static void make_socket_path(struct strbuf *path)
 	strbuf_addstr(path, "/s");
 }
 
+static void request_kill(void)
+{
+	int fd = connect_to_index_helper();
+
+	if (fd >= 0) {
+		write_in_full(fd, "die", 4);
+		close(fd);
+	}
+
+	/*
+	 * The child will try to do this anyway, but we want to be
+	 * ready to launch a new daemon immediately after this command
+	 * returns.
+	 */
+
+	unlink(git_path("index-helper.path"));
+	return;
+}
+
 int main(int argc, char **argv)
 {
 	const char *prefix;
-	int idle_in_seconds = 600, detach = 0;
+	int idle_in_seconds = 600, detach = 0, kill = 0;
 	int fd;
 	struct strbuf socket_path = STRBUF_INIT;
 	struct option options[] = {
@@ -370,6 +391,7 @@ int main(int argc, char **argv)
 		OPT_BOOL(0, "strict", &to_verify,
 			 "verify shared memory after creating"),
 		OPT_BOOL(0, "detach", &detach, "detach the process"),
+		OPT_BOOL(0, "kill", &kill, "request that existing index helper processes exit"),
 		OPT_END()
 	};
 
@@ -383,6 +405,13 @@ int main(int argc, char **argv)
 	if (parse_options(argc, (const char **)argv, prefix,
 			  options, usage_text, 0))
 		die(_("too many arguments"));
+
+	if (kill) {
+		if (detach)
+			die(_("--kill doesn't want any other options"));
+		request_kill();
+		return 0;
+	}
 
 	atexit(cleanup);
 	sigchain_push_common(cleanup_on_signal);
--- t/t7900-index-helper.sh
+++ t/t7900-index-helper.sh
@@ -20,4 +20,17 @@ test_expect_success 'index-helper smoke test' '
 	test_path_is_missing .git/index-helper.path
 '
 
+test_expect_success 'index-helper creates usable path file and can be killed' '
+	test_when_finished "git index-helper --kill" &&
+	test_path_is_missing .git/index-helper.path &&
+	git index-helper --detach &&
+	test -L .git/index-helper.path &&
+	sock="$(readlink .git/index-helper.path)" &&
+	test -S "$sock" &&
+	dir=$(dirname "$sock") &&
+	ls -ld "$dir" | grep ^drwx...--- &&
+	git index-helper --kill &&
+	test_path_is_missing .git/index-helper.path
+'
+
 test_done
