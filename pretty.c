7a0802a196ae3e5c0c486f7cf19e8ecac7f4c69e
David Underhill <david@pocketgems.com>
2016-04-07 17:37:05 -0700

    adds pretty format option to b64 encode the body
    This is similar to the %B option except the data is base 64 encoded. This
    is useful when trying to output data into some structured format (e.g., JSON)
    which might otherwise be impossible to achieve without breaking the
    formatting requirements.

--- pretty.c
+++ pretty.c
@@ -11,6 +11,41 @@
 #include "reflog-walk.h"
 #include "gpg-interface.h"
 
+/* adapted from http://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c */
+#include <stdint.h>
+#include <stdlib.h>
+static char encoding_table[] = {
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+	'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+	'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+	't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+	'8', '9', '+', '/'};
+static int mod_table[] = {0, 2, 1};
+char *base64_encode(const unsigned char *data, size_t input_length) {
+	size_t output_length = 4 * ((input_length + 2) / 3);
+	char *encoded_data = malloc(output_length + 1);
+	if (encoded_data == NULL) return NULL;
+	encoded_data[output_length] = '\0';
+
+	for (int i = 0, j = 0; i < input_length;) {
+		uint32_t octet_a = i < input_length ? (unsigned char)data[i++] : 0;
+		uint32_t octet_b = i < input_length ? (unsigned char)data[i++] : 0;
+		uint32_t octet_c = i < input_length ? (unsigned char)data[i++] : 0;
+
+		uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+
+		encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
+		encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
+		encoded_data[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
+		encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
+	}
+
+	for (int i = 0; i < mod_table[input_length % 3]; i++)
+		encoded_data[output_length - 1 - i] = '=';
+
+	return encoded_data;
+}
+
 static char *user_format;
 static struct cmt_fmt_map {
 	const char *name;
@@ -1265,6 +1300,14 @@ static size_t format_commit_one(struct strbuf *sb, /* in UTF-8 */
 		/* message_off is always left at the initial newline */
 		strbuf_addstr(sb, msg + c->message_off + 1);
 		return 1;
+	case 'X':	/* raw body base-64 encoded */
+		{
+			const char *raw_body = msg + c->message_off + 1;
+			char *b64_body = base64_encode((unsigned char*)raw_body, strlen(raw_body));
+			strbuf_addstr(sb, b64_body);
+			free(b64_body);
+			return 1;
+		}
 	}
 
 	/* Now we need to parse the commit message. */
