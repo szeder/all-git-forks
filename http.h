55b57b961e763263688bfc7274d3fd34b50565f6
Elia Pinto <gitter.spiros@gmail.com>
2016-04-28 11:57:47 +0000

    http.c: implement the GIT_TRACE_CURL environment variable
    Implement the GIT_TRACE_CURL environment variable to allow a
    greater degree of detail of GIT_CURL_VERBOSE, in particular
    the complete transport header and all the data payload exchanged.
    It might be useful if a particular situation could require a more
    thorough debugging analysis. Document the new GIT_TRACE_CURL
    environment variable.
    
    Helped-by: Torsten Bögershausen <tboegi@web.de>
    Helped-by: Ramsay Jones <ramsay@ramsayjones.plus.com>
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Helped-by: Eric Sunshine <sunshine@sunshineco.com>
    Helped-by: Jeff King <peff@peff.net>
    Signed-off-by: Elia Pinto <gitter.spiros@gmail.com>

--- http.h
+++ http.h
@@ -224,4 +224,8 @@ extern int finish_http_object_request(struct http_object_request *freq);
 extern void abort_http_object_request(struct http_object_request *freq);
 extern void release_http_object_request(struct http_object_request *freq);
 
+/* Debug callback and helper routine for curl_easy_setopt CURLOPT_DEBUGFUNCTION */
+int curl_trace_want(void);
+int curl_trace(CURL *handle, curl_infotype type, char *data, size_t size, void *userp);
+
 #endif /* HTTP_H */
