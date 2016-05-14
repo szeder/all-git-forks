5c8434af4348a763a7585e296d439f561523f7cb
Stefan Beller <stefanbeller@gmail.com>
2016-05-14 13:35:30 -0700

    mv: free memory in add_slash

--- builtin/mv.c
+++ builtin/mv.c
@@ -54,6 +54,7 @@ static const char *add_slash(const char *path)
 		memcpy(with_slash, path, len);
 		with_slash[len++] = '/';
 		with_slash[len] = 0;
+		free(path);
 		return with_slash;
 	}
 	return path;
