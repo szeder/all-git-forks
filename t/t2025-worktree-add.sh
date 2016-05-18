59a1aaa80fe7b859095de8ef27fce28f23b13658
Jordan DE GEA <jordan.de-gea@grenoble-inp.org>
2016-05-18 14:09:21 +0200

    worktree: allow "-" short-hand for @{-1} in add command
    Since `git worktree add` uses `git checkout` when `[<branch>]` is used,
    and `git checkout -` is already supported, it makes sense to allow the
    same shortcut in `git worktree add`.
    
    Signed-off-by: Matthieu Moy <Matthieu.Moy@grenoble-inp.fr>
    Signed-off-by: Jordan DE GEA <jordan.de-gea@grenoble-inp.org>

--- t/t2025-worktree-add.sh
+++ t/t2025-worktree-add.sh
@@ -18,6 +18,24 @@ test_expect_success '"add" an existing empty worktree' '
 	git worktree add --detach existing_empty master
 '
 
+test_expect_success '"add" using shorthand - fails when no previous branch' '
+	echo "fatal: invalid reference: @{-1}" >expect &&
+	test_must_fail git worktree add existing_short - 2>actual &&
+	test_cmp expect actual
+'
+
+test_expect_success '"add" using - shorthand' '
+	git checkout -b newbranch &&
+	echo hello >myworld &&
+	git add myworld &&
+	git commit -m myworld &&
+	git checkout master &&
+	git worktree add short-hand - &&
+	echo refs/heads/newbranch >expect &&
+	git -C short-hand rev-parse --symbolic-full-name HEAD >actual &&
+	test_cmp expect actual
+'
+
 test_expect_success '"add" refuses to checkout locked branch' '
 	test_must_fail git worktree add zere master &&
 	! test -d zere &&
