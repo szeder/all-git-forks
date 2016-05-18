90cdad4ff88aa15fcf9de2184c22a98333925fbe
Vasco Almeida <vascomalmeida@sapo.pt>
2016-05-18 15:27:35 +0000

    i18n: advice: mark string about detached head for translation
    Mark string with advice seen by the user when in detached head.
    
    Signed-off-by: Vasco Almeida <vascomalmeida@sapo.pt>

--- advice.c
+++ advice.c
@@ -107,13 +107,13 @@ void NORETURN die_conclude_merge(void)
 void detach_advice(const char *new_name)
 {
 	const char fmt[] =
-	"Note: checking out '%s'.\n\n"
+	N_("Note: checking out '%s'.\n\n"
 	"You are in 'detached HEAD' state. You can look around, make experimental\n"
 	"changes and commit them, and you can discard any commits you make in this\n"
 	"state without impacting any branches by performing another checkout.\n\n"
 	"If you want to create a new branch to retain commits you create, you may\n"
 	"do so (now or later) by using -b with the checkout command again. Example:\n\n"
-	"  git checkout -b <new-branch-name>\n\n";
+	"  git checkout -b <new-branch-name>\n\n");
 
 	fprintf(stderr, fmt, new_name);
 }
