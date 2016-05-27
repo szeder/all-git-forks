8c11921b72d1ebe874260db6fd4d21086cd8f53b
Christian Couder <chriscool@tuxfamily.org>
2016-05-27 16:57:57 +0200

    external odb: add write support
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-helper.c
+++ odb-helper.c
@@ -33,9 +33,10 @@ static void prepare_helper_command(struct argv_array *argv, const char *cmd,
 	strbuf_release(&buf);
 }
 
-__attribute__((format (printf,3,4)))
+__attribute__((format (printf,4,5)))
 static int odb_helper_start(struct odb_helper *o,
 			    struct odb_helper_cmd *cmd,
+			    int use_stdin,
 			    const char *fmt, ...)
 {
 	va_list ap;
@@ -52,7 +53,10 @@ static int odb_helper_start(struct odb_helper *o,
 
 	cmd->child.argv = cmd->argv.argv;
 	cmd->child.use_shell = 1;
-	cmd->child.no_stdin = 1;
+	if (use_stdin)
+		cmd->child.in = -1;
+	else
+		cmd->child.no_stdin = 1;
 	cmd->child.out = -1;
 
 	if (start_command(&cmd->child) < 0) {
@@ -109,7 +113,7 @@ static void odb_helper_load_have(struct odb_helper *o)
 		return;
 	o->have_valid = 1;
 
-	if (odb_helper_start(o, &cmd, "have") < 0)
+	if (odb_helper_start(o, &cmd, 0, "have") < 0)
 		return;
 
 	fh = xfdopen(cmd.child.out, "r");
@@ -164,7 +168,7 @@ int odb_helper_fetch_object(struct odb_helper *o, const unsigned char *sha1,
 	if (!obj)
 		return -1;
 
-	if (odb_helper_start(o, &cmd, "get %s", sha1_to_hex(sha1)) < 0)
+	if (odb_helper_start(o, &cmd, 0, "get %s", sha1_to_hex(sha1)) < 0)
 		return -1;
 
 	memset(&stream, 0, sizeof(stream));
@@ -251,4 +255,52 @@ int odb_helper_for_each_object(struct odb_helper *o,
 	}
 
 	return 0;
+}
+
+int odb_helper_write_object(struct odb_helper *o,
+			    const void *buf, unsigned long len,
+			    const char *type, unsigned char *sha1)
+{
+	struct odb_helper_cmd cmd;
+	struct strbuf sha1_buf = STRBUF_INIT;
+
+	if (odb_helper_start(o, &cmd, 1, "put %lu %s", len, type) < 0)
+		return -1;
+
+	if (xwrite(cmd.child.in, buf, len)) {
+		error("unable to write to odb helper '%s': %s",
+		      o->name, strerror(errno));
+		goto error_return;
+	}
+
+	for (;;) {
+		unsigned char buf[4096];
+		int r;
+
+		r = xread(cmd.child.out, buf, sizeof(buf));
+		if (r < 0) {
+			error("unable to read from odb helper '%s': %s",
+			      o->name, strerror(errno));
+			goto error_return;
+		}
+		if (r == 0)
+			break;
+
+		strbuf_add(&sha1_buf, buf, r);
+	}
+
+	close(cmd.child.in);
+	close(cmd.child.out);
+	odb_helper_finish(o, &cmd);
+
+	if (get_sha1_hex(sha1_buf.buf, sha1))
+		return error("could not convert '%s' to sha1", sha1_buf.buf);
+
+	return 0;
+
+error_return:
+	close(cmd.child.in);
+	close(cmd.child.out);
+	odb_helper_finish(o, &cmd);
+	return -1;
 }
