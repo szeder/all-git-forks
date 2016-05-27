f330e4af4b28e0fd21d4f50f584de2ace44128b9
Simon Rabourg <simon.rabourg@ensimag.grenoble-inp.fr>
2016-05-27 15:40:13 +0200

    Moving tests on strbuf API into a different file

--- t/t0081-line-buffer.sh
+++ t/t0081-line-buffer.sh
@@ -87,48 +87,4 @@ test_expect_success 'long binary reads are truncated' '
 	test_cmp expect actual
 '
 
-test_expect_success 'strbuf_grow and strbuf_release with default flags' '
-	test-strbuf grow_release_default
-'
-
-test_expect_success 'strbuf_init with not fixed and owned memory ' '
-	test-strbuf init_not_fixed
-'
-
-test_expect_success 'default grow and release' '
-	test-strbuf release_free
-'
-
-test_expect_success 'overflow while calling strbuf_grow' '
-	test_must_fail test-strbuf grow_overflow
-'
-
-test_expect_success 'strbuf_wrap_preallocated multiple tests' '
-	test-strbuf preallocated_multiple
-'
-
-test_expect_success 'strbuf_wrap_preallocated NULL initialization' '
-	test_must_fail test-strbuf preallocated_NULL
-'
-
-test_expect_success 'strbuf_grow with wrap_fixed in a basic failure case' '
-	test_must_fail test-strbuf grow_fixed_basic_failure
-'
-
-test_expect_success 'strbuf_grow with wrap_fixed in a complexe failure case' '
-	test_must_fail test-strbuf grow_fixed_complexe_failure
-'
-
-test_expect_success 'strbuf_grow with wrap_fixed in a successfull case' '
-	test-strbuf grow_fixed_success
-'
-
-test_expect_success 'stbuf_detach with wrap_fixed memory' '
-	test-strbuf detach_fixed
-'
-
-test_expect_success 'stbuf_release with wrap_fixed memory' '
-	test-strbuf release_fixed
-'
-
 test_done
