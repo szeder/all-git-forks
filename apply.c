0fe6420c55f5e40f846777c559901badc0b75e40
Christian Couder <chriscool@tuxfamily.org>
2016-06-07 21:50:52 +0200

    apply: fix apply_option_parse_p declaration (remove 'static')
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- apply.c
+++ apply.c
@@ -4725,9 +4725,9 @@ int apply_option_parse_include(const struct option *opt,
 	return 0;
 }
 
-static int apply_option_parse_p(const struct option *opt,
-				const char *arg,
-				int unset)
+int apply_option_parse_p(const struct option *opt,
+			 const char *arg,
+			 int unset)
 {
 	struct apply_state *state = opt->value;
 	state->p_value = atoi(arg);
