1ceb286e899ff80bb8a7f46242ec21167542d352
William Duclot <william.duclot@ensimag.grenoble-inp.fr>
2016-06-09 16:17:20 +0200

    début du test strbuf

--- t/helper/test-strbuf.c
+++ t/helper/test-strbuf.c
@@ -8,13 +8,13 @@ int main(int argc, const char *argv[])
     int i = 0, j = 0;
 
     struct strbuf buf = STRBUF_INIT_ON_STACK(100000);
-       // struct strbuf buf = STRBUF_INIT;
+    //struct strbuf buf = STRBUF_INIT;
     for (i = 0; i < 1000000; i++) {
         for (j = 0; j < 1000; j++)
             strbuf_add(&buf, "lolilolololololololololololololo", 32);
-        //strbuf_release(&buf);
         strbuf_setlen(&buf, 0);
     }
+    /strbuf_release(&buf);
 
     return 0;
 }
