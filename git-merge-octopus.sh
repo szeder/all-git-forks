2223e597740594029f0985ce7822217d0fa680c4
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-06-09 16:22:23 +0200

    fixup! Windows: make sure that merge-octopus only outputs LF line endings
    It appears this patch is no longer necessary.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- git-merge-octopus.sh
+++ git-merge-octopus.sh
@@ -77,9 +77,7 @@ do
 
 	case "$LF$common$LF" in
 	*"$LF$SHA1$LF"*)
-		cat << EOF
-Already up-to-date with $pretty_name
-EOF
+		echo "Already up-to-date with $pretty_name"
 		continue
 		;;
 	esac
@@ -91,9 +89,7 @@ EOF
 		# tree as the intermediate result of the merge.
 		# We still need to count this as part of the parent set.
 
-		cat << EOF
-Fast-forwarding to: $pretty_name
-EOF
+		echo "Fast-forwarding to: $pretty_name"
 		git read-tree -u -m $head $SHA1 || exit
 		MRC=$SHA1 MRT=$(git write-tree)
 		continue
@@ -101,9 +97,7 @@ EOF
 
 	NON_FF_MERGE=1
 
-	cat << EOF
-Trying simple merge with $pretty_name
-EOF
+	echo "Trying simple merge with $pretty_name"
 	git read-tree -u -m --aggressive  $common $MRT $SHA1 || exit 2
 	next=$(git write-tree 2>/dev/null)
 	if test $? -ne 0
