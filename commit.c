804e6959382e79a9312a81584802ffaa5c070e0b
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-06-18 15:12:17 +0200

    commit: make find_commit_subject() more robust
    Just like the pretty printing machinery, we should simply ignore empty
    lines at the beginning of the commit messages.
    
    This discrepancy was noticed when an early version of the rebase--helper
    produced commit objects with more than one empty line between the header
    and the commit message.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- commit.c
+++ commit.c
@@ -399,6 +399,8 @@ int find_commit_subject(const char *commit_buffer, const char **subject)
 		p++;
 	if (*p) {
 		p += 2;
+		while (*p == '\n')
+			p++;
 		for (eol = p; *eol && *eol != '\n'; eol++)
 			; /* do nothing */
 	} else
