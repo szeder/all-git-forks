1dfd538bb88d39b0cc798768c5a4550de55064a3
Jeff King <peff@peff.net>
2016-06-21 15:06:14 -0400

    diff: ignore mode in binary size optimization
    This makes add/delete work.

--- diff.c
+++ diff.c
@@ -2452,7 +2452,6 @@ static void builtin_diff(const char *name_a,
 	    ( (!textconv_one && diff_filespec_is_binary(one)) ||
 	      (!textconv_two && diff_filespec_is_binary(two)) )) {
 		if (!one->data && !two->data &&
-		    S_ISREG(one->mode) && S_ISREG(two->mode) &&
 		    !DIFF_OPT_TST(o, BINARY)) {
 			if (!oidcmp(&one->oid, &two->oid)) {
 				if (must_show_header)
