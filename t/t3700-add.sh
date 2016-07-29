d221e53e650fbb80d473914109cbb01e7983eb20
Ingo Brückl <ib@wupperonline.de>
2016-07-29 14:31:28 +0200

    Fix failing test t3700-add.sh
    At the time of the test xfoo1 already exists and is a link.
    As a result, the check for file mode 100644 fails.
    
    Create not yet existing file xfoo instead.
    
    Signed-off-by: Ingo Brückl <ib@wupperonline.de>

--- t/t3700-add.sh
+++ t/t3700-add.sh
@@ -342,12 +342,12 @@ test_expect_success 'git add --chmod=+x stages a non-executable file with +x' '
 '
 
 test_expect_success 'git add --chmod=-x stages an executable file with -x' '
-	echo foo >xfoo1 &&
-	chmod 755 xfoo1 &&
-	git add --chmod=-x xfoo1 &&
-	case "$(git ls-files --stage xfoo1)" in
-	100644" "*xfoo1) echo pass;;
-	*) echo fail; git ls-files --stage xfoo1; (exit 1);;
+	echo foo >xfoo &&
+	chmod 755 xfoo &&
+	git add --chmod=-x xfoo &&
+	case "$(git ls-files --stage xfoo)" in
+	100644" "*xfoo) echo pass;;
+	*) echo fail; git ls-files --stage xfoo; (exit 1);;
 	esac
 '
 
