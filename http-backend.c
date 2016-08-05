fdf0d88d12a7393d68b81c8a8a1aecbcdcc0d493
Lars Schneider <larsxschneider@gmail.com>
2016-08-06 00:22:06 +0200

    pkt-line: rename packet_write() to packet_write_fmt()
    packet_write() should be called packet_write_fmt() as the string
    parameter can be formatted.
    
    Suggested-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Lars Schneider <larsxschneider@gmail.com>

--- http-backend.c
+++ http-backend.c
@@ -460,7 +460,7 @@ static void get_info_refs(char *arg)
 		hdr_str(content_type, buf.buf);
 		end_headers();
 
-		packet_write(1, "# service=git-%s\n", svc->name);
+		packet_write_fmt(1, "# service=git-%s\n", svc->name);
 		packet_flush(1);
 
 		argv[0] = svc->name;
