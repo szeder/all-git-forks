f34f6596e707b10943a2a0cb8aab4f347796bd6f
Kirill Smelkov <kirr@nexedi.com>
2016-08-08 21:19:42 +0300

    pack-objects: respect --local/--honor-pack-keep/--incremental when bitmap is in use
    Since 6b8fda2d (pack-objects: use bitmaps when packing objects) there
    are two codepaths in pack-objects: with & without using bitmap
    reachability index.
    
    However add_object_entry_from_bitmap(), despite its non-bitmapped
    counterpart add_object_entry(), in no way does check for whether --local
    or --honor-pack-keep or --incremental should be respected. In
    non-bitmapped codepath this is handled in want_object_in_pack(), but
    bitmapped codepath has simply no such checking at all.
    
    The bitmapped codepath however was allowing to pass in all those options
    and with bitmap indices still being used under such conditions -
    potentially giving wrong output (e.g. including objects from non-local or
    .keep'ed pack).
    
    We can easily fix this by noting the following: when an object comes to
    add_object_entry_from_bitmap() it can come for two reasons:
    
        1. entries coming from main pack covered by bitmap index, and
        2. object coming from, possibly alternate, loose or other packs.
    
    "2" can be already handled by want_object_in_pack() and to cover
    "1" we can teach want_object_in_pack() to expect that *found_pack can be
    non-NULL, meaning calling client already found object's pack entry.
    
    In want_object_in_pack() we care to start the checks from already found
    pack, if we have one, this way determining the answer right away
    in case neither --local nor --honour-pack-keep are active. In
    particular, as p5310-pack-bitmaps.sh shows, we do not do harm to
    served-with-bitmap clones performance-wise:
    
        Test                      56dfeb62          this tree
        -----------------------------------------------------------------
        5310.2: repack to disk    9.63(8.67+0.33)   9.47(8.55+0.28) -1.7%
        5310.3: simulated clone   2.07(2.17+0.12)   2.03(2.14+0.12) -1.9%
        5310.4: simulated fetch   0.78(1.03+0.02)   0.76(1.00+0.03) -2.6%
        5310.6: partial bitmap    1.97(2.43+0.15)   1.92(2.36+0.14) -2.5%
    
    with all differences strangely showing we are a bit faster now, but
    probably all being within noise.
    
    And in the general case we care not to have duplicate
    find_pack_entry_one(*found_pack) calls. Worst what can happen is we can
    call want_found_object(*found_pack) -- newly introduced helper for
    checking whether we want object -- twice, but since want_found_object()
    is very lightweight it does not make any difference.
    
    I appreciate help and discussing this change with Junio C Hamano and
    Jeff King.
    
    Signed-off-by: Kirill Smelkov <kirr@nexedi.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5310-pack-bitmaps.sh
+++ t/t5310-pack-bitmaps.sh
@@ -7,6 +7,19 @@ objpath () {
 	echo ".git/objects/$(echo "$1" | sed -e 's|\(..\)|\1/|')"
 }
 
+# show objects present in pack ($1 should be associated *.idx)
+packobjects () {
+	git show-index <$1 | cut -d' ' -f2
+}
+
+# hasany pattern-file content-file
+# tests whether content-file has any entry from pattern-file with entries being
+# whole lines.
+hasany () {
+	# NOTE `grep -f` is not portable
+	git grep --no-index -qFf $1 $2
+}
+
 test_expect_success 'setup repo with moderate-sized history' '
 	for i in $(test_seq 1 10); do
 		test_commit $i
@@ -16,6 +29,7 @@ test_expect_success 'setup repo with moderate-sized history' '
 		test_commit side-$i
 	done &&
 	git checkout master &&
+	bitmaptip=$(git rev-parse master) &&
 	blob=$(echo tagged-blob | git hash-object -w --stdin) &&
 	git tag tagged-blob $blob &&
 	git config repack.writebitmaps true &&
@@ -118,6 +132,86 @@ test_expect_success 'incremental repack can disable bitmaps' '
 	git repack -d --no-write-bitmap-index
 '
 
+test_expect_success 'pack-objects respects --local (non-local loose)' '
+	mkdir -p alt_objects/pack &&
+	echo $(pwd)/alt_objects >.git/objects/info/alternates &&
+	echo content1 >file1 &&
+	# non-local loose object which is not present in bitmapped pack
+	objsha1=$(GIT_OBJECT_DIRECTORY=alt_objects git hash-object -w file1) &&
+	# non-local loose object which is also present in bitmapped pack
+	git cat-file blob $blob | GIT_OBJECT_DIRECTORY=alt_objects git hash-object -w --stdin &&
+	git add file1 &&
+	test_tick &&
+	git commit -m commit_file1 &&
+	echo HEAD | git pack-objects --local --stdout --revs >1.pack &&
+	git index-pack 1.pack &&
+	packobjects 1.idx >1.objects &&
+	printf "$objsha1\n$blob\n" >nonlocal-loose &&
+	if hasany nonlocal-loose 1.objects; then
+		echo "Non-local object present in pack generated with --local"
+		return 1
+	fi
+'
+
+test_expect_success 'pack-objects respects --honor-pack-keep (local non-bitmapped pack)' '
+	echo content2 >file2 &&
+	objsha2=$(git hash-object -w file2) &&
+	git add file2 &&
+	test_tick &&
+	git commit -m commit_file2 &&
+	printf "$objsha2\n$bitmaptip\n" >keepobjects &&
+	pack2=$(git pack-objects pack2 <keepobjects) &&
+	mv pack2-$pack2.* .git/objects/pack/ &&
+	touch .git/objects/pack/pack2-$pack2.keep &&
+	rm $(objpath $objsha2) &&
+	echo HEAD | git pack-objects --honor-pack-keep --stdout --revs >2a.pack &&
+	git index-pack 2a.pack &&
+	packobjects 2a.idx >2a.objects &&
+	if hasany keepobjects 2a.objects; then
+		echo "Object from .keeped pack present in pack generated with --honor-pack-keep"
+		return 1
+	fi
+'
+
+test_expect_success 'pack-objects respects --local (non-local pack)' '
+	mv .git/objects/pack/pack2-$pack2.* alt_objects/pack/ &&
+	echo HEAD | git pack-objects --local --stdout --revs >2b.pack &&
+	git index-pack 2b.pack &&
+	packobjects 2b.idx >2b.objects &&
+	if hasany keepobjects 2b.objects; then
+		echo "Non-local object present in pack generated with --local"
+		return 1
+	fi
+'
+
+test_expect_success 'pack-objects respects --honor-pack-keep (local bitmapped pack)' '
+	ls .git/objects/pack/ | grep bitmap >output &&
+	test_line_count = 1 output &&
+	packbitmap=$(basename $(cat output) .bitmap) &&
+	packobjects .git/objects/pack/$packbitmap.idx >packbitmap.objects &&
+	touch .git/objects/pack/$packbitmap.keep &&
+	echo HEAD | git pack-objects --honor-pack-keep --stdout --revs >3a.pack &&
+	git index-pack 3a.pack &&
+	packobjects 3a.idx >3a.objects &&
+	if hasany packbitmap.objects 3a.objects; then
+		echo "Object from .keeped bitmapped pack present in pack generated with --honour-pack-keep"
+		return 1
+	fi &&
+	rm .git/objects/pack/$packbitmap.keep
+'
+
+test_expect_success 'pack-objects respects --local (non-local bitmapped pack)' '
+	mv .git/objects/pack/$packbitmap.* alt_objects/pack/ &&
+	echo HEAD | git pack-objects --local --stdout --revs >3b.pack &&
+	git index-pack 3b.pack &&
+	packobjects 3b.idx >3b.objects &&
+	if hasany packbitmap.objects 3b.objects; then
+		echo "Non-local object from bitmapped pack present in pack generated with --local"
+		return 1
+	fi &&
+	mv alt_objects/pack/$packbitmap.* .git/objects/pack/
+'
+
 test_expect_success 'full repack, reusing previous bitmaps' '
 	git repack -ad &&
 	ls .git/objects/pack/ | grep bitmap >output &&
@@ -141,6 +235,23 @@ test_expect_success 'create objects for missing-HAVE tests' '
 	^HEAD^
 	^$commit
 	EOF
+'
+
+test_expect_success 'pack-objects respects --incremental' '
+	cat >revs2 <<-EOF &&
+	HEAD
+	$commit
+	EOF
+	git pack-objects --incremental --stdout --revs <revs2 >4.pack &&
+	git index-pack 4.pack &&
+	packobjects 4.idx >4.objects &&
+	test_line_count = 4 4.objects &&
+	git rev-list --objects $commit >revlist &&
+	cut -d" " -f1 revlist |sort >objects &&
+	if !hasany objects 4.objects; then
+		echo "Expected objects not present in incremental pack"
+		return 1
+	fi
 '
 
 test_expect_success 'pack with missing blob' '
