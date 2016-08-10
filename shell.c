cd9f2659e2057922ed1a1fdfb6c834fefb0eb405
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-08-10 10:48:00 +0200

    base: cherry-pick 819db56 onto e8658a7
    This commit helps to teach `git rerere` to resolve merge 
    conflicts when cherry-picking:
    
    	819db56 (common-main: call sanitize_stdfds(), 2016-07-01)

--- shell.c
+++ shell.c
@@ -145,6 +145,15 @@ int cmd_main(int argc, const char **argv)
 	struct commands *cmd;
 	int count;
 
+	git_setup_gettext();
+
+	/*
+	 * Always open file descriptors 0/1/2 to avoid clobbering files
+	 * in die().  It also avoids messing up when the pipes are dup'ed
+	 * onto stdin/stdout/stderr in the child processes we spawn.
+	 */
+	sanitize_stdfds();
+
 	/*
 	 * Special hack to pretend to be a CVS server
 	 */
