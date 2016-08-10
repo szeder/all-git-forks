e9c49c94c5e9ca8c848790d10425387af2b55f88
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-08-10 13:43:24 +0200

    upstream: cherry-pick dbed338 onto e4886f1
    This commit helps to teach `git rerere` to resolve merge 
    conflicts when cherry-picking:
    
    	dbed338 (mingw: declare main()'s argv as const, 2016-07-01)

--- common-main.c
+++ common-main.c
@@ -22,14 +22,8 @@ static void restore_sigpipe_to_default(void)
 	signal(SIGPIPE, SIG_DFL);
 }
 
-int main(int argc, char **av)
+int main(int argc, const char **argv)
 {
-	/*
-	 * This const trickery is explained in
-	 * 84d32bf7678259c08406571cd6ce4b7a6724dcba
-	 */
-	const char **argv = (const char **)av;
-
 	/*
 	 * Always open file descriptors 0/1/2 to avoid clobbering files
 	 * in die().  It also avoids messing up when the pipes are dup'ed
@@ -38,6 +32,13 @@ int main(int argc, char **av)
 	sanitize_stdfds();
 
 	git_setup_gettext();
+
+	/*
+	 * Always open file descriptors 0/1/2 to avoid clobbering files
+	 * in die().  It also avoids messing up when the pipes are dup'ed
+	 * onto stdin/stdout/stderr in the child processes we spawn.
+	 */
+	sanitize_stdfds();
 
 	argv[0] = git_extract_argv0_path(argv[0]);
 
