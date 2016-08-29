de63c46bf4929c480544a8d0a6ce45008f424b72
Julian Phillips <julian@quantumfyre.co.uk>
2016-08-29 20:18:08 +0100

    fast-import: Start adding some tests
    We want to 1) show that the new functionality works, 2) make sure that
    it doesn't get broken in the future.

---
+++ t/t9303-fast-import-tags.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+test_description='test git fast-import tag handling'
+. ./test-lib.sh
+
+test_expect_success 'can update tag' '
+	test_tick &&
+	cat >input <<-INPUT_END &&
+	commit refs/heads/master
+	committer $GIT_COMMITTER_NAME <$GIT_COMMITTER_EMAIL> $GIT_COMMITTER_DATE
+	data <<COMMIT
+	initial
+	COMMIT
+
+	M 644 inline file
+	data <<EOF
+	file content
+	EOF
+
+	tag tag1
+	from refs/heads/master
+	data <<EOF
+	a tag
+	EOF
+
+	commit refs/heads/master
+	committer $GIT_COMMITTER_NAME <$GIT_COMMITTER_EMAIL> $GIT_COMMITTER_DATE
+	data <<COMMIT
+	second
+	COMMIT
+
+	M 644 inline file
+	data <<EOF
+	file content 2
+	EOF
+
+	tag tag1
+	from refs/heads/master
+	data <<EOF
+	An update to tag1
+	EOF
+
+	INPUT_END
+	git fast-import --export-marks=marks.out <input
+'
+
+test_expect_success 'can branch from tag' '
+	test_tick &&
+	cat >input <<-INPUT_END &&
+	commit refs/heads/maint1
+	committer $GIT_COMMITTER_NAME <$GIT_COMMITTER_EMAIL> $GIT_COMMITTER_DATE
+	data <<COMMIT
+	build on tag
+	COMMIT
+
+	from refs/tags/tag1
+	M 644 inline file
+	data <<EOF
+	file content 2
+	EOF
+
+	INPUT_END
+	git fast-import --export-marks=marks.out <input
+'
+
+test_done
