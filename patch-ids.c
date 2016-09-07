60029bbf5d4ce070710e629552a65554f31bab7b
Jeff King <peff@peff.net>
2016-09-07 03:54:25 -0400

    patch-ids: skip merge commits
    The patch-ids code which powers "log --cherry-pick" doesn't
    look at whether each commit is a merge or not. It just feeds
    the commit's first parent to the diff, and ignores any
    additional parents.
    
    In theory, this might be useful if you wanted to find
    equivalence between, say, a merge commit and a squash-merge
    that does the same thing.  But that may also be the wrong
    thing; the diffs may be the same, but the meaning of the two
    commits is definitely not identical. We should err on the
    side of _not_ matching such commits.
    
    Moreover, we may spend a lot of extra time computing these
    merge diffs. In the case that inspired this patch, a "git
    format-patch --cherry-pick" dropped from over 3 minutes to
    less than 4 seconds. This seems pretty drastic, but is
    easily explained. The command was invoked by a "git rebase"
    of an older topic branch; there had been tens of thousands
    of commits on the upstream branch in the meantime. In
    addition, this project used a topic-branch workflow with
    occasional "back-merges" from "master" to each topic (to
    resolve conflicts on the topics rather than in the merge
    commits). So there were not only extra merges, but the diffs
    for these back-merges were generally quite large (because
    they represented _everything_ that had been merged to master
    since the topic branched).
    
    This patch just ignores merge commits entirely when
    generating patch-ids, meaning they will never be matched
    (from either side of a symmetric-diff traversal).
    
    Signed-off-by: Jeff King <peff@peff.net>

--- patch-ids.c
+++ patch-ids.c
@@ -7,10 +7,12 @@
 int commit_patch_id(struct commit *commit, struct diff_options *options,
 		    unsigned char *sha1, int diff_header_only)
 {
-	if (commit->parents)
+	if (commit->parents) {
+		if (commit->parents->next)
+			return 0;
 		diff_tree_sha1(commit->parents->item->object.oid.hash,
 			       commit->object.oid.hash, "", options);
-	else
+	} else
 		diff_root_tree_sha1(commit->object.oid.hash, "", options);
 	diffcore_std(options);
 	return diff_flush_patch_id(options, sha1, diff_header_only);
