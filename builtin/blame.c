149425db007a167433d5489592fcbfdb5ef65a1e
Junio C Hamano <gitster@pobox.com>
2016-09-11 14:32:00 -0700

    SQUASH???

--- builtin/blame.c
+++ builtin/blame.c
@@ -2820,7 +2820,9 @@ parse_done:
 				    &bottom, &top, sb.path))
 			usage(blame_usage);
 		if (lno < top || ((lno || bottom) && lno < bottom))
-			die(_("file %s has only %lu lines"), path, lno);
+			die(Q_("file %s has only %lu line",
+			       "file %s has only %lu lines", lno),
+			    path, lno);
 		if (bottom < 1)
 			bottom = 1;
 		if (top < 1)
