141e832e0fbf2afa67082c917bf17be2b8424c2f
Stefan Beller <sbeller@google.com>
2016-09-10 22:36:47 -0700

    diff.c: emit_line_0 can handle no color setting
    In a later patch, I want to propose an option to detect&color
    moved lines in a diff, which cannot be done in a one-pass over
    the diff. Instead we need to go over the whole diff twice,
    because we cannot detect the first line of the two corresponding
    lines (+ and -) that got moved.
    
    So to prepare the diff machinery for two pass algorithms
    (i.e. buffer it all up and then operate on the result),
    move all emissions to places, such that the only emitting
    function is emit_line_0.
    
    In later patches we may pass lines that are not colored to
    the central function emit_line_0, so we
    need to emit the color only when it is non-NULL.
    
    Change-Id: Ia52dc7476facc7fa757808930fc4986c96d9a720
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- diff.c
+++ diff.c
@@ -542,11 +542,13 @@ static void emit_line_0(struct diff_options *o, const char *set, const char *res
 	}
 
 	if (len || !nofirst) {
-		fputs(set, file);
+		if (set)
+			fputs(set, file);
 		if (!nofirst)
 			fputc(first, file);
 		fwrite(line, len, 1, file);
-		fputs(reset, file);
+		if (reset)
+			fputs(reset, file);
 	}
 	if (has_trailing_carriage_return)
 		fputc('\r', file);
