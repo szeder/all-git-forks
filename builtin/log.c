da196641befbe072d8bd4b0c9fac14f8f8dd5788
Junio C Hamano <gitster@pobox.com>
2016-09-14 14:27:36 -0700

    SQUASH???

--- builtin/log.c
+++ builtin/log.c
@@ -1360,7 +1360,7 @@ static void print_bases(struct base_tree_info *bases, FILE *file)
 		return;
 
 	/* Show the base commit */
-	fprintf(file, "base-commit: %s\n", oid_to_hex(&bases->base_commit));
+	fprintf(file, "\nbase-commit: %s\n", oid_to_hex(&bases->base_commit));
 
 	/* Show the prerequisite patches */
 	for (i = bases->nr_patch_id - 1; i >= 0; i--)
