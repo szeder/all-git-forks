e9be5df3bb6a6dc9eb837e2ceb230e2fbec03391
Christian Couder <chriscool@tuxfamily.org>
2016-09-18 10:14:28 +0200

    WIP: test-dump-s-i-info: write
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/helper/test-dump-s-i-info.c
+++ t/helper/test-dump-s-i-info.c
@@ -1,11 +1,26 @@
 #include "cache.h"
 
+static const char usage_str[] = "-v|--verbose (+|=|=+|=-|-)<seconds> <file>...";
+
 int cmd_main(int ac, const char **av)
 {
 	int i;
+	const char *command;
 
-	for (i = 0; i < ac; i++) {
-		printf("av[%d]: %s\n", i, av[i]);
+	if (ac < 2)
+		die("too few arguments");
+
+	command = av[1];
+
+	if (!strcmp(command, "write")) {
+		printf("writing args (ac: %d):\n", ac);
+		for (i = 2; i < ac; i++) {
+			printf("av[%d]: %s\n", i, av[i]);
+		}
+	} else {
+		for (i = 0; i < ac; i++) {
+			printf("av[%d]: %s\n", i, av[i]);
+		}
 	}
 
 	return 0;
