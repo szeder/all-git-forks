336a4230cab39e1214104ab3e00a76b2aa8c4002
Christian Couder <chriscool@tuxfamily.org>
2016-09-18 11:57:19 +0200

    WIP: test-dump-s-i-info: refactoring
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/helper/test-dump-s-i-info.c
+++ t/helper/test-dump-s-i-info.c
@@ -1,27 +1,39 @@
 #include "cache.h"
 
-static const char usage_str[] = "-v|--verbose (+|=|=+|=-|-)<seconds> <file>...";
+static const char usage_str[] = "(write|delete|read) <args>...";
+
+static void write_s_i_info(int ac, const char **av)
+{
+	int i;
+
+	printf("writing args (ac: %d):\n", ac);
+	for (i = 2; i < ac; i++) {
+		printf("av[%d]: %s\n", i, av[i]);
+	}
+}
+
+static void show_args(int ac, const char **av)
+{
+	int i;
+
+	for (i = 0; i < ac; i++) {
+		printf("av[%d]: %s\n", i, av[i]);
+	}
+}
 
 int cmd_main(int ac, const char **av)
 {
-	int i;
 	const char *command;
 
 	if (ac < 2)
-		die("too few arguments");
+		die("%s\nusage: %s %s", "too few arguments", av[0], usage_str);
 
 	command = av[1];
 
-	if (!strcmp(command, "write")) {
-		printf("writing args (ac: %d):\n", ac);
-		for (i = 2; i < ac; i++) {
-			printf("av[%d]: %s\n", i, av[i]);
-		}
-	} else {
-		for (i = 0; i < ac; i++) {
-			printf("av[%d]: %s\n", i, av[i]);
-		}
-	}
+	if (!strcmp(command, "write"))
+		write_s_i_info(ac, av);
+	else
+		show_args(ac, av);
 
 	return 0;
 }
