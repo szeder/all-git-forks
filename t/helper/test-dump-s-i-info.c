360411c75fafda64f7e043f74c87da9b41996b60
Christian Couder <chriscool@tuxfamily.org>
2016-09-18 15:30:34 +0200

    WIP: test-dump-s-i-info: sha1_from_path
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/helper/test-dump-s-i-info.c
+++ t/helper/test-dump-s-i-info.c
@@ -3,10 +3,20 @@
 static const char usage_str[] = "(write|delete|read) <args>...";
 static const char write_usage_str[] = "write <shared-index> <path>";
 
+static void sha1_from_path(unsigned char *sha1, const char *path)
+{
+	git_SHA_CTX ctx;
+
+	git_SHA1_Init(&ctx);
+	git_SHA1_Update(&ctx, path, strlen(path));
+	git_SHA1_Final(sha1, &ctx);
+}
+
 static void write_s_i_info(int ac, const char **av)
 {
 	const char *shared_index;
 	const char *path;
+	unsigned char path_sha1[GIT_SHA1_RAWSZ];
 
 	if (ac != 4)
 		die("%s\nusage: %s %s",
@@ -16,8 +26,11 @@ static void write_s_i_info(int ac, const char **av)
 	shared_index = av[2];
 	path = av[3];
 
+	sha1_from_path(path_sha1, path);
+
 	printf("writing shared_index: %s\n", shared_index);
 	printf("writing path: %s\n", path);
+	printf("writing path sha1: %s\n", sha1_to_hex(path_sha1));
 }
 
 static void show_args(int ac, const char **av)
