7fbd03e447ddee5d96975b5b4ec988a859653f09
Christian Couder <chriscool@tuxfamily.org>
2016-09-19 17:13:37 +0200

    WIP: test-dump-s-i-info: add read command
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/helper/test-dump-s-i-info.c
+++ t/helper/test-dump-s-i-info.c
@@ -4,6 +4,7 @@ static const char usage_str[] = "(write|delete|list|read) <args>...";
 static const char write_usage_str[] = "write <shared-index> <path>";
 static const char delete_usage_str[] = "delete <shared-index> <path>";
 static const char list_usage_str[] = "list <shared-index>";
+static const char read_usage_str[] = "read <shared-index> <path>";
 
 #define SHAREDINDEX_INFO "sharedindex-info"
 
@@ -89,12 +90,14 @@ static void handle_delete_command(int ac, const char **av)
 	delete_s_i_info(av[2], av[3]);
 }
 
-static void read_from_s_i_info_filename(struct strbuf *sb, const char *filename)
+static void read_s_i_info_into_list(const char *filename, struct string_list *paths)
 {
+	struct strbuf index_path = STRBUF_INIT;
 	const char *path = git_path(SHAREDINDEX_INFO "/%s", filename);
 
-	if (strbuf_read_file(sb, path, 0) < 0)
+	if (strbuf_read_file(&index_path, path, 0) < 0)
 		die_errno(_("could not read '%s'"), path);
+	string_list_append(paths, strbuf_detach(&index_path, NULL));
 }
 
 static void list_s_i_info(const char *shared_index, struct string_list *paths)
@@ -110,11 +113,8 @@ static void list_s_i_info(const char *shared_index, struct string_list *paths)
 	}
 
 	while ((de = readdir(dir)) != NULL) {
-		if (starts_with(de->d_name, shared_index)) {
-			struct strbuf index_path = STRBUF_INIT;
-			read_from_s_i_info_filename(&index_path, de->d_name);
-			string_list_append(paths, strbuf_detach(&index_path, NULL));
-		}
+		if (starts_with(de->d_name, shared_index))
+			read_s_i_info_into_list(de->d_name, paths);
 	}
 	closedir(dir);
 }
@@ -137,6 +137,36 @@ static void handle_list_command(int ac, const char **av)
 	}
 
 	string_list_clear(&paths, 0);
+}
+
+static void read_s_i_info(struct strbuf *sb,
+			  const char *shared_index,
+			  const char *path)
+{
+	struct strbuf s_i_info = STRBUF_INIT;
+
+	s_i_info_filename(&s_i_info, shared_index, path);
+
+	if (strbuf_read_file(sb, s_i_info.buf, 0) < 0)
+		die_errno(_("could not read '%s'"), s_i_info.buf);
+
+	strbuf_release(&s_i_info);
+}
+
+static void handle_read_command(int ac, const char **av)
+{
+	struct strbuf sb = STRBUF_INIT;
+
+	if (ac != 4)
+		die("%s\nusage: %s %s",
+		    "read command requires exactly 2 arguments",
+		    av[0], read_usage_str);
+
+	read_s_i_info(&sb, av[2], av[3]);
+
+	printf("%s", sb.buf);
+
+	strbuf_release(&sb);
 }
 
 static void show_args(int ac, const char **av)
@@ -163,6 +193,8 @@ int cmd_main(int ac, const char **av)
 		handle_delete_command(ac, av);
 	else if (!strcmp(command, "list"))
 		handle_list_command(ac, av);
+	else if (!strcmp(command, "read"))
+		handle_read_command(ac, av);
 	else
 		show_args(ac, av);
 
