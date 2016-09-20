223485c396b7b62dd050821027184887fcc7b56e
Christian Couder <chriscool@tuxfamily.org>
2016-09-20 14:40:13 +0200

    WIP: read-cache: save split index info
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- read-cache.c
+++ read-cache.c
@@ -2184,9 +2184,11 @@ static int write_split_index(struct index_state *istate,
 			     unsigned flags)
 {
 	int ret;
+
 	prepare_to_write_split_index(istate);
 	ret = do_write_locked_index(istate, lock, flags);
 	finish_writing_split_index(istate);
+
 	return ret;
 }
 
