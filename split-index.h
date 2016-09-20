379317e9038be6df21759b8290359f5a93807ff5
Christian Couder <chriscool@tuxfamily.org>
2016-09-20 16:26:05 +0200

    WIP: read-cache: remove split index info
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- split-index.h
+++ split-index.h
@@ -74,5 +74,6 @@ void split_index_canary_filename(struct strbuf *sb,
 				 const char *shared_index,
 				 const char *path);
 void write_split_index_canary(const char *shared_index, const char *path);
+void delete_split_index_canary(const char *shared_index, const char *path);
 
 #endif
