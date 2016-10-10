6d9a6d53cb53dd3d0c980cbbfbe50bc8ba5030fa
Christian Couder <chriscool@tuxfamily.org>
2016-10-10 10:30:35 +0200

    read-cache: check canary files before deleting
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- read-cache.c
+++ read-cache.c
@@ -2216,6 +2216,8 @@ static int can_delete_shared_index(const char *shared_sha1_hex)
 {
 	struct stat st;
 	time_t now = time(NULL);
+	int i;
+	struct string_list index_paths = STRING_LIST_INIT_NODUP;
 	const char *shared_index = git_path("sharedindex.%s", shared_sha1_hex);
 
 	/* Check timestamp */
@@ -2223,6 +2225,16 @@ static int can_delete_shared_index(const char *shared_sha1_hex)
 		return error_errno("could not stat '%s", shared_index);
 	if (now < st.st_mtime + shared_index_expire)
 		return 0;
+
+	/* Check canary files */
+	read_all_split_index_canaries(shared_sha1_hex, &index_paths);
+	for (i = 0; i < index_paths.nr; ++i) {
+		char *path = index_paths.items[i].string;
+		if (file_exists(path))
+			return 0;
+		/* Delete canary file */
+		delete_split_index_canary(shared_sha1_hex, path);
+	}
 
 	return 1;
 }
