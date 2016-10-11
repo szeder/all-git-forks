0cbea0da24b0eaffe32d076035129ae5bb536ca4
Christian Couder <chriscool@tuxfamily.org>
2016-10-11 18:45:18 +0200

    lib-httpd/list: implement retrieving blobs
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/lib-httpd/list.sh
+++ t/lib-httpd/list.sh
@@ -11,11 +11,32 @@ CUR_LOG="list.log"
 
 echo "query string: '$QUERY_STRING'"  >>"$CUR_LOG"
 
+while test $# -gt 0
+do
+    key=${1%=*}
+    val=${1#*=}
+
+    echo "$key: $val" >>"$CUR_LOG"
+
+    case "$key" in
+	"sha1") sha1="$val" ;;
+	*) echo "unknown key '$key'" >>"$CUR_LOG" ;;
+    esac
+
+    shift
+done
+
+
 echo 'Status: 200 OK'
 echo
 
 if test -d "$FILES_DIR"
 then
-    ls "$FILES_DIR" | tr '-' ' '
+    if test -n "$sha1"
+    then
+	cat "$FILES_DIR/$sha1"-*
+    else
+	ls "$FILES_DIR" | tr '-' ' '
+    fi
 fi
 
