01daa347558e3d88998b2bc1d909a64120e28a26
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-10-13 14:34:04 +0200

    base: cherry-pick 138e60d onto 320abcd
    This commit helps to teach `git rerere` to resolve merge 
    conflicts when cherry-picking:
    
    	138e60d (sequencer: use static initializers for replay_opts, 2016-02-08)

--- sequencer.h
+++ sequencer.h
@@ -1,7 +1,10 @@
 #ifndef SEQUENCER_H
 #define SEQUENCER_H
 
-const char *git_path_seq_dir(void);
+#define SEQ_DIR		"sequencer"
+#define SEQ_HEAD_FILE	"sequencer/head"
+#define SEQ_TODO_FILE	"sequencer/todo"
+#define SEQ_OPTS_FILE	"sequencer/opts"
 
 #define APPEND_SIGNOFF_DEDUP (1u << 0)
 
@@ -10,8 +13,16 @@ enum replay_action {
 	REPLAY_PICK
 };
 
+enum replay_subcommand {
+	REPLAY_NONE,
+	REPLAY_REMOVE_STATE,
+	REPLAY_CONTINUE,
+	REPLAY_ROLLBACK
+};
+
 struct replay_opts {
 	enum replay_action action;
+	enum replay_subcommand subcommand;
 
 	/* Boolean options */
 	int edit;
@@ -26,22 +37,18 @@ struct replay_opts {
 
 	int mainline;
 
-	char *gpg_sign;
+	const char *gpg_sign;
 
 	/* Merge strategy */
-	char *strategy;
-	char **xopts;
+	const char *strategy;
+	const char **xopts;
 	size_t xopts_nr, xopts_alloc;
 
 	/* Only used by REPLAY_NONE */
 	struct rev_info *revs;
 };
-#define REPLAY_OPTS_INIT { -1 }
 
 int sequencer_pick_revisions(struct replay_opts *opts);
-int sequencer_continue(struct replay_opts *opts);
-int sequencer_rollback(struct replay_opts *opts);
-int sequencer_remove_state(struct replay_opts *opts);
 
 extern const char sign_off_header[];
 
