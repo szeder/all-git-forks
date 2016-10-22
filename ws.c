544e00e08c1cf56d1a34f0d8cffe103ce8b420b8
Stefan Beller <sbeller@google.com>
2016-10-22 16:32:16 -0700

    attr: convert to new threadsafe API
    This revamps the API of the attr subsystem to be thread safe.
    Before we had the question and its results in one struct type.
    The typical usage of the API was
    
        static struct git_attr_check *check;
    
        if (!check)
            check = git_attr_check_initl("text", NULL);
    
        git_check_attr(path, check);
        act_on(check->value[0]);
    
    This has a couple of issues when it comes to thread safety:
    
    * the initialization is racy in this implementation. To make it
      thread safe, we need to acquire a mutex, such that only one
      thread is executing the code in git_attr_check_initl.
      As we do not want to introduce a mutex at each call site,
      this is best done in the attr code. However to do so, we need
      to have access to the `check` variable, i.e. the API has to
      look like
        git_attr_check_initl(struct git_attr_check*, ...);
      Then one of the threads calling git_attr_check_initl will
      acquire the mutex and init the `check`, while all other threads
      will wait on the mutex just to realize they're late to the
      party and they'll return with no work done.
    
    * While the check for attributes to be questioned only need to
      be initalized once as that part will be read only after its
      initialisation, the answer may be different for each path.
      Because of that we need to decouple the check and the answer,
      such that each thread can obtain an answer for the path it
      is currently processing.
    
    This commit changes the API and adds locking in
    git_attr_check_initl that provides the thread safety for constructing
    `struct git_attr_check`.
    
    The usage of the new API will be:
    
        /*
         * The initl call will thread-safely check whether the
         * struct git_attr_check has been initialized. We only
         * want to do the initialization work once, hence we do
         * that work inside a thread safe environment.
         */
        static struct git_attr_check *check;
        git_attr_check_initl(&check, "text", NULL);
    
        /*
         * Obtain a pointer to a correctly sized result
         * statically allocated on the stack; this macro:
         */
        GIT_ATTR_RESULT_INIT_FOR(myresult, 1);
    
        /* Perform the check and act on it: */
        git_check_attr(path, check, myresult);
        act_on(myresult->value[0]);
    
        /*
         * No need to free the check as it is static, hence doesn't leak
         * memory. The result is also static, so no need to free there either.
         */
    
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- ws.c
+++ ws.c
@@ -74,15 +74,12 @@ unsigned parse_whitespace_rule(const char *string)
 unsigned whitespace_rule(const char *pathname)
 {
 	static struct git_attr_check *attr_whitespace_rule;
+	struct git_attr_result result[1];
 
-	if (!attr_whitespace_rule)
-		attr_whitespace_rule = git_attr_check_initl("whitespace", NULL);
+	git_attr_check_initl(&attr_whitespace_rule, "whitespace", NULL);
 
-	if (!git_check_attr(pathname, attr_whitespace_rule)) {
-		const char *value;
-
-		value = attr_whitespace_rule->check[0].value;
-		if (ATTR_TRUE(value)) {
+	if (!git_check_attr(pathname, attr_whitespace_rule, result)) {
+		if (ATTR_TRUE(result[0].value)) {
 			/* true (whitespace) */
 			unsigned all_rule = ws_tab_width(whitespace_rule_cfg);
 			int i;
@@ -91,15 +88,15 @@ unsigned whitespace_rule(const char *pathname)
 				    !whitespace_rule_names[i].exclude_default)
 					all_rule |= whitespace_rule_names[i].rule_bits;
 			return all_rule;
-		} else if (ATTR_FALSE(value)) {
+		} else if (ATTR_FALSE(result[0].value)) {
 			/* false (-whitespace) */
 			return ws_tab_width(whitespace_rule_cfg);
-		} else if (ATTR_UNSET(value)) {
+		} else if (ATTR_UNSET(result[0].value)) {
 			/* reset to default (!whitespace) */
 			return whitespace_rule_cfg;
 		} else {
 			/* string */
-			return parse_whitespace_rule(value);
+			return parse_whitespace_rule(result[0].value);
 		}
 	} else {
 		return whitespace_rule_cfg;
