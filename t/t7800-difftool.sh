836f806a01cd2705ea94234c5e81c546cdb62923
David Aguilar <davvid@gmail.com>
2016-12-05 14:26:00 -0800

    difftool: fix dir-diff index creation when in a subdirectory
    9ec26e7977 ("difftool: fix argument handling in subdirs",
    2016-07-18) corrected how path arguments are handled in a
    subdirectory, but it introduced a regression in how entries outside
    of the subdirectory are handled by dir-diff.
    
    When preparing the right-side of the diff we only include the
    changed paths in the temporary area.
    
    The left side of the diff is constructed from a temporary
    index that is built from the same set of changed files, but it
    was being constructed from within the subdirectory.  This is a
    problem because the indexed paths are toplevel-relative, and
    thus they were not getting added to the index.
    
    Teach difftool to chdir to the toplevel of the repository before
    preparing its temporary indexes.  This ensures that all of the
    toplevel-relative paths are valid.
    
    Adjust the test cases to more thoroughly exercise this scenario.
    
    Reported-by: Frank Becker <fb@mooflu.com>
    Signed-off-by: David Aguilar <davvid@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7800-difftool.sh
+++ t/t7800-difftool.sh
@@ -413,8 +413,11 @@ run_dir_diff_test 'difftool --dir-diff from subdirectory' '
 	(
 		cd sub &&
 		git difftool --dir-diff $symlinks --extcmd ls branch >output &&
-		grep sub output &&
-		grep file output
+		# "sub" must only exist in "right"
+		# "file" and "file2" must be listed in both "left" and "right"
+		test "1" = $(grep sub output | wc -l) &&
+		test "2" = $(grep file"$" output | wc -l) &&
+		test "2" = $(grep file2 output | wc -l)
 	)
 '
 
