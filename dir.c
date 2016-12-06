ed0cf0fa5bb69272b51c8436b8d90a44f3c83da2
Brandon Williams <bmwill@google.com>
2016-12-06 13:51:32 -0800

    dir: convert fill_directory to use the pathspec struct interface
    Convert 'fill_directory()' to use the pathspec struct interface from
    using the '_raw' entry in the pathspec struct.
    
    Signed-off-by: Brandon Williams <bmwill@google.com>

--- dir.c
+++ dir.c
@@ -188,7 +188,8 @@ int fill_directory(struct dir_struct *dir, const struct pathspec *pathspec)
 	len = common_prefix_len(pathspec);
 
 	/* Read the directory and prune it */
-	read_directory(dir, pathspec->nr ? pathspec->_raw[0] : "", len, pathspec);
+	read_directory(dir, pathspec->nr ? pathspec->items[0].match : "",
+		       len, pathspec);
 	return len;
 }
 
