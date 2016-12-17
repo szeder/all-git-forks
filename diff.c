32c39f1c8013dec040e9fea1fdb9d28c4aab5b28
Jacob Keller <jacob.keller@gmail.com>
2016-12-16 16:54:42 -0800

    diff: prefer indent heuristic over compaction heuristic
    The current configuration code for enabling experimental heuristics
    prefers the last-set heuristic in the configuration. However, it is not
    necessarily easy to see what order the configuration will be read. This
    means that it is possible for a user to have accidentally enabled both
    heuristics, and end up only enabling the older compaction heuristic.
    
    Modify the code so that we do not clear the other heuristic when we set
    each heuristic enabled. Then, during diff_setup() when we check the
    configuration, we will first check the newer indent heuristic. This
    ensures that we only enable the newer heuristic if both have been
    enabled.
    
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- diff.c
+++ diff.c
@@ -223,16 +223,10 @@ void init_diff_ui_defaults(void)
 
 int git_diff_heuristic_config(const char *var, const char *value, void *cb)
 {
-	if (!strcmp(var, "diff.indentheuristic")) {
+	if (!strcmp(var, "diff.indentheuristic"))
 		diff_indent_heuristic = git_config_bool(var, value);
-		if (diff_indent_heuristic)
-			diff_compaction_heuristic = 0;
-	}
-	if (!strcmp(var, "diff.compactionheuristic")) {
+	if (!strcmp(var, "diff.compactionheuristic"))
 		diff_compaction_heuristic = git_config_bool(var, value);
-		if (diff_compaction_heuristic)
-			diff_indent_heuristic = 0;
-	}
 	return 0;
 }
 
