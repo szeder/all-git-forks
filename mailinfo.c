feeee628adaa55a37925f21468873f0679be69b6
Kyle J. McKay <mackyle@gmail.com>
2016-12-17 11:54:18 -0800

    mailinfo.c: move side-effects outside of assert
    Since 6b4b013f18 (mailinfo: handle in-body header continuations,
    2016-09-20, v2.11.0) mailinfo.c has contained new code with an
    assert of the form:
    
    	assert(call_a_function(...))
    
    The function in question, check_header, has side effects.  This
    means that when NDEBUG is defined during a release build the
    function call is omitted entirely, the side effects do not
    take place and tests (fortunately) start failing.
    
    Move the function call outside of the assert and assert on
    the result of the function call instead so that the code
    still works properly in a release build and passes the tests.
    
    Signed-off-by: Kyle J. McKay <mackyle@gmail.com>

--- mailinfo.c
+++ mailinfo.c
@@ -708,9 +708,12 @@ static int is_scissors_line(const char *line)
 
 static void flush_inbody_header_accum(struct mailinfo *mi)
 {
+	int okay;
+
 	if (!mi->inbody_header_accum.len)
 		return;
-	assert(check_header(mi, &mi->inbody_header_accum, mi->s_hdr_data, 0));
+	okay = check_header(mi, &mi->inbody_header_accum, mi->s_hdr_data, 0);
+	assert(okay);
 	strbuf_reset(&mi->inbody_header_accum);
 }
 
