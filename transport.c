5b7a6eef511649d4c1f928e11d0721f81fed9323
Stefan Beller <sbeller@google.com>
2017-01-13 15:54:27 -0800

    transport submodules: correct error message
    When push.recurseSubmodules is set to "check" or "on-demand", the transport
    layer tries to determine if a submodule needs pushing. This check is done
    by walking all remote refs that are known.
    
    For remotes we only store the refs/heads/* (and tags), which doesn't
    include all commits. In e.g. Gerrit commits often end up at refs/changes/*
    (that we do not store) when pushing to refs/for/master (which we also do
    not store). So a workflow such as the following still fails:
    
        $ git -C <submodule> push origin HEAD:refs/for/master
        $ git push origin HEAD:refs/for/master
        The following submodule paths contain changes that can
        not be found on any remote:
          submodule
    
        Please try
    
            git push --recurse-submodules=on-demand
    
        or cd to the path and use
    
            git push
    
        to push them to a remote.
    
    Trying to push with --recurse-submodules=on-demand would run into
    the same problem. To fix this issue
        1) specifically mention that we looked for branches on the remote.
        2) advertise pushing without recursing into submodules. ("Use this
           command to make the error message go away")
    
    While at it, remove some empty lines, as they blow up the error message.
    
    Reported-by: Dave Borowitz <dborowitz@google.com>
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- transport.c
+++ transport.c
@@ -883,14 +883,14 @@ static void die_with_unpushed_submodules(struct string_list *needs_pushing)
 	int i;
 
 	fprintf(stderr, _("The following submodule paths contain changes that can\n"
-			"not be found on any remote:\n"));
+			"not be found on any remote branch:\n"));
 	for (i = 0; i < needs_pushing->nr; i++)
 		fprintf(stderr, "  %s\n", needs_pushing->items[i].string);
-	fprintf(stderr, _("\nPlease try\n\n"
-			  "	git push --recurse-submodules=on-demand\n\n"
-			  "or cd to the path and use\n\n"
-			  "	git push\n\n"
-			  "to push them to a remote.\n\n"));
+	fprintf(stderr, _("\nSuppress submodule checks via\n"
+			  "	git push --no-recurse-submodules\n"
+			  "or cd to the path and use\n"
+			  "	git push\n"
+			  "to push them to a remote.\n"));
 
 	string_list_clear(needs_pushing, 0);
 
